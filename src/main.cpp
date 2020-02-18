7a7533ddfa0a1292ccd0de3d971377020e076fcb
Ben Wibking <benwibking@gmail.com>
2020-02-18 17:55:02 +1100

    added plot of initial and advected density profile; there appears to be a sign error in the PPM limiter

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,9 @@
 #include <cstring>
 #include <iostream>
 
+// external headers
+#include "matplotlibcpp.h"
+
 // internal headers
 #include "athena_arrays.hpp"
 #include "linear_advection.hpp"
@@ -35,8 +38,9 @@ auto main() -> int
 	const int nx = 32;
 	const double Lx = 1.0;
 	const double advection_velocity = 1.0;
-	const double CFL_number = 0.5;
+	const double CFL_number = 0.4;
 	const double max_time = 1.0;
+	const int max_timesteps = 5;
 
 	const double atol = 1e-13; //< absolute tolerance for mass conservation
 
@@ -55,12 +59,18 @@ auto main() -> int
 	auto nghost = advection_system.nghost();
 
 	for (int i = nghost; i < nx + nghost; ++i) {
+
 		auto value = static_cast<double>(i - nghost);
+		advection_system.density_(i) = value;
 
-		// advection_system.density_(i) = value;
+		// advection_system.density_(i) =
+		//    std::sin(M_PI * (value + 0.5) / static_cast<double>(nx));
+	}
 
-		advection_system.density_(i) =
-		    std::sin(M_PI * (value + 0.5) / static_cast<double>(nx));
+	std::vector<double> x(nx), d_initial(nx), d_final(nx);
+	for (int i = 0; i < nx; ++i) {
+		x.at(i) = static_cast<double>(i);
+		d_initial.at(i) = advection_system.density_(i + nghost);
 	}
 
 	std::cout << "Initial conditions:"
@@ -71,8 +81,6 @@ auto main() -> int
 	const auto initial_mass = advection_system.ComputeMass();
 
 	// Main time loop
-
-	const int max_timesteps = 1000;
 
 	for (int j = 0; j < max_timesteps; ++j) {
 		if (advection_system.time() >= max_time) {
@@ -94,6 +102,15 @@ auto main() -> int
 
 		std::cout << "\n";
 	}
+
+	// Plot results
+	for (int i = 0; i < nx; ++i) {
+		d_final.at(i) = advection_system.density_(i + nghost);
+	}
+
+	matplotlibcpp::plot(x, d_initial);
+	matplotlibcpp::plot(x, d_final);
+	matplotlibcpp::save(std::string("./advection.png"));
 
 	// Cleanup and exit
 	std::cout << "Finished." << std::endl;
