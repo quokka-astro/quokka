7e098af59daf750434c84e0d630307c8904c5de2
Ben Wibking <benwibking@gmail.com>
2020-06-24 16:47:43 +1000

    tighten ODE integration tolerances

--- extern/LowrieEdwards/radshock.py
+++ extern/LowrieEdwards/radshock.py
@@ -95,7 +95,7 @@ def dT_dTrad(rho, T, Trad, gamma, M0, P0, sign=-1.0, sign2=1.0):
 ## compute asymptotic states
 gamma = 5./3.
 P0 = 1.0e-4
-M0 = 3.0
+M0 = 30.0
 
 T0 = 1.0
 Trad0 = T0
@@ -117,7 +117,11 @@ c = 100.0 * (M0 + 1.0)  # dimensionless speed of light
 kappa = 1
 
 ## compute solution
-eps = 1e-3
+# eps is slightly problem dependent -- if too small (for high Mach numbers), will cause CVODE errors:
+#   [CVODE ERROR]  CVode
+#     At t = 4.99999, mxstep steps taken before reaching tout.
+eps = 1e-5
+assert(eps <= 1e-3) # but don't make it larger than 1e-3, otherwise solutions are quite wrong
 epsA = eps
 epsB = -eps
 Trad_epsA = Trad0 + epsA
@@ -133,7 +137,8 @@ def rho(T, Trad, sign):
             sqrt((Km - gamma*P0*Trad**4)**2 - 36*gamma*M0**2 * T)) / (6*T)
 
 rho_epsA = rho(T_epsA, Trad_epsA, sign_A)
-rho_epsB = rho(T_epsB, Trad_epsB, sign_B)   # wrong?
+rho_epsB = rho(T_epsB, Trad_epsB, sign_B)
+assert(np.allclose(rho1,rho_epsB,rtol=2*eps))  # if triggered, this indicates the wrong sign_B was chosen
 print(f"rho_epsA = {rho_epsA}")
 print(f"rho_epsB = {rho_epsB}")
 v_epsA = M0 / rho_epsA
@@ -149,7 +154,9 @@ y0_B = np.array([ epsB/Traddot_epsB, T_epsB])
 x0_A = v_epsA / sqrt(T_epsA)
 x0_B = v_epsB / sqrt(T_epsB)
 
-eps_ASP = 0.  # avoid the Adiabatic Sonic Point (only needed for *continuous* solutions w/out hydro shock)
+# avoid the Adiabatic Sonic Point (only needed for *continuous* solutions w/out hydro shock)
+#eps_ASP = 1.0e-5
+eps_ASP = 0.
 x1_A = 1 + eps_ASP
 x1_B = 1 - eps_ASP
 
@@ -157,14 +164,15 @@ print(f"Left-side initial conditions = ({x0_A}, {y0_A})")
 print(f"Right-side initial conditions = ({x0_B}, {y0_B})")
 
 ## integrate ODE
-xsol_A = np.linspace(x0_A, x1_A, 512, endpoint=False)
-xsol_B = np.linspace(x0_B, x1_B, 512, endpoint=False)
+xsol_A = np.linspace(x0_A, x1_A, 1024, endpoint=True)
+xsol_B = np.linspace(x0_B, x1_B, 1024, endpoint=True)
 fun = lambda x,y,ydot: shock(x,y,ydot, gamma=gamma, P0=P0, M0=M0, kappa=kappa)
 
+options = {'max_steps': 50000, 'rtol': 1e-8, 'atol': 1e-12}
 print("Integrating precursor region...")
-solution_A = ode('cvode', fun, old_api=False).solve(xsol_A, y0_A)
+solution_A = ode('cvode', fun, old_api=False, **options).solve(xsol_A, y0_A)
 print("Integrating relaxation region...")
-solution_B = ode('cvode', fun, old_api=False).solve(xsol_B, y0_B)
+solution_B = ode('cvode', fun, old_api=False, **options).solve(xsol_B, y0_B)
 
 ## plot solution
 M_A = solution_A.values.t
@@ -180,6 +188,9 @@ rho_B = M0 / vel_B
 Trad_A = Trad_fun(rho_A, T_A, gamma=gamma, P0=P0, M0=M0)
 Trad_B = Trad_fun(rho_B, T_B, gamma=gamma, P0=P0, M0=M0)
 
+print(f"Mach_A ranges from [{np.min(M_A)}, {np.max(M_A)}] = {np.max(M_A) - np.min(M_A):.3e}")
+print(f"Mach_B ranges from [{np.min(M_B)}, {np.max(M_B)}] = {np.max(M_B) - np.min(M_B):.3e}")
+
 ## connect solutions
 ## -> solve 2d root-finding problem:
 ##      f(\Delta x_A, \Delta x_B) = 0, where
@@ -187,7 +198,7 @@ Trad_B = Trad_fun(rho_B, T_B, gamma=gamma, P0=P0, M0=M0)
 ##      j_p = ( (\rho v)_p, (\rho v^2 + P)_p, [v(\rho E + P)]_p )
 ##      j_s = ( (\rho v)_s, (\rho v^2 + P)_s, [v(\rho E + P)]_s )
 
-interp = lambda x,y: scipy.interpolate.interp1d(x,y,kind='linear',fill_value='extrapolate')
+interp = lambda x,y: scipy.interpolate.interp1d(x,y,kind='cubic',fill_value='extrapolate')
 
 rho_Afun = interp(x_A, rho_A)
 vel_Afun = interp(x_A, vel_A)
@@ -202,20 +213,20 @@ Trad_Bfun= interp(x_B, Trad_B)
 def objective(dx):
     """compute the figure of merit f, defined above."""
     x0 = 0.
-    x_A = x0 - dx[0]
-    x_B = x0 - dx[1]
+    xA = x0 - dx[0]
+    xB = x0 - dx[1]
 
-    rhoA = rho_Afun(x_A)
-    velA = vel_Afun(x_A)
-    TmatA = T_Afun(x_A)
-    TradA = Trad_Afun(x_A)
+    rhoA = rho_Afun(xA)
+    velA = vel_Afun(xA)
+    TmatA = T_Afun(xA)
+    TradA = Trad_Afun(xA)
     P_a = rhoA*TmatA / gamma
     E_a = TmatA / (gamma*(gamma-1)) + 0.5*(velA**2)
 
-    rhoB = rho_Bfun(x_B)
-    velB = vel_Bfun(x_B)
-    TmatB = T_Bfun(x_B)
-    TradB = Trad_Bfun(x_B)
+    rhoB = rho_Bfun(xB)
+    velB = vel_Bfun(xB)
+    TmatB = T_Bfun(xB)
+    TradB = Trad_Bfun(xB)
     P_b = rhoA*TmatA / gamma
     E_b = TmatB / (gamma*(gamma-1)) + 0.5*(velB**2)
 
@@ -223,18 +234,23 @@ def objective(dx):
     j_s = np.array([rhoB*velB, rhoB*velB**2 + P_b, velB*(rhoB*E_b + P_b)])
 
     #norm = np.sum((j_p - j_s)**2) + (TradA - TradB)**2     # bad
-    norm = (rhoA*velA - rhoB*velB)**2 + (TradA - TradB)**2  # good
+    norm = (rhoA*velA - rhoB*velB)**2 + (TradA - TradB)**2 + 0.1*dx[0]**2  # good
     return norm
 
 dx_guess = np.array([-np.max(x_A), -np.min(x_B)])
 print(f"dx_guess = {dx_guess}")
+
 sol = scipy.optimize.minimize(objective, dx_guess, method='powell', tol=1e-10)
 dx_A, dx_B = sol.x
+print(f"objective = {sol.fun} after {sol.nit} iterations.")
+#dx_A, dx_B = dx_guess
 print(f"dx_A = {dx_A}")
 print(f"dx_B = {dx_B}")
-print(f"objective = {sol.fun} after {sol.nit} iterations.")
+
 x_A += dx_A
 x_B += dx_B
+print(f"x_A ranges from [{np.min(x_A)}, {np.max(x_A)}] = {np.max(x_A) - np.min(x_A):.3e}")
+print(f"x_B ranges from [{np.min(x_B)}, {np.max(x_B)}] = {np.max(x_B) - np.min(x_B):.3e}")
 A_mask = (x_A <= 0.)
 B_mask = (x_B >= 0.)
 
@@ -248,16 +264,18 @@ plt.plot(x_B[B_mask], T_B[B_mask], color='black')
 plt.plot(x_B[B_mask], Trad_B[B_mask], '-.', color='black')
 #plt.plot(x_B[B_mask], vel_B[B_mask], color='red')
 
-# plot temperature shock jump
-plt.scatter(x_A[A_mask][-1], T_A[A_mask][-1], color='black')
-plt.scatter(x_B[B_mask][-1], T_B[B_mask][-1], color='black')
+plot_jump = True
+if plot_jump:
+    # plot temperature shock jump
+    plt.scatter(x_A[A_mask][-1], T_A[A_mask][-1], color='black')
+    plt.scatter(x_B[B_mask][-1], T_B[B_mask][-1], color='black')
 
-# plot density shock jump
-plt.scatter(x_A[A_mask][-1], rho_A[A_mask][-1], color='black')
-plt.scatter(x_B[B_mask][-1], rho_B[B_mask][-1], color='black')
+    # plot density shock jump
+    plt.scatter(x_A[A_mask][-1], rho_A[A_mask][-1], color='black')
+    plt.scatter(x_B[B_mask][-1], rho_B[B_mask][-1], color='black')
 
 # plot discarded (unphysical) regions of solutions
-plot_discarded = False
+plot_discarded = True
 if plot_discarded:
     plt.plot(x_A[~A_mask], rho_A[~A_mask], '--', color='blue', alpha=0.5)
     plt.plot(x_A[~A_mask], T_A[~A_mask], '--', color='black',  alpha=0.5)
@@ -271,7 +289,34 @@ if plot_discarded:
 
 plt.legend(loc='best')
 plt.title(f"M0 = {M0}, P0 = {P0}, kappa = {kappa}, sigma_a = {sigma_a:.1e}")
-plt.xlim(-0.01, 0.005)
-plt.ylim(1.0, 4.5)
+
+# Mach 1.05 plot
+#plt.xlim(-0.015, 0.015)
+#plt.ylim(1.0, 1.08)
+
+# Mach 1.2 plot
+#plt.xlim(-0.01, 0.01)
+#plt.ylim(1.0, 1.3)
+
+# Mach 1.4 plot
+#plt.xlim(-0.01, 0.01)
+#plt.ylim(1.0, 1.6)
+
+# Mach 2 plot
+#plt.xlim(-0.01, 0.01)
+#plt.ylim(1, 2.5)
+
+# Mach 3 plot
+#plt.xlim(-0.01, 0.005)
+#plt.ylim(1, 4.5)
+
+# Mach 5 plot
+#plt.xlim(-0.04, 0.01)
+#plt.ylim(1, 11)
+
+# Mach 27 plot
+plt.xlim(-0.25, 0.05)
+plt.ylim(1., 70.)
+
 plt.tight_layout()
 plt.savefig('ode_solution.pdf')
