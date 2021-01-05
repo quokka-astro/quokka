94141b2f8b43572f7403049d063c58130d06d755
Ben Wibking <benwibking@gmail.com>
2021-01-05 17:45:53 +1100

    updated radiation pulse test parameters

--- src/test_radiation_pulse.cpp
+++ src/test_radiation_pulse.cpp
@@ -31,36 +31,26 @@ auto main() -> int
 struct PulseProblem {
 }; // dummy type to allow compile-type polymorphism via template specialization
 
-const double kappa = 1.0e4;
+const double kappa0 = 1.0e3;   // cm^-1 (opacity)
 const double rho = 1.0;	       // g cm^-3 (matter density)
-const double a_rad = 1.0e-5;
-const double c = 1.0;
-const double Erad_floor = 1e-10;
+const double a_rad = 4.0e-10;  // radiation constant == 4sigma_SB/c (dimensionless)
+const double c = 1.0e10;	   // speed of light (dimensionless)
+const double chat = 1.0e4;
+const double Erad_floor = a_rad * std::pow(1.0e-5, 4);
 
 const double Lx = 1.0;	  // dimensionless length
 const double x0 = Lx / 2.0;
-const double initial_time = 1.0;
+const double initial_time = 1.0e-8;
 
-auto compute_exact_erad(const double x, const double t) -> double
+auto compute_exact_Trad(const double x, const double t) -> double
 {
 	// compute exact solution for Gaussian radiation pulse
 	// 		assuming diffusion approximation
 	const double sigma = 0.025;
-	const double D = c / (3.0*kappa);
+	const double D = 4.0 * c * a_rad / (3.0 * kappa0);
 	const double width_sq = (sigma*sigma + D*t);
 	const double normfac = 1.0 / (2.0 * std::sqrt( M_PI * width_sq ));
-	return normfac * std::exp( -(x*x) / (4.0*width_sq) );
-}
-
-auto compute_exact_flux(const double x, const double t) -> double
-{
-	// compute exact solution for Gaussian radiation pulse
-	// 		assuming diffusion approximation
-	const double sigma = 0.025;
-	const double D = c / (3.0*kappa);
-	const double width_sq = (sigma*sigma + D*t);
-	const double normfac = 1.0 / (2.0 * std::sqrt( M_PI * width_sq ));
-	return D * normfac * std::exp( -(x*x) / (4.0*width_sq) ) * x / (2.0*width_sq);
+	return 0.5 * normfac * std::exp( -(x*x) / (4.0*width_sq) );
 }
 
 template <> void RadSystem<PulseProblem>::FillGhostZones(array_t &cons)
@@ -70,8 +60,8 @@ template <> void RadSystem<PulseProblem>::FillGhostZones(array_t &cons)
 	// x1 left side boundary
 	for (int i = 0; i < nghost_; ++i) {
 		const double x = lx_ * ((i + 0.5) / static_cast<double>(nx_));
-		const double Erad = compute_exact_erad(x - x0, t);
-		const double Frad = std::min(compute_exact_flux(x - x0, t), -c*Erad);
+		const double Erad = Erad_floor_;
+		const double Frad = 0.;
 		
 		cons(radEnergy_index, i) = Erad;
 		cons(x1RadFlux_index, i) = Frad;
@@ -80,8 +70,8 @@ template <> void RadSystem<PulseProblem>::FillGhostZones(array_t &cons)
 	// x1 right side boundary
 	for (int i = nghost_ + nx_; i < nghost_ + nx_ + nghost_; ++i) {
 		const double x = lx_ * ((i + 0.5) / static_cast<double>(nx_));
-		const double Erad = compute_exact_erad(x - x0, t);
-		const double Frad = std::max(compute_exact_flux(x - x0, t), c*Erad);
+		const double Erad = Erad_floor_;
+		const double Frad = 0.;
 		
 		cons(radEnergy_index, i) = Erad;
 		cons(x1RadFlux_index, i) = Frad;
@@ -92,59 +82,67 @@ template <>
 auto RadSystem<PulseProblem>::ComputeOpacity(const double rho,
 					       const double Tgas) -> double
 {
-	return (kappa / rho);
+	return (kappa0 / rho) * std::max(std::pow(Tgas, 3), 1.0);
 }
 
 auto testproblem_radiation_pulse() -> int
 {
+	// This problem is a *linear* radiation diffusion problem, i.e.
+	// parameters are chosen such that the radiation and gas temperatures
+	// should be near equilibrium, and the opacity is chosen to go as
+	// T^3, such that the radiation diffusion equation becomes linear in T.
+
+	// This makes this problem a stringent test of the asymptotic-
+	// preserving property of the computational method, since the
+	// optical depth per cell at the peak of the temperature profile is
+	// ~10^2 or greater.
+
+	// Due to the long diffusion time (compared to Lx/c), the reduced 
+	// speed of light approximation must be used.
+
 	// Problem parameters
 
-	const int max_timesteps = 1e5;
+	const int max_timesteps = 1e6;
 	const double CFL_number = 0.8;
-	const int nx = 100;
+	const int nx = 500;
 
-	const double initial_dt = 1e-5; // dimensionless time
-	const double max_dt = 1e-4;	  // dimensionless time
-	const double max_time = 10.0;	  // dimensionless time
+	const double max_dt = 1e-3;	  	// dimensionless time
+	const double max_time = 1.0e-3;	// dimensionless time
 
 	// Problem initialization
 
-	std::vector<double> T_eq(nx);
+	std::vector<double> T_initial(nx);
 	std::vector<double> Erad_initial(nx);
 	std::vector<double> Frad_initial(nx);
 
 	for (int i = 0; i < nx; ++i) {
 		// initialize initial temperature
 		const double x = Lx * ((i + 0.5) / static_cast<double>(nx));
-		const double Erad = compute_exact_erad(x - x0, initial_time);
-		const double Frad = compute_exact_flux(x - x0, initial_time);
-		const double Trad = std::pow(Erad / a_rad, 1./4.);
+		const double Trad = compute_exact_Trad(x - x0, initial_time);
+		const double Erad = a_rad * std::pow(Trad, 4);
+		T_initial.at(i) = Trad;
 		Erad_initial.at(i) = Erad;
-		Frad_initial.at(i) = Frad;
-		T_eq.at(i) = Trad;
 	}
 
 	RadSystem<PulseProblem> rad_system(
 	    {.nx = nx, .lx = Lx, .cflNumber = CFL_number});
 
 	rad_system.set_radiation_constant(a_rad);
-	rad_system.set_c_light(c);
 	rad_system.set_lx(Lx);
+	rad_system.c_light_ = c;
+	rad_system.c_hat_ = chat;
 	rad_system.Erad_floor_ = Erad_floor;
-	rad_system.boltzmann_constant_ = (2./3.)*1.0e6;
+	rad_system.boltzmann_constant_ = (2./3.);
 	rad_system.mean_molecular_mass_ = 1.0;
 	rad_system.gamma_ = 5./3.;
 
-	const auto c_v = (rad_system.boltzmann_constant_ / rad_system.mean_molecular_mass_) /
-			 (rad_system.gamma_ - 1.);
-
 	auto nghost = rad_system.nghost();
 	for (int i = nghost; i < nx + nghost; ++i) {
-		rad_system.set_radEnergy(i) = Erad_initial.at(i - nghost);
-		rad_system.set_x1RadFlux(i) = Frad_initial.at(i - nghost);
+		rad_system.set_radEnergy(i) = Erad_floor;
+		rad_system.set_x1RadFlux(i) = 0.;
 		rad_system.set_radEnergySource(i) = 0.0;
 
-		rad_system.set_gasEnergy(i) = rad_system.ComputeEgasFromTgas(rho, T_eq.at(i - nghost));
+		rad_system.set_gasEnergy(i) = rad_system.ComputeEgasFromTgas(rho, T_initial.at(i - nghost));
 		rad_system.set_staticGasDensity(i) = rho;
 		rad_system.set_x1GasMomentum(i) = 0.0;
 	}
@@ -156,19 +154,26 @@ auto testproblem_radiation_pulse() -> int
 	std::cout << "radiation constant (code units) = " << a_rad << "\n";
 	std::cout << "c_light (code units) = " << c << "\n";
 	std::cout << "Lx = " << Lx << "\n";
-	std::cout << "initial_dt = " << initial_dt << "\n";
 	std::cout << "max_dt = " << max_dt << "\n";
 	std::cout << "initial time = " << initial_time << std::endl;
+	std::cout << "initial gas energy = " << Egas0 << std::endl;
+	std::cout << "initial radiation energy = " << Erad0 << "\n" << std::endl;
 
 	// Main time loop
 	int j;
+	double dt_prev = NAN;
 	for (j = 0; j < max_timesteps; ++j) {
 		if (rad_system.time() >= max_time) {
 			break;
 		}
 
-		const double this_dtMax = ((j == 0) ? initial_dt : max_dt);
-		rad_system.AdvanceTimestepRK2(this_dtMax);
+		// Compute timestep
+		const double dt_expand_fac = 1.2;
+		const double computed_dt = rad_system.ComputeTimestep(max_dt);
+		const double this_dt = std::min(computed_dt, dt_expand_fac*dt_prev);
+
+		rad_system.AdvanceTimestepRK2(this_dt);
+		dt_prev = this_dt;
 	}
 
 	std::cout << "Timestep " << j << "; t = " << rad_system.time()
@@ -193,7 +198,6 @@ auto testproblem_radiation_pulse() -> int
 	std::vector<double> Tgas(nx);
 	std::vector<double> Erad(nx);
 	std::vector<double> Egas(nx);
-	std::vector<double> x1RadFlux(nx);
 
 	for (int i = 0; i < nx; ++i) {
 		const double x = Lx * ((i + 0.5) / static_cast<double>(nx));
@@ -203,9 +207,8 @@ auto testproblem_radiation_pulse() -> int
 		const auto Trad_t = std::pow(Erad_t / a_rad, 1./4.);
 		Erad.at(i) = Erad_t;
 		Trad.at(i) = Trad_t;
-		Egas.at(i) = a_rad * rad_system.gasEnergy(i + nghost);
-		Tgas.at(i) = rad_system.gasEnergy(i + nghost) / (rho * c_v);
-		x1RadFlux.at(i) = rad_system.x1RadFlux(i + nghost);
+		Egas.at(i) = rad_system.gasEnergy(i + nghost);
+		Tgas.at(i) = rad_system.ComputeTgasFromEgas(rho, Egas.at(i));
 	}
 
 	// compute exact solution
@@ -216,8 +219,8 @@ auto testproblem_radiation_pulse() -> int
 	for (int i = 0; i < nx; ++i) {
 		const double x = Lx * ((i + 0.5) / static_cast<double>(nx));
 
-		auto Erad_val = compute_exact_erad(x - x0, initial_time + rad_system.time());
-		auto Trad_val = std::pow(Erad_val / a_rad, 1./4.);
+		auto Trad_val = compute_exact_Trad(x - x0, initial_time + rad_system.time());
+		auto Erad_val = a_rad * std::pow(Trad_val, 4);
 
 		xs_exact.push_back(x);
 		Trad_exact.push_back(Trad_val);
@@ -238,22 +241,22 @@ auto testproblem_radiation_pulse() -> int
 	std::cout << "Relative L1 error norm = " << rel_error << std::endl;
 
 	// plot energy density
-
 	std::map<std::string, std::string> Erad_args, Erad_exact_args, Erad_initial_args;
 	Erad_args["label"] = "Numerical solution";
 	Erad_args["color"] = "red";
 	Erad_args["linestyle"] = ":";
 	
 	Erad_exact_args["label"] = "Exact solution";
+	Erad_exact_args["linestyle"] = "-.";
 	Erad_exact_args["color"] = "blue";
 
 	Erad_initial_args["label"] = "initial condition";
 	Erad_initial_args["color"] = "black";
-	Erad_initial_args["linestyle"] = "-.";
+	Erad_initial_args["linestyle"] = ":";
 
+	matplotlibcpp::plot(xs, Erad_initial, Erad_initial_args);
 	matplotlibcpp::plot(xs, Erad, Erad_args);
 	matplotlibcpp::plot(xs_exact, Erad_exact, Erad_exact_args);
-	matplotlibcpp::plot(xs, Erad_initial, Erad_initial_args);
 
 	matplotlibcpp::xlabel("length x (dimensionless)");
 	matplotlibcpp::ylabel("radiation energy density (dimensionless)");
@@ -261,13 +264,21 @@ auto testproblem_radiation_pulse() -> int
 	matplotlibcpp::title(fmt::format("time ct = {:.4g}", initial_time + rad_system.time() * c));
 	matplotlibcpp::save("./radiation_pulse.pdf");
 
+	// plot temperature
 	matplotlibcpp::clf();
 
-	std::map<std::string, std::string> Trad_args, Tgas_args;
+	std::map<std::string, std::string> Trad_args, Tgas_args, Trad_exact_args, Tinit_args;
 	Trad_args["label"] = "radiation temperature";
+	Trad_args["linestyle"] = "-.";
 	Tgas_args["label"] = "gas temperature";
+	Tgas_args["linestyle"] = "--";
+	Trad_exact_args["label"] = "radiation temperature (exact)";
+	Trad_exact_args["linestyle"] = ":";
+	Tinit_args["label"] = "initial temperature";
+	matplotlibcpp::plot(xs, T_initial, Tinit_args);
 	matplotlibcpp::plot(xs, Trad, Trad_args);
 	matplotlibcpp::plot(xs, Tgas, Tgas_args);
+	matplotlibcpp::plot(xs, Trad_exact, Trad_exact_args);
 
 	matplotlibcpp::xlabel("length x (dimensionless)");
 	matplotlibcpp::ylabel("temperature (dimensionless)");
