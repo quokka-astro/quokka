2a8299cba782f2a9b6d49ce42141ff9b40cd109e
Benjamin Wibking <benjamin.wibking@anu.edu.au>
2021-06-08 15:53:34 +1000

    do not output as much

--- src/test_radiation_tophat.cpp
+++ src/test_radiation_tophat.cpp
@@ -326,7 +326,7 @@ auto testproblem_radiation_marshak_cgs() -> int
 	sim.cflNumber_ = CFL_number;
 	sim.maxTimesteps_ = max_timesteps;
 	sim.outputAtInterval_ = true;
-	sim.plotfileInterval_ = 10; // for debugging
+	sim.plotfileInterval_ = 100; // for debugging
 
 	// initialize
 	sim.setInitialConditions();
