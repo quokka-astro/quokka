84ee8fa2ec7191705e4c8a2fd3e11768bfc13db2
Benjamin Wibking <benjamin.wibking@anu.edu.au>
2021-08-24 09:15:45 +1000

    added MPI_Barrier to start of coarse ts

--- src/simulation.hpp
+++ src/simulation.hpp
@@ -363,6 +363,8 @@ template <typename problem_t> void AMRSimulation<problem_t>::evolve()
 
 	// Main time loop
 	for (int step = istep[0]; step < maxTimesteps_ && cur_time < stopTime_; ++step) {
+		amrex::ParallelDescriptor::Barrier(); // synchronize all MPI ranks
+		
 		if (suppress_output == 0) {
 			amrex::Print() << "\nCoarse STEP " << step + 1
 						   << " at t = " << cur_time << " ("
