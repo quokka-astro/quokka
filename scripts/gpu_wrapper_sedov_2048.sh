3831c5360422561bb72c047e26ea7de12bfbdecf
Benjamin Wibking <benjamin.wibking@anu.edu.au>
2021-10-04 13:05:34 +1100

    update paths for scaling test scripts

--- scripts/gpu_wrapper_sedov_2048.sh
+++ scripts/gpu_wrapper_sedov_2048.sh
@@ -2,4 +2,4 @@
 
 export CUDA_VISIBLE_DEVICES=$(($OMPI_COMM_WORLD_LOCAL_RANK % 4))
 #hpcrun -e gpu=nvidia --trace ./build/src/test_hydro3d_blast tests/blast_unigrid_2048.in
-./build/src/test_hydro3d_blast tests/blast_unigrid_2048.in
+./build/src/HydroBlast3D/test_hydro3d_blast tests/blast_unigrid_2048.in
--- scripts/gpu_wrapper_sedov_512.sh
+++ scripts/gpu_wrapper_sedov_512.sh
@@ -2,4 +2,4 @@
 
 export CUDA_VISIBLE_DEVICES=$(($OMPI_COMM_WORLD_LOCAL_RANK % 4))
 #hpcrun -e gpu=nvidia --trace ./build/src/test_hydro3d_blast tests/blast_unigrid_512.in amrex.async_out=1
-./build/src/test_hydro3d_blast tests/blast_unigrid_512.in amrex.async_out=1
+./build/src/HydroBlast3D/test_hydro3d_blast tests/blast_unigrid_512.in amrex.async_out=1
