f67b98abd2a1917906026338ece49966fa309ebc
Benjamin Wibking <benjamin.wibking@anu.edu.au>
2021-11-26 10:04:27 +1100

    disable CUDA-aware MPI by default (about 1.7x slower if used)

--- scripts/gpu_wrapper_sedov_512.sh
+++ scripts/gpu_wrapper_sedov_512.sh
@@ -2,5 +2,5 @@
 
 export CUDA_VISIBLE_DEVICES=$(($OMPI_COMM_WORLD_LOCAL_RANK % 4))
 #hpcrun -e gpu=nvidia --trace ./build/src/test_hydro3d_blast tests/blast_unigrid_512.in amrex.async_out=1
-./build/src/HydroBlast3D/test_hydro3d_blast tests/blast_unigrid_512.in amrex.async_out=1 amrex.use_gpu_aware_mpi=1
+./build/src/HydroBlast3D/test_hydro3d_blast tests/blast_unigrid_512.in amrex.async_out=1 amrex.use_gpu_aware_mpi=0
 
