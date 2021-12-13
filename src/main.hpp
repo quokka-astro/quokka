6fb14de27e68a211a0b5dee030e7fb559598b234
Benjamin Wibking <benjamin.wibking@anu.edu.au>
2021-12-13 11:36:18 +1100

    prevent compiling with the EDG-based (buggy) Intel compiler

--- src/main.hpp
+++ src/main.hpp
@@ -8,6 +8,10 @@
 /// \file main.hpp
 ///
 
+#if defined(__INTEL_COMPILER)
+#error "Quokka cannot be compiled with Intel Compiler Classic! Use the newer LLVM-based Intel compilers (icx/icpx) instead by adding the following CMake command-line options: -DCMAKE_C_COMPILER=icx -DCMAKE_CXX_COMPILER=icpx"
+#endif // __INTEL_COMPILER
+
 // function declarations
 auto main(int argc, char **argv) -> int;
 auto problem_main() -> int; // defined in problem generator
