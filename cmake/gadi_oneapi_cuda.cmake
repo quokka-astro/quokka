4ef3b1f1006445bfb4958599c321f5328994014f
Benjamin Wibking <benjamin.wibking@anu.edu.au>
2022-01-27 16:02:39 +1100

    added cmake cache file for Gadi

---
+++ cmake/gadi_oneapi_cuda.cmake
@@ -0,0 +1,9 @@
+# host configuration
+# run with `cmake -C host_config.cmake ..` from inside build directory
+
+set(CMAKE_C_COMPILER "icx" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "icpx" CACHE PATH "")
+set(AMReX_DIFFERENT_COMPILER ON CACHE BOOL "")
+set(AMReX_GPU_BACKEND CUDA CACHE STRING "")
+set(CMAKE_CUDA_ARCHITECTURES 70 80 CACHE STRING "")
+option(QUOKKA_PYTHON OFF)
