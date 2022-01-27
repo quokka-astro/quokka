4079b5d257b781f65f9a61712ee982bd3e6e9d52
Benjamin Wibking <benjamin.wibking@anu.edu.au>
2022-01-27 14:01:23 +1100

    add cmake machine-specific files for avatar

---
+++ cmake/avatar_nvhpc_cuda.cmake
@@ -0,0 +1,8 @@
+# host configuration
+# run with `cmake -C host_config.cmake ..` from inside build directory
+
+set(CMAKE_C_COMPILER "nvc" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "nvc++" CACHE PATH "")
+set(AMReX_DIFFERENT_COMPILER ON CACHE BOOL "")
+set(AMReX_GPU_BACKEND CUDA CACHE STRING "")
+set(CMAKE_CUDA_ARCHITECTURES 70 CACHE STRING "")
