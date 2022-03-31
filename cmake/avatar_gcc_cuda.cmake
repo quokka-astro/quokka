73f4a3fb561b92c64b25349a961cc2a3e5c62f18
Benjamin Wibking <benjamin.wibking@anu.edu.au>
2022-03-31 19:54:27 +1100

    update CMake build settings files

--- cmake/avatar_gcc_cuda.cmake
+++ cmake/avatar_gcc_cuda.cmake
@@ -3,6 +3,7 @@
 
 set(CMAKE_C_COMPILER "/opt/rh/gcc-toolset-11/root/usr/bin/gcc" CACHE PATH "")
 set(CMAKE_CXX_COMPILER "/opt/rh/gcc-toolset-11/root/usr/bin/g++" CACHE PATH "")
+set(CMAKE_CUDA_COMPILER "nvcc" CACHE PATH "")
 set(AMReX_DIFFERENT_COMPILER OFF CACHE BOOL "")
 set(AMReX_GPU_BACKEND CUDA CACHE STRING "")
 set(CMAKE_CUDA_ARCHITECTURES 70 CACHE STRING "")
