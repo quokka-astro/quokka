87f3d6738cc0c05b258339a4f98cd3ef6dfeef8f
Benjamin Wibking <benjamin.wibking@anu.edu.au>
2022-04-15 11:13:32 +1000

    use system gcc on avatar

--- cmake/avatar_gcc_cuda.cmake
+++ cmake/avatar_gcc_cuda.cmake
@@ -1,8 +1,8 @@
 # host configuration
 # run with `cmake -C host_config.cmake ..` from inside build directory
 
-set(CMAKE_C_COMPILER "/opt/rh/gcc-toolset-11/root/usr/bin/gcc" CACHE PATH "")
-set(CMAKE_CXX_COMPILER "/opt/rh/gcc-toolset-11/root/usr/bin/g++" CACHE PATH "")
+set(CMAKE_C_COMPILER "gcc" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "g++" CACHE PATH "")
 set(CMAKE_CUDA_COMPILER "nvcc" CACHE PATH "")
 set(AMReX_DIFFERENT_COMPILER OFF CACHE BOOL "")
 set(AMReX_GPU_BACKEND CUDA CACHE STRING "")
