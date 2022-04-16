e0dc45cfc43b112131d5c83a7c38e725a32b2090
Ben Wibking <benjamin.wibking@anu.edu.au>
2022-04-16 16:35:37 +1000

    allow building device code with Clang (#42)
    * disable -Wno-unknown-pragmas
    
    * fixes to allow building with CUDA Clang
    
    * add CMake options file for building on Avatar with Clang
    
    * update CMake build settings files
    
    * add option to disable/enable FMAD on device
    
    * add -ffp-contract to host options
    
    * avoid __managed__ variables
    
    * move azure pipelines to .ci/

--- cmake/avatar_gcc_cuda.cmake
+++ cmake/avatar_gcc_cuda.cmake
@@ -1,8 +1,9 @@
 # host configuration
 # run with `cmake -C host_config.cmake ..` from inside build directory
 
-set(CMAKE_C_COMPILER "/usr/bin/gcc" CACHE PATH "")
-set(CMAKE_CXX_COMPILER "/usr/bin/g++" CACHE PATH "")
+set(CMAKE_C_COMPILER "/opt/rh/gcc-toolset-11/root/usr/bin/gcc" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "/opt/rh/gcc-toolset-11/root/usr/bin/g++" CACHE PATH "")
+set(CMAKE_CUDA_COMPILER "nvcc" CACHE PATH "")
 set(AMReX_DIFFERENT_COMPILER OFF CACHE BOOL "")
 set(AMReX_GPU_BACKEND CUDA CACHE STRING "")
 set(CMAKE_CUDA_ARCHITECTURES 70 CACHE STRING "")
--- cmake/gadi_llvm_cuda.cmake
+++ cmake/gadi_llvm_cuda.cmake
@@ -1,9 +1,9 @@
 # host configuration
 # run with `cmake -C host_config.cmake ..` from inside build directory
 
-set(CMAKE_C_COMPILER "icx" CACHE PATH "")
-set(CMAKE_CXX_COMPILER "icpx" CACHE PATH "")
-set(CMAKE_CUDA_COMPILER "nvcc" CACHE PATH "")
+set(CMAKE_C_COMPILER "clang" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "clang++" CACHE PATH "")
+set(CMAKE_CUDA_COMPILER "clang" CACHE PATH "")
 set(AMReX_DIFFERENT_COMPILER ON CACHE BOOL "")
 set(AMReX_GPU_BACKEND CUDA CACHE STRING "")
 set(CMAKE_CUDA_ARCHITECTURES 70 80 CACHE STRING "")
