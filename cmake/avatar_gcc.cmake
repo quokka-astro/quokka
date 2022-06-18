0b3f33465a1eaa0add0474f4009f0905f88d681e
Benjamin Wibking <benjamin.wibking@anu.edu.au>
2022-06-18 13:48:01 +1000

    update ascent settings

--- cmake/avatar_gcc.cmake
+++ cmake/avatar_gcc.cmake
@@ -2,11 +2,9 @@
 ## run with `cmake -C host_config.cmake ..` from inside build directory
 ##
 ## for ascent support:
-##   Ascent_DIR=../../ascent/install cmake -C ../cmake/avatar_gcc_cuda.cmake .. -G Ninja
+##   Ascent_DIR=../../ascent/install cmake -C ../cmake/avatar_gcc.cmake .. -G Ninja
 
 set(CMAKE_C_COMPILER "gcc" CACHE PATH "")
 set(CMAKE_CXX_COMPILER "g++" CACHE PATH "")
-set(CMAKE_CUDA_COMPILER "nvcc" CACHE PATH "")
 set(AMReX_DIFFERENT_COMPILER OFF CACHE BOOL "")
-set(AMReX_GPU_BACKEND CUDA CACHE STRING "")
-set(CMAKE_CUDA_ARCHITECTURES 70 CACHE STRING "")
+set(AMReX_GPU_BACKEND NONE CACHE STRING "")
