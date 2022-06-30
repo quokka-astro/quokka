1c815591f5a381ab4c6f64f3b1ee73a3724b4044
Benjamin Wibking <benjamin.wibking@anu.edu.au>
2022-06-30 14:51:11 +1000

    add execution space annotations

--- src/FastMath.hpp
+++ src/FastMath.hpp
@@ -18,16 +18,18 @@
 // https://github.com/lanl/singularity-eos
 
 #include "AMReX_Extension.H"
+#include "AMReX_GpuQualifiers.H"
 #include <cassert>
 #include <cmath>
 
 namespace FastMath {
 
-AMREX_FORCE_INLINE
+AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE
 auto as_int(double f) {
   return *reinterpret_cast<int64_t*>(&f); // NOLINT
 }
-AMREX_FORCE_INLINE
+
+AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE
 auto as_double(int64_t i) {
   return *reinterpret_cast<double*>(&i); // NOLINT
 }
@@ -35,14 +37,14 @@ auto as_double(int64_t i) {
 // Reference implementations, however the integer cast implementation
 // below is probably faster.
 /*
-AMREX_FORCE_INLINE
+AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE
 double lg(const double x) {
   int n;
   assert(x > 0 && "log divergent for x <= 0");
   const double y = frexp(x, &n);
   return 2 * (y - 1) + n;
 }
-AMREX_FORCE_INLINE
+AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE
 double pow2(const double x) {
   const int flr = std::floor(x);
   const double remainder = x - flr;
@@ -52,7 +54,7 @@ double pow2(const double x) {
 }
 */
 
-AMREX_FORCE_INLINE
+AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE
 auto lg(const double x) -> double {
   // Magic numbers constexpr because C++ doesn't constexpr reinterpret casts
   // these are floating point numbers as reinterpreted as integers.
@@ -63,7 +65,7 @@ auto lg(const double x) -> double {
   return static_cast<double>(as_int(x) - one_as_int) * scale_down;
 }
 
-AMREX_FORCE_INLINE
+AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE
 auto pow2(const double x) -> double {
   // Magic numbers constexpr because C++ doesn't constexpr reinterpret casts
   // these are floating point numbers as reinterpreted as integers.
@@ -74,31 +76,31 @@ auto pow2(const double x) -> double {
   return as_double(static_cast<int64_t>(x*scale_up) + one_as_int);
 }
 
-AMREX_FORCE_INLINE
+AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE
 auto ln(const double x) -> double {
   constexpr double ILOG2E = 0.6931471805599453;
   return ILOG2E * lg(x);
 }
 
-AMREX_FORCE_INLINE
+AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE
 auto exp(const double x) -> double {
   constexpr double LOG2E = 1.4426950408889634;
   return pow2(LOG2E * x);
 }
 
-AMREX_FORCE_INLINE
+AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE
 auto log10(const double x) -> double {
   constexpr double LOG2OLOG10 = 0.301029995663981195;
   return LOG2OLOG10 * lg(x);
 }
 
-AMREX_FORCE_INLINE
+AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE
 auto pow10(const double x) -> double {
   constexpr double LOG10OLOG2 = 3.321928094887362626;
   return pow2(LOG10OLOG2 * x);
 }
 
-AMREX_FORCE_INLINE
+AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE
 auto tanh(const double x) -> double {
   const double expx = exp(2 * x);
   return (expx - 1) / (expx + 1);
