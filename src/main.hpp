c96b0c3b4b9ad14c4c65c3b5a6a9eb2c79df99d7
Benjamin Wibking <benjamin.wibking@anu.edu.au>
2022-07-07 11:36:35 +1000

    add suggested ASAN_OPTIONS to CMakeLists

--- src/main.hpp
+++ src/main.hpp
@@ -12,10 +12,6 @@
 #error "Quokka cannot be compiled with Intel Compiler Classic! Use the newer LLVM-based Intel compilers (icx/icpx) instead by adding the following CMake command-line options: -DCMAKE_C_COMPILER=icx -DCMAKE_CXX_COMPILER=icpx"
 #endif // __INTEL_COMPILER
 
-// disable LeakSanitizer
-// (it has too many leaks reported from external libraries that we can't control)
-const char* __asan_default_options() { return "detect_leaks=0"; }
-
 // function declarations
 auto main(int argc, char **argv) -> int;
 auto problem_main() -> int; // defined in problem generator
