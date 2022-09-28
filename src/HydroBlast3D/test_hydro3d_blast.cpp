d9a27f093a24783ae2454b733fcaf9920057943e
Benjamin Wibking <wibkingb@msu.edu>
2022-09-28 11:37:31 -0500

    updated delta cmake config

--- src/HydroBlast3D/test_hydro3d_blast.cpp
+++ src/HydroBlast3D/test_hydro3d_blast.cpp
@@ -188,8 +188,6 @@ auto problem_main() -> int {
   sim.reconstructionOrder_ = 3; // 2=PLM, 3=PPM
   sim.stopTime_ = 0.5;   // 0.01;
   sim.cflNumber_ = 0.25; // *must* be less than 1/3 in 3D!
-  sim.maxTimesteps_ = 100;
-  sim.plotfileInterval_ = -1;
   //sim.maxTimesteps_ = 10000;
   //sim.plotfileInterval_ = 100;
 
