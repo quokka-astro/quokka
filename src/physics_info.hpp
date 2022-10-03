c6e7a619445335697a3917473a05004691134739
Neco Kriel <necokriel@gmail.com>
2022-10-03 17:45:32 +1100

    add: compile-time calc. num. of cc quantities used in sim.

--- src/physics_info.hpp
+++ src/physics_info.hpp
@@ -5,16 +5,19 @@
 
 // this struct is specialized by the user application code.
 template <typename problem_t> struct Physics_Traits {
+  // cell-centred
   static constexpr bool is_hydro_enabled = false;
-  static constexpr bool is_radiation_enabled = false;
   static constexpr bool is_chemistry_enabled = false;
-
   static constexpr int numPassiveScalars = 0;
+  static constexpr bool is_radiation_enabled = false;
 };
 
 // this struct stores the indices at which quantities start
 template <typename problem_t> struct Physics_Indices {
   // cell-centered quantities
+  static const int nvarTotal_cc = Physics_NumVars::numHydroVars
+        + Physics_Traits<problem_t>::numPassiveScalars
+        + static_cast<int>(Physics_Traits<problem_t>::is_radiation_enabled) * Physics_NumVars::numRadVars;
   static const int hydroFirstIndex = 0;
   static const int pscalarFirstIndex = Physics_NumVars<problem_t>::numHydroVars;
   static const int radFirstIndex = pscalarFirstIndex + Physics_Traits<problem_t>::numPassiveScalars;
