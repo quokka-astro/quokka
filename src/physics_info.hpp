ccc26f64ca13cf153dd06a4cf4d46dc16112d5d9
Neco Kriel <necokriel@gmail.com>
2022-10-04 16:41:22 +1100

    add ability to track the number of face-centred qunatities

--- src/physics_info.hpp
+++ src/physics_info.hpp
@@ -10,17 +10,23 @@ template <typename problem_t> struct Physics_Traits {
   static constexpr bool is_chemistry_enabled = false;
   static constexpr int numPassiveScalars = 0;
   static constexpr bool is_radiation_enabled = false;
+  // face-centred
+  static constexpr bool is_mhd_enabled = false;
 };
 
 // this struct stores the indices at which quantities start
 template <typename problem_t> struct Physics_Indices {
-  // cell-centered quantities
+  // cell-centered
   static const int nvarTotal_cc = Physics_NumVars::numHydroVars
         + Physics_Traits<problem_t>::numPassiveScalars
         + static_cast<int>(Physics_Traits<problem_t>::is_radiation_enabled) * Physics_NumVars::numRadVars;
   static const int hydroFirstIndex = 0;
   static const int pscalarFirstIndex = Physics_NumVars::numHydroVars;
   static const int radFirstIndex = pscalarFirstIndex + Physics_Traits<problem_t>::numPassiveScalars;
+  // face-centered
+  static const int nvarTotal_fc =
+        static_cast<int>(Physics_Traits<problem_t>::is_mhd_enabled) * Physics_NumVars::numMHDVars; // total number of quantities for each face-centering
+  static const int mhdFirstIndex = 0;
 };
 
 #endif // PHYSICS_INFO_HPP_
--- src/physics_numVars.hpp
+++ src/physics_numVars.hpp
@@ -2,8 +2,11 @@
 #define PHYSICS_NUMVARS_HPP_
 
 struct Physics_NumVars {
+  // cell-centred
   static const int numHydroVars = 6;
   static const int numRadVars = 4;
+  // face-centred (specify the number of quantities stored per spatial dimension, i.e., each face-centering)
+  static const int numMHDVars = 1;
 };
 
 #endif // PHYSICS_NUMVARS_HPP_
