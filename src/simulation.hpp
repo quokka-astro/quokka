149a2043962c2a6c9a34c6b6fbe7a943518fbaa2
Ben Wibking <wibkingb@msu.edu>
2022-10-11 15:02:32 -0400

    disable maxSubcycle check

--- src/simulation.hpp
+++ src/simulation.hpp
@@ -127,7 +127,7 @@ public:
   amrex::Real stopTime_ = 1.0;  // default
   amrex::Real cflNumber_ = 0.3; // default
   amrex::Real dtToleranceFactor_ = 1.1; // default
-  amrex::Long maxSubcyclingFactor_ = 16; // default
+  amrex::Long maxSubcyclingFactor_ = 2048; // default
   amrex::Long cycleCount_ = 0;
   amrex::Long maxTimesteps_ = 1e4; // default
   amrex::Long maxWalltime_ = 0;    // default: no limit
@@ -794,7 +794,7 @@ auto AMRSimulation<problem_t>::timeStepWithSubcycling(int lev, amrex::Real time,
         factor *= 2;
         dtCur *= 0.5;
       }
-
+#if 0
       // check if we have reduced the timestep by too large of a factor
       if (factor > maxSubcyclingFactor_) {
         amrex::Print() << "The CFL timestep on level " << i << "is: " << dtCFL << "\n";
@@ -803,7 +803,7 @@ auto AMRSimulation<problem_t>::timeStepWithSubcycling(int lev, amrex::Real time,
         amrex::Print() << "This exceeds maxSubcyclingFactor. Probably something has gone wrong!\n";
         amrex::Abort("The CFL-limited timestep has become too small! Aborting...");
       }
-
+#endif
       if (factor > maxFactorSublevels) {
         maxFactorSublevels = factor;
       }
