0b799682a05ccf965f34ba3e675c2344dfca144a
Ben Wibking <ben@wibking.com>
2022-10-15 20:47:48 +0000

    add stream synchronize after each stage

--- src/RadhydroSimulation.hpp
+++ src/RadhydroSimulation.hpp
@@ -716,6 +716,7 @@ void RadhydroSimulation<problem_t>::advanceHydroAtLevel(int lev, amrex::Real tim
 			incrementFluxRegisters(fr_as_crse, fr_as_fine, fluxArrays, lev, fluxScaleFactor * dt_lev);
 		}
 	}
+	amrex::Gpu::streamSynchronizeAll();
 
 	// Stage 2 of RK2-SSP
 	{
@@ -752,6 +753,7 @@ void RadhydroSimulation<problem_t>::advanceHydroAtLevel(int lev, amrex::Real tim
 			incrementFluxRegisters(fr_as_crse, fr_as_fine, fluxArrays, lev, fluxScaleFactor * dt_lev);
 		}
 	}
+	amrex::Gpu::streamSynchronizeAll();
 
 	// do Strang split source terms (second half-step)
 	if (Verbose()) {
