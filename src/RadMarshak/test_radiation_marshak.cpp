131065f2c08c295a68380d3e7a5d5b96d7557aa8
Neco Kriel <43261821+AstroKriel@users.noreply.github.com>
2022-10-21 04:47:51 +1100

    Update input argument to user initialisation function (#129)
    * fix init-ing bug: now init-ing all state varaibles (for running in debug mode)
    
    * Delete highmach_output.csv
    
    * update gitignore to ignore extraneous (output or testing) files
    
    * remove testing files
    
    * remove debugging code
    
    * update: only pass single grid element to user-initialisation function.
    
    * remove unused vector
    
    * update: refer directly to grid elem passed to func.
    
    * remove extra git ignore line
    
    * revert git ignore to original version
    
    * undo edits to gitignore (reitroduce duplicate entry)
    
    * add empty line in gitignore as in ben/quokka/development/gitignore
    
    * remove init. of (unused) vector of grid elems

--- src/RadMarshak/test_radiation_marshak.cpp
+++ src/RadMarshak/test_radiation_marshak.cpp
@@ -157,9 +157,9 @@ AMRSimulation<SuOlsonProblem>::setCustomBoundaryConditions(
 
 template <>
 void RadhydroSimulation<SuOlsonProblem>::setInitialConditionsOnGrid(
-    std::vector<quokka::grid> &grid_vec) {
-  const amrex::Box &indexRange = grid_vec[0].indexRange;
-  const amrex::Array4<double>& state_cc = grid_vec[0].array;
+    quokka::grid grid_elem) {
+  const amrex::Box &indexRange = grid_elem.indexRange;
+  const amrex::Array4<double>& state_cc = grid_elem.array;
   
   // loop over the grid and set the initial condition
   amrex::ParallelFor(indexRange, [=] AMREX_GPU_DEVICE(int i, int j, int k) {
