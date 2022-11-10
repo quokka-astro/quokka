2064464f77e9513a3451aeeee9edf5d5c74178c8
Neco Kriel <43261821+AstroKriel@users.noreply.github.com>
2022-11-10 15:09:05 +1100

    update order of imported libraries in src/simulation.hpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/simulation.hpp
+++ src/simulation.hpp
@@ -64,10 +64,10 @@
 
 // internal headers
 #include "CheckNaN.hpp"
+#include "MFInterpolater.H"
 #include "grid.hpp"
 #include "math_impl.hpp"
 #include "physics_info.hpp"
-#include "MFInterpolater.H"
 
 #define USE_YAFLUXREGISTER
 
