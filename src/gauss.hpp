09d11d0df58507fba752793aa200557565fb3ef8
Ben Wibking <ben@wibking.com>
2022-11-11 18:20:07 -0500

    Update src/gauss.hpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/gauss.hpp
+++ src/gauss.hpp
@@ -955,7 +955,7 @@ public:
    template <class F>
    AMREX_GPU_DEVICE static auto integrate(F f, Real a, Real b, Real* pL1 = nullptr)->decltype(std::declval<F>()(std::declval<Real>()))
    {
-      typedef decltype(f(a)) K;
+      using K = decltype(f(a));
       static const char* function = "boost::math::quadrature::gauss<%1%>::integrate(f, %1%, %1%)";
       if (!(std::isnan)(a) && !(std::isnan)(b))
       {
