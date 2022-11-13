44c496486846635dbb352c73b8a1063147841823
Ben Wibking <ben@wibking.com>
2022-11-13 18:29:29 -0500

    use SimulationData for problem-specific data (#180)
    * add SimulationData member variable
    
    * use SimulationData instead of problem-specific member variables
    
    * update ShockCloud
    
    * update cooling test

---
+++ src/SimulationData.hpp
@@ -0,0 +1,16 @@
+#ifndef SIMULATION_DATA_HPP_ // NOLINT
+#define SIMULATION_DATA_HPP_
+//==============================================================================
+// TwoMomentRad - a radiation transport library for patch-based AMR codes
+// Copyright 2020 Benjamin Wibking.
+// Released under the MIT license. See LICENSE file included in the GitHub repo.
+//==============================================================================
+/// \file SimulationData.hpp
+/// \brief Defines a template class for user-specified simulation data
+
+template <typename problem_t> struct SimulationData
+{
+	// none
+};
+
+#endif // SIMULATION_DATA_HPP_
