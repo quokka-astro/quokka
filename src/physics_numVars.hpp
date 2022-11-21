01040d7107cc9a8f9b6f149450774d469ef17a91
neco kriel <necokriel@gmail.com>
2022-11-21 16:05:22 +1100

    fc quantities are defined in each space-dimension

--- src/physics_numVars.hpp
+++ src/physics_numVars.hpp
@@ -1,12 +1,14 @@
 #ifndef PHYSICS_NUMVARS_HPP_ // NOLINT
 #define PHYSICS_NUMVARS_HPP_
 
+#include <AMReX.H>
+
 struct Physics_NumVars {
   // cell-centred
   static const int numHydroVars = 6;
   static const int numRadVars = 4;
-  // face-centred (specify the number of quantities stored per spatial dimension, i.e., each face-centering)
-  static const int numMHDVars = 1;
+  // face-centred
+  static const int numMHDVars = AMREX_SPACEDIM;
 };
 
 #endif // PHYSICS_NUMVARS_HPP_
