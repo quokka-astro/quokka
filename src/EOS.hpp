a82728f45ad12282f320373896a82114dce60062
Ben Wibking <ben@wibking.com>
2022-12-06 22:14:09 +0000

    add execution space annotations for GPU

--- src/EOS.hpp
+++ src/EOS.hpp
@@ -39,7 +39,8 @@ template <typename problem_t> class EOS
 	static constexpr amrex::Real mean_molecular_weight_ = EOS_Traits<problem_t>::mean_molecular_weight;
 };
 
-template <typename problem_t> auto EOS<problem_t>::ComputeTgasFromEint(amrex::Real rho, amrex::Real Eint) -> amrex::Real
+template <typename problem_t>
+AMREX_FORCE_INLINE AMREX_GPU_HOST_DEVICE auto EOS<problem_t>::ComputeTgasFromEint(amrex::Real rho, amrex::Real Eint) -> amrex::Real
 {
 	// return temperature for an ideal gas
 	amrex::Real Tgas = NAN;
@@ -50,7 +51,8 @@ template <typename problem_t> auto EOS<problem_t>::ComputeTgasFromEint(amrex::Re
 	return Tgas;
 }
 
-template <typename problem_t> AMREX_GPU_HOST_DEVICE auto EOS<problem_t>::ComputeEintFromTgas(amrex::Real rho, amrex::Real Tgas) -> amrex::Real
+template <typename problem_t>
+AMREX_FORCE_INLINE AMREX_GPU_HOST_DEVICE auto EOS<problem_t>::ComputeEintFromTgas(amrex::Real rho, amrex::Real Tgas) -> amrex::Real
 {
 	// return internal energy density for a gamma-law ideal gas
 	amrex::Real Eint = NAN;
@@ -62,7 +64,7 @@ template <typename problem_t> AMREX_GPU_HOST_DEVICE auto EOS<problem_t>::Compute
 }
 
 template <typename problem_t>
-AMREX_GPU_HOST_DEVICE auto EOS<problem_t>::ComputeEintTempDerivative(const amrex::Real rho, const amrex::Real /*Tgas*/) -> amrex::Real
+AMREX_FORCE_INLINE AMREX_GPU_HOST_DEVICE auto EOS<problem_t>::ComputeEintTempDerivative(const amrex::Real rho, const amrex::Real /*Tgas*/) -> amrex::Real
 {
 	// compute derivative of internal energy w/r/t temperature
 	amrex::Real dEint_dT = NAN;
