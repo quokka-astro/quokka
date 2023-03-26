df1beb18dc89943d86406222755ef9c6e0188a82
psharda <piyush.suneet@gmail.com>
2023-03-26 21:34:57 +1100

    added test_primordial_chem.hpp file

---
+++ src/PrimordialChem/test_primordial_chem.hpp
@@ -0,0 +1,24 @@
+#ifndef TEST_PRIMORDIAL_CHEM_HPP_ // NOLINT
+#define TEST_PRIMORDIAL_CHEM_HPP_
+//==============================================================================
+// TwoMomentRad - a radiation transport library for patch-based AMR codes
+// Copyright 2020 Benjamin Wibking.
+// Released under the MIT license. See LICENSE file included in the GitHub repo.
+//==============================================================================
+/// \file test_primordial_chem.cpp
+/// \brief Defines a test problem for primordial chemistry.
+///
+
+// external headers
+#ifdef HAVE_PYTHON
+#include "matplotlibcpp.h"
+#endif
+#include <fmt/format.h>
+
+// internal headers
+
+#include "linear_advection.hpp"
+
+// function definitions
+
+#endif // TEST_PRIMORDIAL_CHEM_HPP_
