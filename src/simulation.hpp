2085a469baec03f5649510791db46e9a5f1cba10
Aditi Vijayan <aditi.vijayan@anu.edu.au>
2023-04-11 14:21:39 +1000

    makign restarting with higher CPUs more efficient

--- src/simulation.hpp
+++ src/simulation.hpp
@@ -1867,13 +1867,33 @@ template <typename problem_t> void AMRSimulation<problem_t>::ReadCheckpointFile(
 		}
 	}
 
-	amrex::Vector<amrex::MultiFab>  tmp;
-
+	
 	for (int lev = 0; lev <= finest_level; ++lev) {
 		// read in level 'lev' BoxArray from Header
 		amrex::BoxArray ba;
 		ba.readFrom(is);
 		GotoNextLine(is);
+
+		if(lev==0){
+			amrex::IntVect fac(2);
+			amrex::IntVect domlo(0,0,0);
+			amrex::IntVect domhi(ba[ba.size()-1].bigEnd(0), ba[ba.size()-1].bigEnd(1), ba[ba.size()-1].bigEnd(2));
+			amrex::Box dom(domlo, domhi);
+			amrex::Box dom2 = amrex::refine(amrex::coarsen(dom,2),2);
+			for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
+				if (dom.length(idim) != dom2.length(idim)) {
+					fac[idim] = 1;
+				}
+			}
+			amrex::BoxArray ba_lev0(amrex::coarsen(dom,fac));
+			ba_lev0.maxSize(max_grid_size[0]/fac);
+			ba_lev0.refine(fac);
+			// Boxes in ba have even number of cells in each direction
+			// unless the domain has odd number of cells in that direction.
+			ChopGrids(0, ba_lev0, amrex::ParallelDescriptor::NProcs());
+			ba == ba_lev0;
+
+		}
 
 		// create a distribution mapping
 		amrex::DistributionMapping dm{ba, amrex::ParallelDescriptor::NProcs()};
@@ -1886,7 +1906,6 @@ template <typename problem_t> void AMRSimulation<problem_t>::ReadCheckpointFile(
 		const int ncomp_cc = Physics_Indices<problem_t>::nvarTotal_cc;
 		const int nghost_cc = nghost_cc_;
 		state_old_cc_[lev].define(grids[lev], dmap[lev], ncomp_cc, nghost_cc);
-		// tmp[lev].define(grids[lev], dmap[lev], ncomp_cc, nghost_cc);
 		state_new_cc_[lev].define(grids[lev], dmap[lev], ncomp_cc, nghost_cc);
 		max_signal_speed_[lev].define(ba, dm, 1, nghost_cc);
 
@@ -1910,7 +1929,14 @@ template <typename problem_t> void AMRSimulation<problem_t>::ReadCheckpointFile(
 	// read in the MultiFab data
 	for (int lev = 0; lev <= finest_level; ++lev) {
 		// cell-centred
+		if(lev==0){
+			amrex::MultiFab  tmp;  
+            amrex::VisMF::Read( tmp, amrex::MultiFabFileFullPrefix(lev, restart_chkfile, "Level_", "Cell"));   
+		    state_new_cc_[0].ParallelCopy(tmp, 0, 0, Physics_Indices<problem_t>::nvarTotal_cc, nghost_cc_, nghost_cc_);
+		}
+		else{
 		amrex::VisMF::Read(state_new_cc_[lev], amrex::MultiFabFileFullPrefix(lev, restart_chkfile, "Level_", "Cell"));
+		}
 		// face-centred
 		if constexpr (Physics_Indices<problem_t>::nvarTotal_fc > 0) {
 			for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
