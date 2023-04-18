69eaf0b32e5cfc59f5eab06d17541b189ea8781d
psharda <piyush.suneet@gmail.com>
2023-04-18 20:35:58 +0200

    added comment on generalizing charge consv

--- src/Chemistry.hpp
+++ src/Chemistry.hpp
@@ -88,6 +88,7 @@ template <typename problem_t> void computeChemistry(amrex::MultiFab &mf, const R
 			}
 
 			// update the number density of electrons due to charge conservation
+			// TODO(psharda): generalize this to other chem networks
 			chemstate.xn[0] = -chemstate.xn[3] - chemstate.xn[7] + chemstate.xn[1] + chemstate.xn[12] + chemstate.xn[6] + chemstate.xn[4] +
 					  chemstate.xn[9] + 2.0 * chemstate.xn[11];
 
