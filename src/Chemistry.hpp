0a8d8892b6f1027b4841b874dabd6384b02146aa
psharda <piyush.suneet@gmail.com>
2023-04-25 18:23:16 +0200

    fixed call to chemburner

--- src/Chemistry.hpp
+++ src/Chemistry.hpp
@@ -74,7 +74,7 @@ template <typename problem_t> void computeChemistry(amrex::MultiFab &mf, const R
 			// do the actual integration
 			// do it in .cpp so that it is not built at compile time for all tests
 			// which would otherwise slow down compilation due to the large RHS file
-			void chemburner(burn_t & chemstate, const Real dt);
+			chemburner(chemstate, dt);
 
 			if (!chemstate.success) {
 				amrex::Abort("VODE integration was unsuccessful!");
