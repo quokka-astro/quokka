a3930798a638a0c4613abcd24ad2821f094de44d
psharda <piyush.suneet@gmail.com>
2023-05-05 09:30:54 +0200

    added Shocktube CMA test

---
+++ src/HydroShocktubeCMA/test_hydro_shocktube_cma.hpp
@@ -0,0 +1,26 @@
+#ifndef TEST_HYDRO_SHOCKTUBE_CMA_HPP_ // NOLINT
+#define TEST_HYDRO_SHOCKTUBE_CMA_HPP_
+//==============================================================================
+// TwoMomentRad - a radiation transport library for patch-based AMR codes
+// Copyright 2020 Benjamin Wibking.
+// Released under the MIT license. See LICENSE file included in the GitHub repo.
+//==============================================================================
+/// \file test_hydro_shocktube_CMA.hpp
+/// \brief Defines a test problem for a shock tube with passive scalars using consistent molecular advection (CMA).
+///
+
+// external headers
+#ifdef HAVE_PYTHON
+#include "matplotlibcpp.h"
+#endif
+#include <fmt/format.h>
+#include <fstream>
+
+// internal headers
+
+#include "hydro_system.hpp"
+#include "interpolate.hpp"
+
+// function definitions
+
+#endif // TEST_HYDRO_SHOCKTUBE_CMA_HPP_
