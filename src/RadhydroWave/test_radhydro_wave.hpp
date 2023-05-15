bd99213b117658003dfda510d285127fd91f4300
Ben Wibking <ben@wibking.com>
2023-05-15 15:17:02 -0400

    initial commit

---
+++ src/RadhydroWave/test_radhydro_wave.hpp
@@ -0,0 +1,23 @@
+#ifndef TEST_HYDRO_WAVE_HPP_ // NOLINT
+#define TEST_HYDRO_WAVE_HPP_
+//==============================================================================
+// TwoMomentRad - a radiation transport library for patch-based AMR codes
+// Copyright 2020 Benjamin Wibking.
+// Released under the MIT license. See LICENSE file included in the GitHub repo.
+//==============================================================================
+/// \file test_hydro_wave.hpp
+/// \brief Defines a test problem for a linear hydro wave.
+///
+
+// external headers
+#ifdef HAVE_PYTHON
+#include "matplotlibcpp.h"
+#endif
+#include <fmt/format.h>
+
+// internal headers
+#include "hydro_system.hpp"
+
+// function definitions
+
+#endif // TEST_HYDRO_WAVE_HPP_
