380fec701191486fcc6cf2725d106144fbf106cb
psharda <piyush.suneet@gmail.com>
2023-07-26 22:04:57 +0200

    define a finite mean molecular weight

--- src/HydroVacuum/test_hydro_vacuum.cpp
+++ src/HydroVacuum/test_hydro_vacuum.cpp
@@ -27,7 +27,7 @@ struct ShocktubeProblem {
 
 template <> struct quokka::EOS_Traits<ShocktubeProblem> {
 	static constexpr double gamma = 1.4;
-	static constexpr double mean_molecular_weight = NAN;
+	static constexpr double mean_molecular_weight = 1.0;
 	static constexpr double boltzmann_constant = C::k_B;
 };
 
