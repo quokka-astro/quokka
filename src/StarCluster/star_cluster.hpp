fb96683045919a9b574b2329d59039605e9e6cf6
Piyush Sharda <34922596+psharda@users.noreply.github.com>
2023-08-09 10:35:49 -0700

    Starcluster example problem (#342)
    * adjust multigrid parameters for gravity solve
    
    * add prototype star cluster example
    
    * reduce reltol for poisson solve
    
    * initialize with gaussian random field
    
    * add Jeans refinement
    
    * update params
    
    * change debugging settings
    
    * move settings to runtime params
    
    * add derived var
    
    * gpu fixes
    
    * gpu fix
    
    * fix OpenBCSolver init
    
    * add StarCluster AMR params file
    
    * delete old file
    
    * refactor code
    
    * read perturbations from file
    
    * fix TableData indices
    
    * increase max_retries
    
    * fix build on Crusher
    
    * add TinyProfiler regions
    
    * rename file content
    
    * define problem_main
    
    * generate zdrv.hdf5
    
    * update file
    
    * update file
    
    * update microp
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix spellings
    
    * reorder tests
    
    * reorder tests
    
    * run python script for starcluster test
    
    * add filename
    
    * add custom command to run python script
    
    * revert changes
    
    * use python to generate input file
    
    * copy input file to binary
    
    * reset maxtries to 4
    
    * make Gconst a runtime parameter with a default value
    
    * copy zdrv.hdf5 to tests/
    
    * remove StaarCluster AMR test from repo
    
    * remove sim.Gconst
    
    * updated microp
    
    * produce input file at runtime
    
    * check for h5py
    
    * synced microp;
    
    * update microp
    
    * clang-tidy comments
    
    * clang-tidy
    
    * fix bug
    
    * increase simulation time time to ~tff
    
    * set Gconst to unity
    
    ---------
    
    Co-authored-by: Ben Wibking <ben@wibking.com>
    Co-authored-by: Ben Wibking <wibkingb@msu.edu>
    Co-authored-by: Piyush Sharda <psharda@RSAA-043527.local>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Piyush Sharda <psharda@setonix-02.pawsey.org.au>
    Co-authored-by: Piyush Sharda <psharda@setonix-01.pawsey.org.au>

--- src/StarCluster/star_cluster.hpp
+++ src/StarCluster/star_cluster.hpp
@@ -1,12 +1,12 @@
-#ifndef TEST_SPHERICAL_COLLAPSE_HPP_ // NOLINT
-#define TEST_SPHERICAL_COLLAPSE_HPP_
+#ifndef STAR_CLUSTER_HPP_ // NOLINT
+#define STAR_CLUSTER_HPP_
 //==============================================================================
 // TwoMomentRad - a radiation transport library for patch-based AMR codes
 // Copyright 2020 Benjamin Wibking.
 // Released under the MIT license. See LICENSE file included in the GitHub repo.
 //==============================================================================
-/// \file spherical_collapse.hpp
-/// \brief Defines a test problem for a pressureless spherical collapse
+/// \file star_cluster.hpp
+/// \brief Defines a test problem for a 3D explosion.
 ///
 
 // external headers
@@ -19,4 +19,4 @@
 // function definitions
 auto problem_main() -> int;
 
-#endif // TEST_SPHERICAL_COLLAPSE_HPP_
+#endif // STAR_CLUSTER_HPP_
