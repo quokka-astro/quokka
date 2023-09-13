db3d1d293ce3748e1e76c96dc3b25bb959095578
Piyush Sharda <psharda@RSAA-043527.wind3.hub>
2023-09-13 16:57:18 +0200

    return invalid state if mass scalars have negative partial densities

--- src/hydro_system.hpp
+++ src/hydro_system.hpp
@@ -422,6 +422,18 @@ AMREX_GPU_DEVICE AMREX_FORCE_INLINE auto HydroSystem<problem_t>::isStateValid(am
 	const amrex::Real rho = cons(i, j, k, density_index);
 	bool isDensityPositive = (rho > 0.);
 
+    bool isMassScalarPositive = true;
+	if (nmscalars_ > 0) {
+	    amrex::GpuArray<Real, nmscalars_> massScalars_ = RadSystem<problem_t>::ComputeMassScalars(cons, i, j, k);
+
+	    for (int idx = 0; idx < nmscalars_; ++idx) {
+	        if (massScalars_[idx] < 0.0) {
+	            isMassScalarPositive = false;
+	            break;  // Exit the loop early if any element is not positive
+	        }
+	    }
+
+	}
 	// when the dual energy method is used, we *cannot* reset on pressure
 	// failures. on the other hand, we don't need to -- the auxiliary internal
 	// energy is used instead!
@@ -436,7 +448,7 @@ AMREX_GPU_DEVICE AMREX_FORCE_INLINE auto HydroSystem<problem_t>::isStateValid(am
 #endif
 	// return (isDensityPositive && isPressurePositive);
 
-	return isDensityPositive;
+	return isDensityPositive && isMassScalarPositive;
 }
 
 template <typename problem_t>
