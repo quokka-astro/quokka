46ab681af591b29800ef1bd1bde412b51a9c8fb3
ChongChong He <chongchonghe99@gmail.com>
2023-10-31 20:05:06 +1100

    first try

--- src/radiation_system.hpp
+++ src/radiation_system.hpp
@@ -1039,6 +1039,7 @@ void RadSystem<problem_t>::AddSourceTerms(array_t &consVar, arrayconst_t &radEne
 		}
 
 		const double a_rad = radiation_constant_;
+		double fourPiB = NAN;
 
 		if constexpr (gamma_ != 1.0) {
 			Egas0 = ComputeEintFromEgas(rho, x1GasMom0, x2GasMom0, x3GasMom0, Egastot0);
@@ -1054,7 +1055,6 @@ void RadSystem<problem_t>::AddSourceTerms(array_t &consVar, arrayconst_t &radEne
 			EradVec_guess = Erad0Vec;
 
 			double F_G = NAN;
-			double fourPiB = NAN;
 			double dFG_dEgas = NAN;
 			double deltaEgas = NAN;
 			double Rtot = NAN;
@@ -1158,54 +1158,70 @@ void RadSystem<problem_t>::AddSourceTerms(array_t &consVar, arrayconst_t &radEne
 		amrex::GpuArray<amrex::Real, 3> Frad_t1{};
 		amrex::GpuArray<amrex::Real, 3> dMomentum{};
 
-		T_gas = quokka::EOS<problem_t>::ComputeTgasFromEint(rho, Egas_guess, massScalars);
-    fourPiB = chat * a_rad * std::pow(T_gas, 4);
-		kappaFVec = ComputeFluxMeanOpacity(rho, T_gas);
-    kappaPVec = ComputePlanckOpacity(rho, T_gas);
-    auto gasMtm = {x1GasMom0, x2GasMom0, x3GasMom0};
+    if constexpr (gamma_ != 1.0) {
+		  T_gas = quokka::EOS<problem_t>::ComputeTgasFromEint(rho, Egas_guess, massScalars);
+      fourPiB = chat * a_rad * std::pow(T_gas, 4);
 
-		for (int g = 0; g < nGroups_; ++g) {
+      quokka::valarray<amrex::Real, nGroups_> radEnergyFractions{};
+      if constexpr (nGroups_ > 1) {
+        radEnergyFractions = ComputePlanckEnergyFractions(radBoundaries_g_copy, T_gas);
+        AMREX_ASSERT(min(radEnergyFractions) > 0.);
+      } else {
+        radEnergyFractions[0] = 1.0;
+      }
 
-			Frad_t0[0] = consPrev(i, j, k, x1RadFlux_index + numRadVars_ * g);
-			Frad_t0[1] = consPrev(i, j, k, x2RadFlux_index + numRadVars_ * g);
-			Frad_t0[2] = consPrev(i, j, k, x3RadFlux_index + numRadVars_ * g);
+      kappaFVec = ComputeFluxMeanOpacity(rho, T_gas);
+      kappaPVec = ComputePlanckOpacity(rho, T_gas);
+      std::array<double, 3> gasMtm = {x1GasMom0, x2GasMom0, x3GasMom0};
 
-      // // compute radiation pressure F using ComputeRadPressure
-      // auto erad = EradVec_guess[g];
-      // auto Fx = Frad_t0[0];
-      // auto Fy = Frad_t0[1];
-      // auto Fz = Frad_t0[2];
-      // auto fx = Fx / (c_light_ * erad);
-      // auto fy = Fy / (c_light_ * erad);
-      // auto fz = Fz / (c_light_ * erad);
+      for (int g = 0; g < nGroups_; ++g) {
 
-      // quokka::valarray<double, numRadVars_> radPressure{};
-      // double S = NAN;
-      // ComputeRadPressure<DIR>(erad, Fx, Fy, Fz, fx, fy, fz, radPressure, S);
+        Frad_t0[0] = consPrev(i, j, k, x1RadFlux_index + numRadVars_ * g);
+        Frad_t0[1] = consPrev(i, j, k, x2RadFlux_index + numRadVars_ * g);
+        Frad_t0[2] = consPrev(i, j, k, x3RadFlux_index + numRadVars_ * g);
 
-      // compute Planck function B
-      const double kappaP = kappaPVec[g];
+        // compute radiation pressure F using ComputeRadPressure
+        auto erad = EradVec_guess[g];
+        auto Fx = Frad_t0[0];
+        auto Fy = Frad_t0[1];
+        auto Fz = Frad_t0[2];
+        auto fx = Fx / (c_light_ * erad);
+        auto fy = Fy / (c_light_ * erad);
+        auto fz = Fz / (c_light_ * erad);
 
-			for (int n = 0; n < 3; ++n) {
-				AMREX_ASSERT((advectionFluxes(i, j, k, n) == 0.0));
-				Frad_t1[n] = (Frad_t0[n] + (dt * advectionFluxes(i, j, k, n))) / (1.0 + rho * kappaFVec[g] * chat * dt);
-				// Compute conservative gas momentum update
-				dMomentum[n] = -(Frad_t1[n] - Frad_t0[n]) / (c * chat);
-			}
+        std::array<std::array<double, 4>, 3> P{};
+        double S = NAN;
+        // loop DIR over {FluxDir::X1, FluxDir::X2, FluxDir::X3}
+        ComputeRadPressure<FluxDir::X1>(erad, Fx, Fy, Fz, fx, fy, fz, P[0], S);
+        ComputeRadPressure<FluxDir::X2>(erad, Fx, Fy, Fz, fx, fy, fz, P[1], S);
+        ComputeRadPressure<FluxDir::X3>(erad, Fx, Fy, Fz, fx, fy, fz, P[2], S);
 
-			// update radiation flux on consNew at current group
-			consNew(i, j, k, x1RadFlux_index + numRadVars_ * g) = Frad_t1[0];
-			consNew(i, j, k, x2RadFlux_index + numRadVars_ * g) = Frad_t1[1];
-			consNew(i, j, k, x3RadFlux_index + numRadVars_ * g) = Frad_t1[2];
+        // loop over spatial dimensions
+        for (int n = 0; n < 3; ++n) {
+          AMREX_ASSERT((advectionFluxes(i, j, k, n) == 0.0));
+          double lastTwoTerms = gasMtm[n] * kappaPVec[g] * fourPiB * radEnergyFractions[g] * chat / c;
+          // loop over the second rank of the radiation pressure tensor
+          for (int z = 0; z < 3; ++z) {
+            lastTwoTerms += chat * kappaFVec[g] * gasMtm[z] * P[n][z + 1];
+          }
+          Frad_t1[n] = (Frad_t0[n] + (dt * lastTwoTerms) + (dt * advectionFluxes(i, j, k, n))) / (1.0 + rho * kappaFVec[g] * chat * dt);
 
-			// compute the gas momentum after adding the current group's momentum
-			consNew(i, j, k, x1GasMomentum_index) += dMomentum[0];
-			consNew(i, j, k, x2GasMomentum_index) += dMomentum[1];
-			consNew(i, j, k, x3GasMomentum_index) += dMomentum[2];
-		}
+          // Compute conservative gas momentum update
+          dMomentum[n] = -(Frad_t1[n] - Frad_t0[n]) / (c * chat);
+        }
 
-		// update gas momentum from radiation momentum of the current group
-		if constexpr (gamma_ != 1.0) {
+        // update radiation flux on consNew at current group
+        consNew(i, j, k, x1RadFlux_index + numRadVars_ * g) = Frad_t1[0];
+        consNew(i, j, k, x2RadFlux_index + numRadVars_ * g) = Frad_t1[1];
+        consNew(i, j, k, x3RadFlux_index + numRadVars_ * g) = Frad_t1[2];
+
+        // compute the gas momentum after adding the current group's momentum
+        consNew(i, j, k, x1GasMomentum_index) += dMomentum[0];
+        consNew(i, j, k, x2GasMomentum_index) += dMomentum[1];
+        consNew(i, j, k, x3GasMomentum_index) += dMomentum[2];
+      }
+
+		  // update gas momentum from radiation momentum of the current group
 			amrex::Real x1GasMom1 = consNew(i, j, k, x1GasMomentum_index);
 			amrex::Real x2GasMom1 = consNew(i, j, k, x2GasMomentum_index);
 			amrex::Real x3GasMom1 = consNew(i, j, k, x3GasMomentum_index);
