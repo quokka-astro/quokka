f58aaf0c0e00963d80fa0697f43333a9355c91c7
chongchonghe <chongchonghe99@gmail.com>
2023-12-04 19:47:45 +1100

    Created the radhydro_pulse_grey test problem

---
+++ src/RadhydroPulseGrey/test_radhydro_pulse_grey.hpp
@@ -0,0 +1,21 @@
+#ifndef TEST_RADHYDRO_PULSE_GREY_HPP_ // NOLINT
+#define TEST_RADHYDRO_PULSE_GREY_HPP_
+/// \file test_radhydro_pulse_grey.hpp
+/// \brief Defines a test problem for radiation in the static diffusion regime.
+///
+
+// external headers
+#ifdef HAVE_PYTHON
+#include "matplotlibcpp.h"
+#endif
+#include <fmt/format.h>
+#include <fstream>
+
+// internal headers
+
+#include "interpolate.hpp"
+#include "radiation_system.hpp"
+
+// function definitions
+
+#endif // TEST_RADHYDRO_PULSE_GREY_HPP_
