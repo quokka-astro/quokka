6214c0f382e977859c8b8e77fa116422d015a117
chongchonghe <chongchonghe99@gmail.com>
2023-12-06 12:11:37 +1100

    Update odd-even instability check to i+j+k=odd

--- src/radiation_system.hpp
+++ src/radiation_system.hpp
@@ -979,18 +979,25 @@ void RadSystem<problem_t>::ComputeFluxes(array_t &x1Flux_in, array_t &x1FluxDiff
 
 			// fix odd-even instability that appears in the asymptotic diffusion limit
 			// [for details, see section 3.1: https://ui.adsabs.harvard.edu/abs/2022MNRAS.512.1499R/abstract]
-			for (int idx = 0; idx < numRadVars_; ++idx) {
-				const double u_im2 = consVar(i - 2, j, k, idx + numRadVars_ * g);
-				const double u_im1 = consVar(i - 1, j, k, idx + numRadVars_ * g);
-				const double u_i = consVar(i, j, k, idx + numRadVars_ * g);
-				const double u_ip1 = consVar(i + 1, j, k, idx + numRadVars_ * g);
-				// check for local maximum/minimum
-				if (((u_im1 - u_im2) * (u_i - u_im1) < 0.) && ((u_i - u_im1) * (u_ip1 - u_i) < 0.)) {
-					// revert to more diffusive flux (has no effect in optically-thin limit)
-					epsilon /= S_corr;
-					break;
-				}
-			}
+			// for (int idx = 0; idx < numRadVars_; ++idx) {
+			// 	const double u_im2 = consVar(i - 2, j, k, idx + numRadVars_ * g);
+			// 	const double u_im1 = consVar(i - 1, j, k, idx + numRadVars_ * g);
+			// 	const double u_i = consVar(i, j, k, idx + numRadVars_ * g);
+			// 	const double u_ip1 = consVar(i + 1, j, k, idx + numRadVars_ * g);
+			// 	// check for local maximum/minimum
+			// 	if (((u_im1 - u_im2) * (u_i - u_im1) < 0.) && ((u_i - u_im1) * (u_ip1 - u_i) < 0.)) {
+			// 		// revert to more diffusive flux (has no effect in optically-thin limit)
+			// 		// epsilon /= S_corr;
+      //     epsilon = {S_corr, 1.0, 1.0, 1.0};
+			// 		break;
+			// 	}
+			// }
+
+      // if ((i % 2 == 1) || (j % 2 == 1) || (k % 2 == 1)) {
+      if ((i + j + k) % 2 == 1) {
+        // revert to more diffusive flux (has no effect in optically-thin limit)
+        epsilon = {S_corr, 1.0, 1.0, 1.0};
+      }
 
 			// compute the norm of the wavespeed vector
 			const double S_L = std::min(-0.1 * c_hat_, -c_hat_ * std::sqrt(Tnormal_L));
