4f9e1eeedc43a1489c8f513e646624f423a75afe
ChongChong He <chongchonghe99@gmail.com>
2024-01-04 15:08:36 +1100

    Update RadhydroPulse.in

--- src/RadhydroPulse/test_radhydro_pulse.cpp
+++ src/RadhydroPulse/test_radhydro_pulse.cpp
@@ -29,7 +29,7 @@ constexpr double v0_nonadv = 0.; // non-advecting pulse
 // static diffusion: tau = 2e3, beta = 3e-5, beta tau = 6e-2
 constexpr double kappa0 = 100.;	    // cm^2 g^-1
 constexpr double v0_adv = 1.0e6;    // advecting pulse
-constexpr double max_time = 4.8e-5; // max_time = 2.0 * width / v1;
+constexpr double max_time = 2.4e-5; // max_time = 2.0 * width / v1;
 
 // dynamic diffusion: tau = 2e4, beta = 3e-3, beta tau = 60
 // constexpr double kappa0 = 1000.; // cm^2 g^-1
@@ -350,7 +350,7 @@ auto problem_main() -> int
 		err_norm += std::abs(Tgas2[i] - Trad[i]);
 		sol_norm += std::abs(Trad[i]) * 3.0;
 	}
-	const double error_tol = 0.001;
+	const double error_tol = 0.006;
 	const double rel_error = err_norm / sol_norm;
 	amrex::Print() << "Relative L1 error norm = " << rel_error << std::endl;
 
