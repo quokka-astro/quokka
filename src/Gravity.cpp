dc8c77d8650af8917e4c65a89e9c5a3b1b48ef12
Piyush Sharda <psharda@RSAA-043527.local>
2024-01-15 09:53:12 +0100

    remove multipole stuff

--- src/Gravity.cpp
+++ src/Gravity.cpp
@@ -1,11 +1,2 @@
 #include "Gravity.hpp"
 
-///
-/// Multipole gravity data
-///
-
-AMREX_GPU_MANAGED Real multipole::rmax;
-
-AMREX_GPU_MANAGED amrex::Array2D<Real, 0, multipole::lnum_max, 0, multipole::lnum_max> multipole::factArray;
-AMREX_GPU_MANAGED amrex::Array1D<Real, 0, multipole::lnum_max> multipole::parity_q0;
-AMREX_GPU_MANAGED amrex::Array2D<Real, 0, multipole::lnum_max, 0, multipole::lnum_max> multipole::parity_qC_qS;
