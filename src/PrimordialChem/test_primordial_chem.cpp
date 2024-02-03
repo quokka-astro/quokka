70424b38a759918c98e8f6dfc6f48941b2944ab7
Piyush Sharda <psharda@RSAA-043527.local>
2024-02-03 13:23:21 +0100

    dummy commit

--- src/PrimordialChem/test_primordial_chem.cpp
+++ src/PrimordialChem/test_primordial_chem.cpp
@@ -38,7 +38,7 @@
 using amrex::Real;
 
 struct PrimordialChemTest {
-}; // dummy type to allow compile-type polymorphism via template specialization
+}; // dummy type to allow compile-type polymorphism via template specializationnnnnn
 
 template <> struct Physics_Traits<PrimordialChemTest> {
 	// cell-centred
