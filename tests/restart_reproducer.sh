34d6f356ba62d5c587dcf58cebc5d1e8c83e6486
Ben Wibking <ben@wibking.com>
2024-03-11 17:58:27 -0400

    update reproducer

--- tests/restart_reproducer.sh
+++ tests/restart_reproducer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -x
 
-../build/src/ShockCloud/shock_cloud ShockCloud_32.in
+../build/src/ShockCloud/shock_cloud ShockCloud_32.in max_walltime=0:00:30
 ../build/src/ShockCloud/shock_cloud ShockCloud_32.in restartfile=last_chk max_timesteps=1
 
 old_plotfile=`ls -1drt plt*.old.* | head -1`
