db4f3d1d45c0866629800e34e30298acafa70176
Neco Kriel <necokriel@gmail.com>
2024-05-08 16:59:42 +1000

    debug: add evolve step to check induction eqn

--- src/FCQuantities/test_fc_quantities.cpp
+++ src/FCQuantities/test_fc_quantities.cpp
@@ -157,6 +157,7 @@ auto problem_main() -> int
 
 	RadhydroSimulation<FCQuantities> sim_write(BCs_cc, BCs_fc);
 	sim_write.setInitialConditions();
+  sim_write.evolve();
 	amrex::Vector<amrex::Array<amrex::MultiFab, AMREX_SPACEDIM>> const &state_new_fc_write = sim_write.getNewMF_fc();
 	amrex::Print() << "\n";
 
