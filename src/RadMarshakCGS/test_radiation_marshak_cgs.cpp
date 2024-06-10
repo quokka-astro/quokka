c52d071483c295bcf2f12ce585bce7bd442bea7f
Ben Wibking <ben@wibking.com>
2024-06-10 06:39:12 -0400

    quokka::EOS code linting (#578)
    ### Description
    This fixes some clang-tidy and CodeQL warnings for EOS.hpp. All of the
    changes convert pass-by-value to pass-by-reference for large objects in
    EOS function calls.
    
    ### Related issues
    https://github.com/quokka-astro/quokka/security/code-scanning/2609
    
    ### Checklist
    _Before this pull request can be reviewed, all of these tasks should be
    completed. Denote completed tasks with an `x` inside the square brackets
    `[ ]` in the Markdown source below:_
    - [x] I have added a description (see above).
    - [x] I have added a link to any related issues see (see above).
    - [x] I have read the [Contributing
    Guide](https://github.com/quokka-astro/quokka/blob/development/CONTRIBUTING.md).
    - [ ] I have added tests for any new physics that this PR adds to the
    code.
    - [x] I have tested this PR on my local computer and all tests pass.
    - [x] I have manually triggered the GPU tests with the magic comment
    `/azp run`.
    - [x] I have requested a reviewer for this PR.
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/RadMarshakCGS/test_radiation_marshak_cgs.cpp
+++ src/RadMarshakCGS/test_radiation_marshak_cgs.cpp
@@ -77,7 +77,7 @@ AMREX_GPU_HOST_DEVICE auto RadSystem<SuOlsonProblemCgs>::ComputeFluxMeanOpacity(
 static constexpr int nmscalars_ = Physics_Traits<SuOlsonProblemCgs>::numMassScalars;
 template <>
 AMREX_GPU_HOST_DEVICE auto quokka::EOS<SuOlsonProblemCgs>::ComputeTgasFromEint(const double /*rho*/, const double Egas,
-									       std::optional<amrex::GpuArray<amrex::Real, nmscalars_>> /*massScalars*/)
+									       std::optional<amrex::GpuArray<amrex::Real, nmscalars_>> const & /*massScalars*/)
     -> double
 {
 	return std::pow(4.0 * Egas / alpha_SuOlson, 1. / 4.);
@@ -85,16 +85,16 @@ AMREX_GPU_HOST_DEVICE auto quokka::EOS<SuOlsonProblemCgs>::ComputeTgasFromEint(c
 
 template <>
 AMREX_GPU_HOST_DEVICE auto quokka::EOS<SuOlsonProblemCgs>::ComputeEintFromTgas(const double /*rho*/, const double Tgas,
-									       std::optional<amrex::GpuArray<amrex::Real, nmscalars_>> /*massScalars*/)
+									       std::optional<amrex::GpuArray<amrex::Real, nmscalars_>> const & /*massScalars*/)
     -> double
 {
 	return (alpha_SuOlson / 4.0) * std::pow(Tgas, 4);
 }
 
 template <>
-AMREX_GPU_HOST_DEVICE auto quokka::EOS<SuOlsonProblemCgs>::ComputeEintTempDerivative(const double /*rho*/, const double Tgas,
-										     std::optional<amrex::GpuArray<amrex::Real, nmscalars_>> /*massScalars*/)
-    -> double
+AMREX_GPU_HOST_DEVICE auto
+quokka::EOS<SuOlsonProblemCgs>::ComputeEintTempDerivative(const double /*rho*/, const double Tgas,
+							  std::optional<amrex::GpuArray<amrex::Real, nmscalars_>> const & /*massScalars*/) -> double
 {
 	// This is also known as the heat capacity, i.e.
 	// 		\del E_g / \del T = \rho c_v,
