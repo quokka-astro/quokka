34593a07003a9b6e3869a49eb8d025be4d53241e
Ben Wibking <ben@wibking.com>
2024-08-04 19:17:22 +0000

    add Jeans test problem

---
+++ src/problems/Jeans/jeans.hpp
@@ -0,0 +1,22 @@
+#ifndef TEST_SPHERICAL_COLLAPSE_HPP_ // NOLINT
+#define TEST_SPHERICAL_COLLAPSE_HPP_
+//==============================================================================
+// TwoMomentRad - a radiation transport library for patch-based AMR codes
+// Copyright 2020 Benjamin Wibking.
+// Released under the MIT license. See LICENSE file included in the GitHub repo.
+//==============================================================================
+/// \file spherical_collapse.hpp
+/// \brief Defines a test problem for a pressureless spherical collapse
+///
+
+// external headers
+#include <fstream>
+
+// internal headers
+#include "hydro/hydro_system.hpp"
+#include "math/interpolate.hpp"
+
+// function definitions
+auto problem_main() -> int;
+
+#endif // TEST_SPHERICAL_COLLAPSE_HPP_
