dcdd9a68f186744b548f5961a295c39e7f1ee649
Aditi <45175933+aditivijayan@users.noreply.github.com>
2024-08-07 06:59:57 +1000

    Update test_sne.hpp

--- src/problems/MetalAdvectionProblem/test_sne.hpp
+++ src/problems/MetalAdvectionProblem/test_sne.hpp
@@ -1,5 +1,5 @@
-#ifndef TEST_HYDRO3D_BLAST_HPP_ // NOLINT
-#define TEST_HYDRO3D_BLAST_HPP_
+#ifndef TEST_SNE_ // NOLINT
+#define TEST_SNE_
 //==============================================================================
 // TwoMomentRad - a radiation transport library for patch-based AMR codes
 // Copyright 2020 Benjamin Wibking.
