ccef32b435b0cd0d2f366b8a8fa2745e6c7a7061
ChongChong He <chongchong.he@anu.edu.au>
2024-08-15 19:59:55 +1000

    merge in RadDust folder from T_d branch

---
+++ src/problems/RadDust/test_rad_dust.hpp
@@ -0,0 +1,26 @@
+#ifndef TEST_RAD_DUST_HPP_ // NOLINT
+#define TEST_RAD_DUST_HPP_
+//==============================================================================
+// TwoMomentRad - a radiation transport library for patch-based AMR codes
+// Copyright 2020 Benjamin Wibking.
+// Released under the MIT license. See LICENSE file included in the GitHub repo.
+//==============================================================================
+/// \file test_rad_dust.hpp
+/// \brief Defines a test problem for radiation in the static diffusion regime.
+///
+
+// external headers
+#ifdef HAVE_PYTHON
+#include "util/matplotlibcpp.h"
+#endif
+#include <fmt/format.h>
+#include <fstream>
+
+// internal headers
+
+#include "math/interpolate.hpp"
+#include "radiation/radiation_system.hpp"
+
+// function definitions
+
+#endif // TEST_RAD_DUST_HPP_
