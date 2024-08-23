0e7f5a604549fc85bc37bc542178d8a885a22599
ChongChong He <chongchong.he@anu.edu.au>
2024-08-23 15:06:25 +1000

    define MarshakHot test

---
+++ src/problems/RadMarshakHot/test_radiation_marshak_hot.hpp
@@ -0,0 +1,24 @@
+#ifndef TEST_RADIATION_MARSHAK_HOT_HPP_ // NOLINT
+#define TEST_RADIATION_MARSHAK_HOT_HPP_
+//==============================================================================
+// TwoMomentRad - a radiation transport library for patch-based AMR codes
+// Copyright 2020 Benjamin Wibking.
+// Released under the MIT license. See LICENSE file included in the GitHub repo.
+//==============================================================================
+/// \file test_radiation_marshak_hot.hpp
+/// \brief Defines a test problem for radiation in the free-streaming regime.
+///
+
+// external headers
+#ifdef HAVE_PYTHON
+#include "util/matplotlibcpp.h"
+#endif
+#include <fmt/format.h>
+
+// internal headers
+
+#include "radiation/radiation_system.hpp"
+
+// function definitions
+
+#endif // TEST_RADIATION_MARSHAK_HOT_HPP_
