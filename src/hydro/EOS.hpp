faab134d97fca0e9a9564262b967348c671c427f
Ben Wibking <ben@wibking.com>
2025-01-20 22:34:09 -0500

    Update EOS.hpp

--- src/hydro/EOS.hpp
+++ src/hydro/EOS.hpp
@@ -7,10 +7,6 @@
 /// \file EOS.hpp
 /// \brief A class for equation of state calculations.
 
-// this is required to workaround libstdc++ compatibility issues with HIP
-#ifdef __HIP_DEVICE_COMPILE__
-#undef _GLIBCXX_ASSERTIONS
-#endif
 #include <cmath>
 #include <optional>
 #include <tuple>
