b49252dca976494eac681cdfd31011f9249bfaea
ChongChong He <chongchonghe99@gmail.com>
2025-02-20 11:59:19 +1100

    update

--- src/particles/PhysicsParticles.hpp
+++ src/particles/PhysicsParticles.hpp
@@ -46,7 +46,7 @@ constexpr int RadParticleRealComps = []() constexpr {
 	if constexpr (Physics_Traits<problem_t>::is_hydro_enabled || Physics_Traits<problem_t>::is_radiation_enabled) {
 		return 2 + Physics_Traits<problem_t>::nGroups; // birth_time death_time lum1 ... lumN
 	} else {
-		return 3; // birth_time death_time placeholder
+		return 4; // birth_time death_time placeholder
 	}
 }();
 
