003d8ab664d72fb36dc9be6027258736a9fce566
Ben Wibking <ben@wibking.com>
2025-03-27 16:33:39 -0400

    update

---
+++ extern/agora_data/create_amrex_particles.py
@@ -0,0 +1,43 @@
+import numpy as np
+import argparse
+from pathlib import Path
+
+cgs_length = 1.0e3 * 3.086e18 # 1 kpc
+cgs_vel = 1.0e5 # 1 km/s
+cgs_mass = 1.0e9 * 1.989e33 # 1e9 solar masses
+
+if __name__ == "__main__":
+    ## save ASCII file in AMReX particle format
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_directory")
+    parser.add_argument("output_file")
+
+    args = parser.parse_args()
+    input_dir = Path(args.data_directory)
+
+    # Dark matter halo (halo.dat): x,y,z,vx,vy,vz,mdark
+    # Stellar disk (disk.dat): x,y,z,vx,vy,vz,mdisk
+    # S tellar bulge (bulge.dat): x,y,z,vx,vy,vz,mbulge    
+    dm_file = input_dir / "halo.dat"
+    disk_file = input_dir / "disk.dat"
+    bulge_file = input_dir / "bulge.dat"
+
+    with open(args.output_file, 'w') as output:
+        for part_file in [dm_file, disk_file, bulge_file]:            
+            ## read data
+            x,y,z,vx,vy,vz,m = np.loadtxt(part_file, unpack=True)
+
+            ## convert to CGS units
+            x *= cgs_length
+            y *= cgs_length
+            z *= cgs_length
+            vx *= cgs_vel
+            vy *= cgs_vel
+            vz *= cgs_vel
+            m *= cgs_mass
+
+            ## save to output file
+            print("writing", part_file, "...")
+            Npart = len(x)
+            for i in range(Npart):
+                output.write(f"{x[i]} {y[i]} {z[i]} {m[i]} {vx[i]} {vy[i]} {vz[i]}\n")
--- src/problems/AgoraGalaxy/galaxy.cpp
+++ src/problems/AgoraGalaxy/galaxy.cpp
@@ -10,6 +10,7 @@
 #include <cmath>
 
 #include "AMReX_BC_TYPES.H"
+#include "AMReX_GpuContainers.H"
 #include "AMReX_REAL.H"
 
 #include "QuokkaSimulation.hpp"
@@ -18,6 +19,7 @@
 #include "galaxy.hpp"
 #include "hydro/EOS.hpp"
 #include "hydro/hydro_system.hpp"
+#include "math/interpolate.hpp"
 #include "physics_info.hpp"
 
 struct AgoraGalaxy {
@@ -25,7 +27,7 @@ struct AgoraGalaxy {
 
 template <> struct quokka::EOS_Traits<AgoraGalaxy> {
 	static constexpr double gamma = 5. / 3.;
-	static constexpr double mean_molecular_weight = C::m_u;
+	static constexpr double mean_molecular_weight = 0.6 * C::m_u;
 	static constexpr double boltzmann_constant = C::k_B;
 };
 
@@ -48,41 +50,81 @@ template <> struct Particle_Traits<AgoraGalaxy> {
 };
 
 template <> struct SimulationData<AgoraGalaxy> {
-	std::vector<amrex::Real> radius;
-	std::vector<amrex::Real> vcirc;
+	amrex::Gpu::PinnedVector<amrex::Real> radius;
+	amrex::Gpu::PinnedVector<amrex::Real> vcirc;
 };
 
 template <> void QuokkaSimulation<AgoraGalaxy>::preCalculateInitialConditions()
 {
-	// 1. read in circular velocity table
-	// 2. copy to GPU
-	// 3. save interpolator object
-	//
-	// TODO(bwibking): implement.
+	// 1. read in circular velocity table "vcirc_SPH.dat"
+	std::vector<amrex::Real> radius_h;
+	std::vector<amrex::Real> vcirc_h;
+
+	std::string filename = "../extern/agora_data/vcirc_SPH.dat";
+	std::ifstream fstream(filename, std::ios::in);
+	AMREX_ALWAYS_ASSERT(fstream.is_open());
+	std::string header;
+	std::getline(fstream, header);
+
+	for (std::string line; std::getline(fstream, line);) {
+		std::istringstream iss(line);
+		std::vector<double> values;
+
+		for (double value = NAN; iss >> value;) {
+			values.push_back(value);
+		}
+		Real R_val = values.at(0);
+		Real vcirc_val = values.at(1);
+
+		radius_h.push_back(R_val);
+		vcirc_h.push_back(vcirc_val);
+	}
+
+	// 2. copy data to simData_.radius and simData_.vcirc
+	const size_t N = radius_h.size();
+	userData_.radius.resize(N);
+	userData_.vcirc.resize(N);
+
+	for (int i = 0; i < N; ++i) {
+		userData_.radius[i] = radius_h[i];
+		userData_.vcirc[i] = vcirc_h[i];
+	}
 }
 
 template <> void QuokkaSimulation<AgoraGalaxy>::setInitialConditionsOnGrid(quokka::grid const &grid_elem)
 {
 	const amrex::Box &indexRange = grid_elem.indexRange_;
 	const amrex::GpuArray<amrex::Real, AMREX_SPACEDIM> dx = grid_elem.dx_;
-	const amrex::Array4<double> &state_cc = grid_elem.array_;
 	const amrex::GpuArray<amrex::Real, AMREX_SPACEDIM> prob_lo = grid_elem.prob_lo_;
-	const amrex::Real gamma = quokka::EOS_Traits<AgoraGalaxy>::gamma;
+	const amrex::Array4<double> &state_cc = grid_elem.array_;
+
+	double const *R_table = userData_.radius.dataPtr();
+	double const *vcirc_table = userData_.vcirc.dataPtr();
+	int const len_table = static_cast<int>(userData_.radius.size());
 
 	amrex::ParallelFor(indexRange, [=] AMREX_GPU_DEVICE(int i, int j, int k) {
 		// Cartesian coordinates
 		amrex::Real const x = prob_lo[0] + ((i + static_cast<amrex::Real>(0.5)) * dx[0]);
 		amrex::Real const y = prob_lo[1] + ((j + static_cast<amrex::Real>(0.5)) * dx[1]);
+		amrex::Real const z = prob_lo[2] + ((j + static_cast<amrex::Real>(0.5)) * dx[2]);
 
 		// cylindrical coordinates
 		amrex::Real const R = std::sqrt(std::pow(x, 2) + std::pow(y, 2));
 		amrex::Real const theta = std::atan2(x, y);
 
-		// compute double exponential density profile
-		double const rho = 1.0e-22; // g cm^{-3}
+		// Disk mass: 8.59322e9 Msun  (i.e. 20% gas fraction)
+		constexpr double M_GAS = 8.59322e9 * C::M_solar;
+		// Disk scale length: 3.43218 kpc
+		constexpr double r_d = 3.43218e3 * C::parsec;
+		// Disk scale height: 0.343218 kpc (10% of scale length)
+		constexpr double z_d = 0.343218e3 * C::parsec;
 
-		// interpolate circular velocity based on radius of cell center
-		double const vcirc = 0;
+		// compute double exponential density profile
+		const double rho_0 = M_GAS / 4. / M_PI / (r_d * r_d) / z_d;
+		double rho = rho_0 * std::exp(-R / r_d) * std::exp(-std::abs(z) / z_d);
+
+		// interpolate circular velocity based on radius of cell center R
+		double const vcirc = interpolate_value(R, R_table, vcirc_table, len_table);
 		double const vx = vcirc * std::cos(theta);
 		double const vy = vcirc * std::sin(theta);
 		double const vz = 0;
@@ -90,10 +132,11 @@ template <> void QuokkaSimulation<AgoraGalaxy>::setInitialConditionsOnGrid(quokk
 
 		// compute temperature
 		double T = NAN;
-		if (R < 20.0e3 * C::parsec) {
+		if ((R < 20.0e3 * C::parsec) && (std::abs(z) < 3.0e3 * C::parsec)) {
 			T = 1.0e4; // K
 		} else {
 			T = 1.0e6; // K
+			rho = 1.0e-6 * quokka::EOS_Traits<AgoraGalaxy>::mean_molecular_weight;
 		}
 		const double Eint = quokka::EOS<AgoraGalaxy>::ComputeEintFromTgas(rho, T);
 
