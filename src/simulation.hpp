4829d440d23dbc796e9748dc5fc45cf21427d3fc
Ben Wibking <ben@wibking.com>
2025-04-04 13:32:13 -0400

    revert files

--- src/simulation.hpp
+++ src/simulation.hpp
@@ -1239,12 +1239,6 @@ template <typename problem_t> void AMRSimulation<problem_t>::calculateGpotAllLev
 
 		// check for NaN
 		for (int lev = 0; lev <= finest_level; ++lev) {
-			const bool this_lev_contains_nan = phi[lev].contains_nan();
-			if (this_lev_contains_nan) {
-				amrex::Print() << "[ERROR] Phi contains NAN on level " << lev << "!" << std::endl; // NOLINT
-				amrex::Print() << "[ERROR] Writing plotfile for debugging..." << std::endl; // NOLINT
-				WritePlotFile();
-			}
 			AMREX_ALWAYS_ASSERT(!phi[lev].contains_nan()); // this fails when max_level=2 for SphericalCollapse
 		}
 	}
