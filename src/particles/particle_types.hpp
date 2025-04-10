37bd57b7400904cf45790dd9302f58be0b9f0d18
ChongChong He <chongchong.he@anu.edu.au>
2025-04-11 01:28:32 +1000

    fix bug in TestParticleRealComps

--- src/particles/particle_types.hpp
+++ src/particles/particle_types.hpp
@@ -216,7 +216,7 @@ constexpr int TestParticleStageIdx = 0; // Evolution stage of the particle, inde
 // Number of real components for StochasticStellarPop_particles, mass + 3 velocity components + luminosity
 template <typename problem_t>
 constexpr int TestParticleRealComps = []() constexpr {
-	if constexpr (Physics_Traits<problem_t>::is_hydro_enabled && Physics_Traits<problem_t>::is_radiation_enabled) {
+	if constexpr (Physics_Traits<problem_t>::is_hydro_enabled || Physics_Traits<problem_t>::is_radiation_enabled) {
 		return 6 + Physics_Traits<problem_t>::nGroups; // mass, vx, vy, vz, birth_time, death_time, lum[nGroups]
 	} else {
 		return 6; // mass, vx, vy, vz, birth_time, death_time
