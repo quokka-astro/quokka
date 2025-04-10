0d71da67c8e222ed3bf4d601224a2b96572705ab
Aditi Vijayan <aditi.vijayan@anu.edu.au>
2025-04-11 09:54:56 +1000

    fix

--- src/particles/particle_creation.hpp
+++ src/particles/particle_creation.hpp
@@ -339,7 +339,7 @@ template <> struct ParticleCreationTraits<ParticleType::StochasticStellarPop> {
 
 				if (num_particles > 1) { // Update momentum of the low mass star if there is(are) high mass star(s) in the cell
 					const int p_idx = 0;
-					auto &plow = particles[p_idx];
+					auto &plow = particles[p_idx]; //NOLINT
 					plow.rdata(mass_idx + 1) = -total_momx / plow.rdata(mass_idx);
 					plow.rdata(mass_idx + 2) = -total_momy / plow.rdata(mass_idx);
 					plow.rdata(mass_idx + 3) = -total_momz / plow.rdata(mass_idx);
