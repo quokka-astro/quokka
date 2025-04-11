1582301f030c2d926285819c1318d48be52d12bd
Aditi Vijayan <aditi.vijayan@anu.edu.au>
2025-04-11 22:36:10 +1000

    IMF break at Msun and not 8Msun

--- src/particles/particle_creation.hpp
+++ src/particles/particle_creation.hpp
@@ -152,7 +152,7 @@ template <> struct ParticleCreationTraits<ParticleType::StochasticStellarPop> {
 	static constexpr amrex::Real m_star_high = 8.0 * C::M_solar; // all stars above this mass are considered high mass stars
 	static constexpr amrex::Real m_imf_min = 0.08 * C::M_solar;  // lower limit of the IMF
 	static constexpr amrex::Real m_imf_max = 120.0 * C::M_solar; // high mass limit of the IMF
-	static constexpr amrex::Real m_imf_break = 8.0 * C::M_solar; // IMF is lognormal below this mass and powerlaw above
+	static constexpr amrex::Real m_imf_break = 1.0 * C::M_solar; // IMF is lognormal below this mass and powerlaw above
 	static constexpr amrex::Real imf_disp = 0.55;		     // dispersion of the lognormal IMF
 	static constexpr amrex::Real imf_mu = 32.599;		     //=log10(0.2 * C::M_solar), mean of the lognormal IMF, avoid compiler error
 	static constexpr amrex::Real alpha = 2.35;		     // slope of the powerlaw
