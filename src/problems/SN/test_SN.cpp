b6b9cb7805f83f93087cc03f1cd01ddcff4f47e3
ChongChong He <chongchong.he@anu.edu.au>
2025-04-19 22:06:27 +1000

    use static

--- src/problems/SN/test_SN.cpp
+++ src/problems/SN/test_SN.cpp
@@ -26,7 +26,7 @@ struct SNProblem {
 
 static double max_Eint_global = 0.0;
 
-AMREX_GPU_MANAGED std::string SN_particles_file = "SN_particles.txt";
+static std::string SN_particles_file = "SN_particles.txt";
 
 constexpr double mu = 1.0 * C::m_u;
 // constexpr double mu = 1.295 * C::m_u; // neutral gas
@@ -35,9 +35,9 @@ const double CV = 1. / (gamma_ - 1.) / mu * C::k_B;
 const double cloudy_H_mass_fraction = 1.0 / (1.0 + 0.1 * 3.971);
 const double year = 3.15576e+07; // in seconds
 
-AMREX_GPU_MANAGED double n_amb = 1.0;	 // ambient density (g cm^-3)
-AMREX_GPU_MANAGED double T_amb = 100.0;	 // ambient temperature (K)
-AMREX_GPU_MANAGED double t_stop = 3.0e5; // stop time (yr)
+static double n_amb = 1.0;	 // ambient density (g cm^-3)
+static double T_amb = 100.0;	 // ambient temperature (K)
+static double t_stop = 3.0e5; // stop time (yr)
 
 template <> struct Particle_Traits<SNProblem> {
 	// static constexpr ParticleSwitch particle_switch = ParticleSwitch::None;
