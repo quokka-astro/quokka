cd8702ddaa40a991245d1764fbbf32f493fc377a
ChongChong He <chongchong.he@anu.edu.au>
2025-04-20 17:36:42 +1000

    merge in ParticleCreation test; passing

---
+++ src/problems/ParticleCreation/particle_creation.hpp
@@ -0,0 +1,10 @@
+#ifndef TEST_PARTICLE_CREATION_HPP_ // NOLINT
+#define TEST_PARTICLE_CREATION_HPP_
+/// \file particle_creation.hpp
+/// \brief Defines a test problem for particle creation
+///
+
+// function definitions
+auto problem_main() -> int;
+
+#endif // TEST_PARTICLE_CREATION_HPP_
