415fa2bebc761e05270a57702f3a9e5e68a0819b
Ben Wibking <ben@wibking.com>
2025-05-01 14:39:58 -0400

    add stellar mass function script

---
+++ src/problems/AgoraGalaxy/check_stars.py
@@ -0,0 +1,95 @@
+import yt
+import numpy as np
+import argparse
+import matplotlib
+import matplotlib.pyplot as plt
+from math import *
+
+Msun = 1.99e33
+seconds_in_yr = 3.15e7
+
+def compute_fstar_high():
+    m_star_high = 8.0 * Msun
+    m_imf_min = 0.08 * Msun
+    m_imf_max = 120.0 * Msun
+    m_imf_break = 1.0 * Msun
+    imf_disp = 0.55
+    imf_mu = log10(0.2 * Msun)
+    alpha = 2.35
+    log_m_imf_break = log10(m_imf_break)
+    log_m_imf_min = log(m_imf_min)
+    arg_m_imf_break = (log_m_imf_break - imf_mu) / (sqrt(2.) * imf_disp);
+    arg_m_imf_min = (log_m_imf_min - imf_mu) / (sqrt(2.) * imf_disp);
+    pow_alpha_m_imf_max = pow(m_imf_max, 2.0 - alpha);
+    pow_alpha_m_imf_break = pow(m_imf_break, 2.0 - alpha);
+    pow_alpha_m_star_high = pow(m_star_high, 2.0 - alpha);
+    norm_ratio = pow(m_imf_break, (1 - alpha)) * imf_disp * sqrt(2.0 * pi) / exp(-(arg_m_imf_break * arg_m_imf_break));
+    total_star_mass = ((2. - alpha) * norm_ratio) * exp(imf_mu + imf_disp * imf_disp / 2) * (erf(arg_m_imf_break - imf_disp / sqrt(2.)) - erf(arg_m_imf_min - imf_disp / sqrt(2.))) + pow_alpha_m_imf_max - pow_alpha_m_imf_break
+    mass_highmass_stars = pow_alpha_m_imf_max - pow_alpha_m_star_high
+    fstar_high = mass_highmass_stars / total_star_mass
+    return fstar_high
+
+if __name__ == "__main__":
+    ## make histogram for each plotfile
+    parser = argparse.ArgumentParser()
+    parser.add_argument("plotfiles", nargs='*')
+    args = parser.parse_args()
+
+    for my_plotfile in args.plotfiles:
+        # load data
+        ds = yt.load(my_plotfile)
+        field_prefix, field_name = zip(*ds.field_list)
+
+        if 'StochasticStellarPop_particles' not in field_prefix: 
+            continue   
+
+        ad = ds.all_data()
+        mass = ad[('StochasticStellarPop_particles', 'particle_real_comp0')]
+        vx = ad[('StochasticStellarPop_particles', 'particle_real_comp1')]
+        vy = ad[('StochasticStellarPop_particles', 'particle_real_comp2')]
+        vz = ad[('StochasticStellarPop_particles', 'particle_real_comp3')]
+        birth_time = ad[('StochasticStellarPop_particles', 'particle_real_comp4')].value
+        death_time = ad[('StochasticStellarPop_particles', 'particle_real_comp5')].value
+
+        # filter by age
+        print(f"current time: {ds.current_time.value/seconds_in_yr/1.0e6:f} Myr")
+        age = np.ones_like(birth_time) * ds.current_time.value - birth_time
+        age_cut = 3.0e6 * seconds_in_yr
+        mass = mass[age < age_cut]
+        print(f"number of young stars (< 3 Myr): {len(mass)}")
+        
+        # sum mass below, above the mass cut
+        mass_cut = 8.0 * Msun
+        total_mass = np.sum(mass)
+        total_low_mass = np.sum(mass[mass < mass_cut])
+        total_high_mass = np.sum(mass[mass >= mass_cut])
+        print(f"total mass of low mass stars: {total_low_mass / Msun:e} Msun")
+        print(f"total mass of high mass stars: {total_high_mass / Msun:e} Msun")
+        print(f"high mass / total mass: {total_high_mass / total_mass:.3f}")
+        print(f"(expected) high mass / total mass: {compute_fstar_high():.3f}")
+
+        # make histogram of stellar masses
+        mass_in_Msun = mass / Msun
+
+        min_mass = 0.1 # Msun
+        max_mass = 120. # Msun
+        nbins = 20
+        my_bins = np.logspace(np.log10(min_mass), np.log10(max_mass), nbins)
+        counts, bin_edges = np.histogram(mass_in_Msun.value, bins=my_bins, density=False) # returns counts
+        bin_cen = 0.5*(bin_edges[:-1] + bin_edges[1:])
+        dM = bin_edges[1:] - bin_edges[:-1]
+        dN_dM = counts / dM
+        dNdM_err = np.sqrt(counts) / dM
+
+        alpha = -2.35 # Salpeter dN/dM power-law slope
+        norm = 1e6 # arbitrary
+        m = np.logspace(0, np.log10(max_mass), nbins)
+        plt.plot(m, norm * m**alpha, '--', label='Salpeter IMF')
+        plt.errorbar(bin_cen, dN_dM, yerr=dNdM_err, label='stellar masses')
+        plt.legend()
+        plt.xscale('log')
+        plt.yscale('log')
+        plt.ylabel(r'$dN/dM$')
+        plt.xlabel(r'stellar mass ($M_{\odot}$)')
+        plt.tight_layout()
+        plt.savefig("stellar_mf.pdf")
