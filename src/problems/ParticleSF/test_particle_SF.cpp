24d8f49a0f78832908ee96d859e468d5a66d8606
ChongChong He <chongchong.he@anu.edu.au>
2025-05-03 14:01:13 +1000

    update IC, high density cell at center

--- src/problems/ParticleSF/test_particle_SF.cpp
+++ src/problems/ParticleSF/test_particle_SF.cpp
@@ -30,6 +30,9 @@ const double T0 = 10.0;		  // K
 const double CV = 1. / (gamma_ - 1.) / mu * C::k_B;
 const double year = 3.15576e+07; // in seconds
 
+const double sf_cell_density = 1.0e5 * C::m_p; // g cm^-3
+const double sf_cell_loc = 1.0; // in x,y,z direction, cm
+
 template <> struct Particle_Traits<SinkProblem> {
 	// static constexpr ParticleSwitch particle_switch = ParticleSwitch::None;
 	static constexpr ParticleSwitch particle_switch = ParticleSwitch::Sink;
@@ -61,10 +64,19 @@ template <> void QuokkaSimulation<SinkProblem>::setInitialConditionsOnGrid(quokk
 	const amrex::Box &indexRange = grid_elem.indexRange_;
 	const amrex::Array4<double> &state_cc = grid_elem.array_;
 	const double rho_e = CV * T0 * rho0;
+	const auto prob_lo = geom[0].ProbLoArray();
+	const auto dx = geom[0].CellSizeArray();
 
 	// loop over the grid and set the initial condition
 	amrex::ParallelFor(indexRange, [=] AMREX_GPU_DEVICE(int i, int j, int k) {
-		state_cc(i, j, k, HydroSystem<SinkProblem>::density_index) = rho0;
+		const double x = prob_lo[0] + (i * dx[0]);
+		const double y = prob_lo[1] + (j * dx[1]);
+		const double z = prob_lo[2] + (k * dx[2]);
+		if (x < sf_cell_loc && x + dx[0] > sf_cell_loc && y < sf_cell_loc && y + dx[1] > sf_cell_loc && z < sf_cell_loc && z + dx[2] > sf_cell_loc) {
+			state_cc(i, j, k, HydroSystem<SinkProblem>::density_index) = sf_cell_density;
+		} else {
+			state_cc(i, j, k, HydroSystem<SinkProblem>::density_index) = rho0;
+		}
 		state_cc(i, j, k, HydroSystem<SinkProblem>::x1Momentum_index) = 0.0;
 		state_cc(i, j, k, HydroSystem<SinkProblem>::x2Momentum_index) = 0.0;
 		state_cc(i, j, k, HydroSystem<SinkProblem>::x3Momentum_index) = 0.0;
