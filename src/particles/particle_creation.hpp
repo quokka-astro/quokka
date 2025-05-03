dfb308eb50dca1793b51c37a2926d1eb93d58cce
ChongChong He <chongchong.he@anu.edu.au>
2025-05-03 16:20:23 +1000

    cleanup

--- src/particles/particle_creation.hpp
+++ src/particles/particle_creation.hpp
@@ -224,10 +224,6 @@ template <> struct ParticleCreationTraits<ParticleType::StochasticStellarPop> {
 			const amrex::Real cell_volume = AMREX_D_TERM(dx[0], *dx[1], *dx[2]);
 			const amrex::Real cell_density = state_arr(i, j, k, HydroSystem<problem_t>::density_index);
 
-			if (i == 16 && j == 16 && k == 16) {
-				amrex::Print() << "cell_density: " << cell_density << "\n";
-			}
-
 			const amrex::Real cs = HydroSystem<problem_t>::ComputeSoundSpeed(state_arr, i, j, k);
 			const amrex::Real LambdaJ = cs / std::sqrt(C::Gconst * cell_density);
 			const amrex::Real t_ff = std::sqrt(3.0 * M_PI / (32.0 * C::Gconst * cell_density));
