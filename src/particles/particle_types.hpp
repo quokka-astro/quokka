7e35af387414a541abe877827a3667139335ec48
Aditi Vijayan <aditi.vijayan@anu.edu.au>
2025-05-07 08:09:23 +1000

    add comment explaining LowMassStar

--- src/particles/particle_types.hpp
+++ src/particles/particle_types.hpp
@@ -157,7 +157,7 @@ template <typename problem_t> using CICRadParticleIterator = amrex::ParIter<CICR
 //-------------------- Stellar evolution stage enum --------------------
 
 // Enum for particle evolution stages. This is designed to be shared among several particle types. However, not all particle types will use all stages.
-// - LowMassStar: singular low mass stars
+// - LowMassStar: singular low mass star
 // - SNProgenitor: singular high-mass stars (> 8 Msun). Depending on the SF and SN scheme, these stars will either explode as supernovae in
 //   the end of their lifetime unconditionally, or conditionally according to their evolutional track.
 // - SNRemnant: Supernova remnant stage
