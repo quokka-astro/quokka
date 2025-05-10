9689b335f82945c889e85a954d5be9889d14d2f9
ChongChong He <chongchong.he@anu.edu.au>
2025-05-11 00:39:36 +1000

    update tidy.sh

--- scripts/tidy.sh
+++ scripts/tidy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BUILD_DIR="$1"
-COMPARETO="${2:-HEAD}"  # Default to HEAD if not specified
+COMPARETO="${2:-changed}"  # Default to HEAD if not specified
 
 # get the directory of this script
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -10,11 +10,13 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CURRENT_BRANCH=$(git branch --show-current)
 
 # run clang-tidy on the diff
-if [ "$COMPARETO" = "HEAD" ]; then
+if [ "$COMPARETO" = "changed" ]; then
+    git diff -U0 HEAD | python "$SCRIPT_DIR/tidy.py" -p1 -path "$BUILD_DIR"
+elif [ "$COMPARETO" = "previous" ]; then
     git diff -U0 HEAD^ | python "$SCRIPT_DIR/tidy.py" -p1 -path "$BUILD_DIR"
 elif [ "$COMPARETO" = "origin" ]; then
     git diff -U0 "origin/$CURRENT_BRANCH" | python "$SCRIPT_DIR/tidy.py" -p1 -path "$BUILD_DIR"
 else
-    echo "Invalid COMPARETO argument. Use 'HEAD' or 'origin'"
+    echo "Invalid COMPARETO argument. Use 'changed', 'previous' or 'origin'"
     exit 1
 fi
