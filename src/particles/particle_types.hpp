61301a3dbe3bb5b20dbfc8e959d6f5e47552a280
Ben Wibking <ben@wibking.com>
2025-05-18 17:13:58 -0400

    use AMREX_ENUM for SNScheme

--- src/particles/particle_types.hpp
+++ src/particles/particle_types.hpp
@@ -78,12 +78,12 @@ enum class ParticleType {
 };
 
 // Enum for SN schemes: ThermalOnly, ThermalAndMomentum
-enum class SNScheme {
+AMREX_ENUM(SNScheme, // NOLINT
 	SN_thermal_only,			// pure thermal
 	SN_thermal_or_thermal_momentum,		// pure thermal (RM<1) or thermal+momentum (RM>=1)
 	SN_thermal_kinetic_or_thermal_momentum, // thermal+kinetic (RM<1) or thermal+momentum (RM>=1)
 	SN_pure_kinetic_or_thermal_momentum	// pure kinetic (RM<1) or thermal+momentum (RM>=1)
-};
+);
 
 //-------------------- Radiation particles --------------------
 
@@ -324,9 +324,7 @@ inline void particleParmParse()
 	pp.query("sink_particle_use_uniform_kernel", sink_particle_use_uniform_kernel);
 
 	// Handle SNScheme enum
-	int sn_scheme_int = static_cast<int>(SN_scheme);
-	pp.query("SN_scheme", sn_scheme_int);
-	SN_scheme = static_cast<SNScheme>(sn_scheme_int);
+	pp.query("SN_scheme", SN_scheme);
 
 	// Handle integer verbose flag
 	pp.query("verbose", particle_verbose);
