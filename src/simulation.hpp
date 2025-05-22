quokka-astro/quokka

commit 8d3d2e4433230dd3dbda3431c942bfb129b3ed56

Add plotfilemf_fc (#1071)
### Description
Adds data files for face-centered variables in subdirectories to
pltNNNNN files. The regular plotfileMF and associated functions are
appended with _cc, and the new functions use _fc.

New files: 
src/problems/HydroWaveFC/hydro_wave_fc_.cpp, hydro_wave_fc.hpp,
CMakeLists.txt
tests/hydro_wave_fc.in

Modified files:
src/simulation.hpp
src/QuokkaSimulation.hpp
src/problems/CMakeLists.txt

### Related issues
N/A
 

### Checklist
_Before this pull request can be reviewed, all of these tasks should be
completed. Denote completed tasks with an `x` inside the square brackets
`[ ]` in the Markdown source below:_
- [x] I have added a description (see above).
- [ ] I have added a link to any related issues (if applicable; see
above).
- [ ] I have read the [Contributing
Guide](https://github.com/quokka-astro/quokka/blob/development/CONTRIBUTING.md).
- [x] I have added tests for any new physics that this PR adds to the
code.
- [x] *(For quokka-astro org members)* I have manually triggered the GPU
tests with the magic comment `/azp run`.

---------

Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

src/simulation.hpp
@@ -10,6 +10,7 @@
 /// timestepping, solving, and I/O of a simulation.
 
 // c++ headers
+#include "AMReX_String.H"
 #include <cfenv>
 #include <cmath>
 #include <cstdio>
@@ -336,8 +337,11 @@ template <typename problem_t> class AMRSimulation : public amrex::AmrCore
 	[[nodiscard]] auto PlotFileName(int lev) const -> std::string;
 	[[nodiscard]] auto CustomPlotFileName(const char *base, int lev) const -> std::string;
 	[[nodiscard]] auto GetPlotfileVarNames() const -> amrex::Vector<std::string>;
-	[[nodiscard]] auto PlotFileMF(int included_ghosts) -> amrex::Vector<amrex::MultiFab>;
-	[[nodiscard]] auto PlotFileMFAtLevel(int lev, int included_ghosts) -> amrex::MultiFab;
+	[[nodiscard]] auto GetPlotfileVarNames_fc() const -> std::array<amrex::Vector<std::string>, AMREX_SPACEDIM>;
+	[[nodiscard]] auto PlotFileMF_cc(int included_ghosts) -> amrex::Vector<amrex::MultiFab>;
+	[[nodiscard]] auto PlotFileMFAtLevel_cc(int lev, int included_ghosts) -> amrex::MultiFab;
+	[[nodiscard]] auto PlotFileMF_fc(int nghost_fc_) -> std::array<amrex::Vector<amrex::MultiFab>, AMREX_SPACEDIM>;
+	[[nodiscard]] auto PlotFileMFAtLevel_fc(int lev, int idim, int nghost_fc_) -> amrex::MultiFab;
 	void createDiagnostics();
 	void updateDiagnostics();
 	void doDiagnostics();
@@ -393,7 +397,8 @@ template <typename problem_t> class AMRSimulation : public amrex::AmrCore
 	int nghost_cc_ = 4;						    // PPM needs nghost >= 3, PPM+flattening needs nghost >= 4
 	int nghost_fc_ = Physics_Traits<problem_t>::is_mhd_enabled ? 4 : 2; // 4 needed for MHD, otherwise only 2 for tracer particles
 	amrex::Vector<std::string> componentNames_cc_;
-	amrex::Vector<std::string> componentNames_fc_;
+	amrex::Vector<std::string> componentNames_fc_flat_;
+	std::array<amrex::Vector<std::string>, AMREX_SPACEDIM> componentNames_fc_;
 	amrex::Vector<std::string> derivedNames_;
 	bool areInitialConditionsDefined_ = false;
 
@@ -2371,7 +2376,7 @@ void AMRSimulation<problem_t>::AverageFCToCC(amrex::MultiFab &mf_cc, const amrex
 	amrex::Gpu::streamSynchronize();
 }
 
-template <typename problem_t> auto AMRSimulation<problem_t>::PlotFileMFAtLevel(const int lev, const int included_ghosts) -> amrex::MultiFab
+template <typename problem_t> auto AMRSimulation<problem_t>::PlotFileMFAtLevel_cc(const int lev, const int included_ghosts) -> amrex::MultiFab
 {
 	// Combine state_new_cc_[lev] and derived variables in a new MF
 	const int ncomp_cc = state_new_cc_[lev].nComp();
@@ -2423,14 +2428,51 @@ template <typename problem_t> auto AMRSimulation<problem_t>::PlotFileMFAtLevel(c
 	return plotMF;
 }
 
+template <typename problem_t> auto AMRSimulation<problem_t>::PlotFileMFAtLevel_fc(const int lev, int idim, const int nghost_fc_) -> amrex::MultiFab
+{
+	int comp = 0;
+	int nvar_dim_tot_fc = 0;
+	if constexpr (Physics_Indices<problem_t>::nvarPerDim_fc > 0) {
+		nvar_dim_tot_fc = Physics_Indices<problem_t>::nvarPerDim_fc;
+	}
+	const int ncomp_plotMF_fc = nvar_dim_tot_fc;
+
+	amrex::MultiFab plotMF_fc(grids[lev], dmap[lev], ncomp_plotMF_fc, nghost_fc_);
+
+	// Fill ghost zones for state_new_fc_
+	if constexpr (Physics_Indices<problem_t>::nvarPerDim_fc > 0) {
+		fillBoundaryConditions(state_new_fc_[lev][idim], state_new_fc_[lev][idim], lev, tNew_[lev], quokka::centering::fc,
+				       static_cast<quokka::direction>(idim), InterpHookNone, InterpHookNone, FillPatchType::fillpatch_function);
+	}
+
+	// copy data from face-centred state variables
+	for (int i = 0; i < ncomp_plotMF_fc; i++) {
+		amrex::MultiFab::Copy(plotMF_fc, state_new_fc_[lev][idim], i, comp, 1, nghost_fc_);
+		comp++;
+	}
+	return plotMF_fc;
+}
+
 // put together an array of multifabs for writing
-template <typename problem_t> auto AMRSimulation<problem_t>::PlotFileMF(const int included_ghosts) -> amrex::Vector<amrex::MultiFab>
+template <typename problem_t> auto AMRSimulation<problem_t>::PlotFileMF_cc(const int included_ghosts) -> amrex::Vector<amrex::MultiFab>
 {
 	amrex::Vector<amrex::MultiFab> r;
 	for (int i = 0; i <= finest_level; ++i) {
-		r.push_back(PlotFileMFAtLevel(i, included_ghosts));
+		r.push_back(PlotFileMFAtLevel_cc(i, included_ghosts));
 	}
 	return r;
+}
+
+template <typename problem_t> auto AMRSimulation<problem_t>::PlotFileMF_fc(const int nghost_fc_) -> std::array<amrex::Vector<amrex::MultiFab>, AMREX_SPACEDIM>
+{
+	std::array<amrex::Vector<amrex::MultiFab>, AMREX_SPACEDIM> r_fc;
+	for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
+		for (int i = 0; i <= finest_level; ++i) {
+			r_fc[idim].push_back(PlotFileMFAtLevel_fc(i, idim, nghost_fc_));
+		}
+	}
+
+	return r_fc;
 }
 
 template <typename problem_t> void AMRSimulation<problem_t>::createDiagnostics()
@@ -2496,7 +2538,7 @@ template <typename problem_t> void AMRSimulation<problem_t>::doDiagnostics()
 	if (computeVars) {
 		for (int lev{0}; lev <= finestLevel(); ++lev) {
 			diagMFVec[lev] = std::make_unique<amrex::MultiFab>(grids[lev], dmap[lev], m_diagVars.size(), 1);
-			amrex::MultiFab const mf = PlotFileMFAtLevel(lev, std::min(nghost_cc_, nghost_fc_));
+			amrex::MultiFab const mf = PlotFileMFAtLevel_cc(lev, std::min(nghost_cc_, nghost_fc_));
 			auto const varnames = GetPlotfileVarNames();
 
 			for (int v{0}; v < m_diagVars.size(); ++v) {
@@ -2605,11 +2647,24 @@ template <typename problem_t> auto AMRSimulation<problem_t>::GetPlotfileVarNames
 	varnames.insert(varnames.end(), componentNames_cc_.begin(), componentNames_cc_.end());
 	if constexpr (Physics_Indices<problem_t>::nvarTotal_fc > 0) {
 		for (int icomp = 0; icomp < Physics_Indices<problem_t>::nvarTotal_fc; ++icomp) {
-			varnames.push_back(componentNames_fc_[icomp]);
+			varnames.push_back(componentNames_fc_flat_[icomp]);
 		}
 	}
 	varnames.insert(varnames.end(), derivedNames_.begin(), derivedNames_.end());
 	return varnames;
+}
+
+template <typename problem_t> auto AMRSimulation<problem_t>::GetPlotfileVarNames_fc() const -> std::array<amrex::Vector<std::string>, AMREX_SPACEDIM>
+{
+	std::array<amrex::Vector<std::string>, AMREX_SPACEDIM> varnames_fc; // nvarTotal or perDim?
+	if constexpr (Physics_Indices<problem_t>::nvarTotal_fc > 0) {
+		for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
+			for (int icomp = 0; icomp < Physics_Indices<problem_t>::nvarPerDim_fc; ++icomp) {
+				varnames_fc[idim].push_back(componentNames_fc_[idim][icomp]);
+			}
+		}
+	}
+	return varnames_fc;
 }
 
 // write plotfile to disk
@@ -2628,22 +2683,34 @@ template <typename problem_t> void AMRSimulation<problem_t>::WritePlotFile()
 #else
 	int included_ghosts = std::min(nghost_cc_, nghost_fc_);
 #endif
-	amrex::Vector<amrex::MultiFab> mf = PlotFileMF(included_ghosts);
-	amrex::Vector<const amrex::MultiFab *> mf_ptr = amrex::GetVecOfConstPtrs(mf); // NOLINT(misc-const-correctness)
+	amrex::Vector<amrex::MultiFab> mf_cc = PlotFileMF_cc(included_ghosts);
+	amrex::Vector<const amrex::MultiFab *> mf_cc_ptr = amrex::GetVecOfConstPtrs(mf_cc); // NOLINT(misc-const-correctness)
 
 	const std::string &plotfilename = PlotFileName(istep[0]);
 	auto varnames = GetPlotfileVarNames();
-
-	// write plotfile
 	amrex::Print() << "Writing plotfile " << plotfilename << "\n";
 
 #ifdef QUOKKA_USE_OPENPMD
 	// TODO(bwibking): write particles using openPMD
-	quokka::OpenPMDOutput::WriteFile(varnames, finest_level + 1, mf_ptr, Geom(), plot_file, tNew_[0], istep[0]);
+	quokka::OpenPMDOutput::WriteFile(varnames, finest_level + 1, mf_cc_ptr, Geom(), plot_file, tNew_[0], istep[0]);
 	WriteMetadataFile(plotfilename + ".yaml");
 #else
-	amrex::WriteMultiLevelPlotfile(plotfilename, finest_level + 1, mf_ptr, varnames, Geom(), tNew_[0], istep, refRatio());
+	amrex::WriteMultiLevelPlotfile(plotfilename, finest_level + 1, mf_cc_ptr, varnames, Geom(), tNew_[0], istep, refRatio());
+	if constexpr (Physics_Indices<problem_t>::nvarTotal_fc > 0) {
+		std::array<amrex::Vector<amrex::MultiFab>, AMREX_SPACEDIM> mf_fc = PlotFileMF_fc(nghost_fc_);
+		std::vector<std::string> dimNames = {"x", "y", "z"};
+		auto varnames_fc = GetPlotfileVarNames_fc();
+		for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
+			amrex::Vector<const amrex::MultiFab *> mf_fc_ptr = amrex::GetVecOfConstPtrs(mf_fc[idim]);
+			auto plotfilename_base = plotfilename + "/fc_vars/" + dimNames[idim];
+			const std::string &plotfilename_fc = CustomPlotFileName(plotfilename_base.c_str(), istep[0]);
+			auto varnames_fc_dim = varnames_fc[idim];
+			amrex::WriteMultiLevelPlotfile(plotfilename_fc, finest_level + 1, mf_fc_ptr, varnames_fc_dim, Geom(), tNew_[0], istep, refRatio());
+			WriteMetadataFile(plotfilename_fc + "/metadata.yaml");
+		}
+	}
 	WriteMetadataFile(plotfilename + "/metadata.yaml");
+
 #ifdef AMREX_PARTICLES
 	// write particles
 	if (do_tracers != 0) {

src/QuokkaSimulation.hpp
@@ -9,6 +9,7 @@
 /// \brief Implements classes and functions to organise the overall setup,
 /// timestepping, solving, and I/O of a simulation for radiation moments.
 
+#include "grid.hpp"
 #include "hydro/EOS.hpp"
 #include <array>
 #include <iostream>
@@ -87,6 +88,7 @@ template <typename problem_t> class QuokkaSimulation : public AMRSimulation<prob
 	using AMRSimulation<problem_t>::BCs_cc_;
 	using AMRSimulation<problem_t>::BCs_fc_;
 	using AMRSimulation<problem_t>::componentNames_cc_;
+	using AMRSimulation<problem_t>::componentNames_fc_flat_;
 	using AMRSimulation<problem_t>::componentNames_fc_;
 	using AMRSimulation<problem_t>::cflNumber_;
 	using AMRSimulation<problem_t>::fillBoundaryConditions;
@@ -346,12 +348,14 @@ template <typename problem_t> void QuokkaSimulation<problem_t>::defineComponentN
 
 	// add face-centered velocities
 	for (int idim = 0; idim < AMREX_SPACEDIM; idim++) {
-		componentNames_fc_.push_back({quokka::face_dir_str[idim] + "-RiemannSolverVelocity"});
+		componentNames_fc_flat_.push_back({quokka::face_dir_str[idim] + "-RiemannSolverVelocity"});  // rename to _fc_flatten_
+		componentNames_fc_[idim].push_back({quokka::face_dir_str[idim] + "-RiemannSolverVelocity"}); // array for fc_
 	}
 	// add mhd state variables
 	if constexpr (Physics_Traits<problem_t>::is_mhd_enabled) {
 		for (int idim = 0; idim < AMREX_SPACEDIM; idim++) {
-			componentNames_fc_.push_back({quokka::face_dir_str[idim] + "-BField"});
+			componentNames_fc_flat_.push_back({quokka::face_dir_str[idim] + "-BField"});
+			componentNames_fc_[idim].push_back({quokka::face_dir_str[idim] + "-BField"});
 		}
 	}
 }
@@ -798,6 +802,36 @@ template <typename problem_t> void QuokkaSimulation<problem_t>::computeAfterEvol
 		const double rel_error = err_norm / sol_norm;
 		errorNorm_ = rel_error;
 		amrex::Print() << "Relative rms L1 error norm = " << rel_error << '\n';
+		if constexpr (Physics_Traits<problem_t>::is_mhd_enabled) {
+			for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
+				amrex::Print() << "Checking fc-quantities in the " << idim << " direction\n";
+				const int ncomp = state_new_fc_[0][idim].nComp();
+				const int nghost = state_new_fc_[0][idim].nGrow();
+				amrex::MultiFab state_ref_level0(amrex::convert(boxArray(0), amrex::IntVect::TheDimensionVector(idim)), DistributionMap(0),
+								 ncomp, nghost);
+
+				// compute error norm
+				amrex::MultiFab residual(amrex::convert(boxArray(0), amrex::IntVect::TheDimensionVector(idim)), DistributionMap(0), ncomp,
+							 nghost);
+				amrex::MultiFab::Copy(residual, state_ref_level0, 0, 0, ncomp, nghost);
+				amrex::MultiFab::Saxpy(residual, -1., state_new_fc_[0][idim], 0, 0, ncomp, nghost);
+
+				amrex::Real sol_norm = 0.;
+				amrex::Real err_norm = 0.;
+				// compute rms of each component
+				for (int n = 0; n < ncomp; ++n) {
+					sol_norm += std::pow(state_ref_level0.norm1(n), 2);
+					err_norm += std::pow(residual.norm1(n), 2);
+				}
+				sol_norm = std::sqrt(sol_norm);
+				err_norm = std::sqrt(err_norm);
+
+				const double rel_error = err_norm / sol_norm;
+				errorNorm_ = rel_error;
+				amrex::Print() << "Relative rms L1 error norm = " << rel_error << ", with err_norm = " << err_norm
+					       << " and sol_norm = " << sol_norm << "\n";
+			}
+		}
 	}
 	amrex::Print() << '\n';
 

src/problems/CMakeLists.txt
@@ -18,6 +18,7 @@ add_subdirectory(HydroShuOsher)
 add_subdirectory(HydroSMS)
 add_subdirectory(HydroVacuum)
 add_subdirectory(HydroWave)
+add_subdirectory(HydroWaveFC)
 add_subdirectory(HydroQuirk)
 add_subdirectory(HydroHighMach)
 add_subdirectory(RayleighTaylor2D)

src/problems/HydroWaveFC/CMakeLists.txt
@@ -1,0 +1,7 @@
+add_executable(test_hydro_wave_fc test_hydro_wave_fc.cpp ../../util/fextract.cpp ${QuokkaObjSources})
+
+if(AMReX_GPU_BACKEND MATCHES "CUDA")
+    setup_target_for_cuda_compilation(test_hydro_wave_fc)
+endif()
+
+add_test(NAME HydroWave COMMAND test_hydro_wave_fc hydro_wave_fc.in ${QuokkaTestParams} WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/tests)

src/problems/HydroWaveFC/test_hydro_wave_fc.cpp
@@ -1,0 +1,152 @@
+//==============================================================================
+// TwoMomentRad - a radiation transport library for patch-based AMR codes
+// Copyright 2020 Benjamin Wibking.
+// Released under the MIT license. See LICENSE file included in the GitHub repo.
+//==============================================================================
+/// \file test_hydro_wave.cpp
+/// \brief Defines a test problem for a linear hydro wave.
+///
+
+#include <valarray>
+
+#include "AMReX_Array.H"
+#include "AMReX_Array4.H"
+#include "AMReX_REAL.H"
+
+#include "QuokkaSimulation.hpp"
+#include "hydro/hydro_system.hpp"
+#include "test_hydro_wave_fc.hpp"
+#include "util/fextract.hpp"
+
+struct WaveProblemFC {
+};
+
+template <> struct quokka::EOS_Traits<WaveProblemFC> {
+	static constexpr double gamma = 5. / 3.;
+	static constexpr double mean_molecular_weight = C::m_u;
+};
+
+template <> struct Physics_Traits<WaveProblemFC> {
+	// cell-centred
+	static constexpr bool is_hydro_enabled = true;
+	static constexpr int numMassScalars = 0;		     // number of mass scalars
+	static constexpr int numPassiveScalars = numMassScalars + 0; // number of passive scalars
+	static constexpr bool is_radiation_enabled = false;
+	// face-centred
+	static constexpr bool is_mhd_enabled = false;
+	static constexpr int nGroups = 1; // number of radiation groups
+	static constexpr UnitSystem unit_system = UnitSystem::CGS;
+};
+
+constexpr double rho0 = 1.0;					      // background density
+constexpr double P0 = 1.0 / quokka::EOS_Traits<WaveProblemFC>::gamma; // background pressure
+constexpr double v0 = 0.;					      // background velocity
+constexpr double amp = 1.0e-6;					      // perturbation amplitude
+
+AMREX_GPU_DEVICE void computeWaveSolution(int i, int j, int k, amrex::Array4<amrex::Real> const &state, amrex::GpuArray<amrex::Real, AMREX_SPACEDIM> const &dx,
+					  amrex::GpuArray<amrex::Real, AMREX_SPACEDIM> const &prob_lo)
+{
+	const amrex::Real x_L = prob_lo[0] + (i + static_cast<amrex::Real>(0.0)) * dx[0];
+	const amrex::Real x_R = prob_lo[0] + (i + static_cast<amrex::Real>(1.0)) * dx[0];
+	const amrex::Real A = amp;
+
+	const quokka::valarray<double, 3> R = {1.0, -1.0, 1.5}; // right eigenvector of sound wave
+	const quokka::valarray<double, 3> U_0 = {rho0, rho0 * v0, P0 / (quokka::EOS_Traits<WaveProblemFC>::gamma - 1.0) + 0.5 * rho0 * std::pow(v0, 2)};
+	const quokka::valarray<double, 3> dU = (A * R / (2.0 * M_PI * dx[0])) * (std::cos(2.0 * M_PI * x_L) - std::cos(2.0 * M_PI * x_R));
+
+	double const rho = U_0[0] + dU[0];
+	double const xmom = U_0[1] + dU[1];
+	double const Etot = U_0[2] + dU[2];
+	double const Eint = Etot - 0.5 * (xmom * xmom) / rho;
+
+	state(i, j, k, HydroSystem<WaveProblemFC>::density_index) = rho;
+	state(i, j, k, HydroSystem<WaveProblemFC>::x1Momentum_index) = xmom;
+	state(i, j, k, HydroSystem<WaveProblemFC>::x2Momentum_index) = 0;
+	state(i, j, k, HydroSystem<WaveProblemFC>::x3Momentum_index) = 0;
+	state(i, j, k, HydroSystem<WaveProblemFC>::energy_index) = Etot;
+	state(i, j, k, HydroSystem<WaveProblemFC>::internalEnergy_index) = Eint;
+}
+
+template <> void QuokkaSimulation<WaveProblemFC>::setInitialConditionsOnGrid(quokka::grid const &grid_elem)
+{
+	// extract variables required from the geom object
+	amrex::GpuArray<amrex::Real, AMREX_SPACEDIM> const dx = grid_elem.dx_;
+	amrex::GpuArray<amrex::Real, AMREX_SPACEDIM> const prob_lo = grid_elem.prob_lo_;
+	const amrex::Box &indexRange = grid_elem.indexRange_;
+	const amrex::Array4<double> &state_cc = grid_elem.array_;
+	const int ncomp_cc = Physics_Indices<WaveProblemFC>::nvarTotal_cc;
+	// loop over the grid and set the initial condition
+	amrex::ParallelFor(indexRange, [=] AMREX_GPU_DEVICE(int i, int j, int k) {
+		for (int n = 0; n < ncomp_cc; ++n) {
+			state_cc(i, j, k, n) = 0; // fill unused components with zeros
+		}
+		computeWaveSolution(i, j, k, state_cc, dx, prob_lo);
+	});
+}
+
+auto problem_main() -> int
+{
+	// Based on the ATHENA test page:
+	// https://www.astro.princeton.edu/~jstone/Athena/tests/linear-waves/linear-waves.html
+
+	// Problem parameters
+	const double CFL_number = 0.1;
+	const double max_time = 1.0;
+	const int max_timesteps = 1;
+
+	// Problem initialization
+	const int ncomp_cc = Physics_Indices<WaveProblemFC>::nvarTotal_cc;
+	amrex::Vector<amrex::BCRec> BCs_cc(ncomp_cc);
+	for (int n = 0; n < ncomp_cc; ++n) {
+		for (int i = 0; i < AMREX_SPACEDIM; ++i) {
+			BCs_cc[n].setLo(i, amrex::BCType::int_dir); // periodic
+			BCs_cc[n].setHi(i, amrex::BCType::int_dir);
+		}
+	}
+
+	QuokkaSimulation<WaveProblemFC> sim(BCs_cc);
+
+	sim.cflNumber_ = CFL_number;
+	sim.stopTime_ = max_time;
+	sim.maxTimesteps_ = max_timesteps;
+	sim.plotfileInterval_ = 1;
+	sim.do_tracers = 1;
+
+	// set initial conditions
+	sim.setInitialConditions();
+	auto [pos_exact, val_exact] = fextract(sim.state_new_cc_[0], sim.geom[0], 0, 0.5);
+
+	// Main time loop
+	sim.evolve();
+
+	auto [position, values] = fextract(sim.state_new_cc_[0], sim.geom[0], 0, 0.5);
+	auto const nx = static_cast<int>(position.size());
+	std::vector<double> const xs = position;
+
+	// compute error norm
+	amrex::Real err_sq = 0.;
+	for (int n = 0; n < QuokkaSimulation<WaveProblemFC>::ncompHydro_; ++n) {
+		if (n == HydroSystem<WaveProblemFC>::internalEnergy_index) {
+			continue;
+		}
+		amrex::Real dU_k = 0.;
+		for (int i = 0; i < nx; ++i) {
+			// Δ Uk = ∑i |Uk,in - Uk,i0| / Nx
+			const amrex::Real U_k0 = val_exact.at(n)[i];
+			const amrex::Real U_k1 = values.at(n)[i];
+			dU_k += std::abs(U_k1 - U_k0) / static_cast<double>(nx);
+		}
+		// ε = || Δ U || = [&sum_k (Δ Uk)2]^{1/2}
+		err_sq += dU_k * dU_k;
+	}
+	const amrex::Real epsilon = std::sqrt(err_sq);
+	amrex::Print() << "rms of component-wise L1 error norms = " << epsilon << '\n';
+
+	const double err_tol = 1.0e-8; // for Nx = 100
+	int status = 0;
+	if (epsilon > err_tol) {
+		status = 1;
+	}
+
+	return status;
+}

src/problems/HydroWaveFC/test_hydro_wave_fc.hpp
@@ -1,0 +1,23 @@
+#ifndef TEST_HYDRO_WAVE_FC_HPP_ // NOLINT
+#define TEST_HYDRO_WAVE_FC_HPP_
+//==============================================================================
+// TwoMomentRad - a radiation transport library for patch-based AMR codes
+// Copyright 2020 Benjamin Wibking.
+// Released under the MIT license. See LICENSE file included in the GitHub repo.
+//==============================================================================
+/// \file test_hydro_wave_fc.hpp
+/// \brief Defines a test problem for a linear hydro wave with face centering
+///
+
+// external headers
+#ifdef HAVE_PYTHON
+#include "util/matplotlibcpp.h"
+#endif
+#include <fmt/format.h>
+
+// internal headers
+#include "hydro/hydro_system.hpp"
+
+// function definitions
+
+#endif // TEST_HYDRO_WAVE_FC_HPP_

tests/hydro_wave_fc.in
@@ -1,0 +1,32 @@
+# *****************************************************************
+# Problem size and geometry
+# *****************************************************************
+geometry.prob_lo     =  0.0  0.0  0.0 
+geometry.prob_hi     =  1.0  1.0  1.0
+geometry.is_periodic =  1    1    1
+
+# *****************************************************************
+# VERBOSITY
+# *****************************************************************
+amr.v              = 1       # verbosity in Amr
+
+# *****************************************************************
+# Resolution and refinement
+# *****************************************************************
+amr.n_cell          = 100 4 4
+amr.max_level       = 0 # number of levels = max_level + 1
+
+amr.max_grid_size_x = 100
+amr.max_grid_size_y = 4
+amr.max_grid_size_z = 4
+amr.blocking_factor = 4
+
+do_reflux = 0
+do_subcycle = 0
+
+plotfile_interval = 1
+checkpoint_interval = -1
+cfl = 0.3
+stop_time = 1.0
+max_timesteps = 1
+do_tracers = 1

commit 82bdf261d1f9930abb1cb997637fe5c655b0e054

Fix bug caused by function rename in #1071 (#1091)
### Description
Fixes a bug introduced by renaming `PlotFileMF` to `PlotFileMF_cc` in
https://github.com/quokka-astro/quokka/pull/1071.

### Related issues
N/A

### Checklist
_Before this pull request can be reviewed, all of these tasks should be
completed. Denote completed tasks with an `x` inside the square brackets
`[ ]` in the Markdown source below:_
- [x] I have added a description (see above).
- [x] I have added a link to any related issues (if applicable; see
above).
- [x] I have read the [Contributing
Guide](https://github.com/quokka-astro/quokka/blob/development/CONTRIBUTING.md).
- [ ] I have added tests for any new physics that this PR adds to the
code.
- [ ] *(For quokka-astro org members)* I have manually triggered the GPU
tests with the magic comment `/azp run`.

src/simulation.hpp
@@ -2600,7 +2600,7 @@ template <typename problem_t> void AMRSimulation<problem_t>::RenderAscent()
 
 	// combine multifabs
 	const int included_ghosts = std::min(nghost_cc_, nghost_fc_);
-	amrex::Vector<amrex::MultiFab> mf_overlapping = PlotFileMF(included_ghosts);
+	amrex::Vector<amrex::MultiFab> mf_overlapping = PlotFileMF_cc(included_ghosts);
 	amrex::Vector<const amrex::MultiFab *> mf_overlapping_ptr = amrex::GetVecOfConstPtrs(mf_overlapping);
 	amrex::Vector<std::string> varnames;
 	varnames.insert(varnames.end(), componentNames_cc_.begin(), componentNames_cc_.end());