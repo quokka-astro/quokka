ad1ad1c90564b0fc7a081bd161b3871f0d026e92
Aditi Vijayan <aditi.vijayan@anu.edu.au>
2025-05-23 08:10:51 +1000

    print prob star formation

--- src/problems/ParticleSF/test_particle_SF.cpp
+++ src/problems/ParticleSF/test_particle_SF.cpp
@@ -147,7 +147,8 @@ auto problem_main() -> int
 	const double exp_Mstar_high_mean = 19.39;
 	const double exp_fstar_high = 0.220;
 	const amrex::Real t_ff = std::sqrt(3.0 * M_PI / (32.0 * C::Gconst * rho0));
-	const amrex::Real prob_star_formation = eps_ff * sim.initDt_ / eps_star / t_ff;
+	const amrex::Real prob_star_formation = (eps_ff/eps_star) * sim.initDt_ / t_ff;
+	amrex::Print() << "prob_star_formation = " << prob_star_formation << "\n";
 
 	const amrex::Real particle_mass = rho0 * cell_volume * eps_star * sim.initDt_ / t_ff;
 	const amrex::Real m_high_tot = particle_mass * exp_fstar_high;
