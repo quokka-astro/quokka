5b0b7be21a72341cc4601bfe15da8b6a1197cdc9
ChongChong He <chongchong.he@anu.edu.au>
2025-05-26 21:24:00 +1000

    fix mistakes by clang-tidy autofix

--- src/problems/RadMatterCouplingRSLA/test_radiation_matter_coupling_rsla.cpp
+++ src/problems/RadMatterCouplingRSLA/test_radiation_matter_coupling_rsla.cpp
@@ -68,16 +68,16 @@ template <> AMREX_GPU_HOST_DEVICE auto RadSystem<CouplingProblem>::ComputeFluxMe
 
 [[maybe_unused]] static constexpr int nmscalars_ = Physics_Traits<CouplingProblem>::numMassScalars;
 template <>
-AMREX_GPU_HOST_DEVICE auto quokka::EOS<CouplingProblem>::ComputeTgasFromEint(const double /*rho*/ /*rho*/ const double Egas,
-									     std::optional<amrex::GpuArray<amrex::Real, nmscalars_>> const & /*massScalars*/ /*massScalars*/
+AMREX_GPU_HOST_DEVICE auto quokka::EOS<CouplingProblem>::ComputeTgasFromEint(const double /*rho*/, const double Egas,
+									     std::optional<amrex::GpuArray<amrex::Real, nmscalars_>> const & /*massScalars*/)
     -> double
 {
 	return std::pow(4.0 * Egas / alpha_SuOlson, 1. / 4.);
 }
 
 template <>
-AMREX_GPU_HOST_DEVICE auto quokka::EOS<CouplingProblem>::ComputeEintFromTgas(const double /*rho*/ /*rho*/ const double Tgas,
-									     std::optional<amrex::GpuArray<amrex::Real, nmscalars_>> const & /*massScalars*/ /*massScalars*/
+AMREX_GPU_HOST_DEVICE auto quokka::EOS<CouplingProblem>::ComputeEintFromTgas(const double /*rho*/, const double Tgas,
+									     std::optional<amrex::GpuArray<amrex::Real, nmscalars_>> const & /*massScalars*/)
     -> double
 {
 	return (alpha_SuOlson / 4.0) * std::pow(Tgas, 4);
--- src/problems/RadTophat/test_radiation_tophat.cpp
+++ src/problems/RadTophat/test_radiation_tophat.cpp
@@ -88,7 +88,7 @@ quokka::EOS<TophatProblem>::ComputeTgasFromEint(const double rho, const double E
 template <>
 AMREX_FORCE_INLINE AMREX_GPU_HOST_DEVICE auto
 quokka::EOS<TophatProblem>::ComputeEintFromTgas(const double rho, const double Tgas,
-						std::optional<amrex::GpuArray<amrex::Real, nmscalars_>> const & /*massScalars*/ /*massScalars*/ -> double
+						std::optional<amrex::GpuArray<amrex::Real, nmscalars_>> const & /*massScalars*/) -> double
 {
 	return rho * c_v * Tgas;
 }
