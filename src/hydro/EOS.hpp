50e586757c530cca96acee8e309a0186632795bc
Ben Wibking <ben@wibking.com>
2025-07-13 06:01:39 -0400

    ok, we need one line EOS.hpp change

--- src/hydro/EOS.hpp
+++ src/hydro/EOS.hpp
@@ -39,7 +39,6 @@ template <typename problem_t> struct EOS_Traits {
 template <typename problem_t> class EOS
 {
       private:
-	static constexpr amrex::Real gamma_ = EOS_Traits<problem_t>::gamma;
 	static constexpr amrex::Real mean_molecular_weight_ = EOS_Traits<problem_t>::mean_molecular_weight;
 
       public:
@@ -67,6 +66,8 @@ template <typename problem_t> class EOS
 	[[nodiscard]] AMREX_FORCE_INLINE AMREX_GPU_HOST_DEVICE static auto
 	ComputeSoundSpeed(amrex::Real rho, amrex::Real Pressure, std::optional<amrex::GpuArray<amrex::Real, nmscalars_>> const &massScalars = {})
 	    -> amrex::Real;
+
+        static constexpr amrex::Real gamma_ = EOS_Traits<problem_t>::gamma; // needed for HLLD solver
 
 	static constexpr amrex::Real boltzmann_constant_ = []() constexpr {
 		if constexpr (Physics_Traits<problem_t>::unit_system == UnitSystem::CGS) {
