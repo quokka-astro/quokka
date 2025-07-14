dacd93d87515235f34703ea54250d9da4f63fd31
ChongChong He <chongchong.he@anu.edu.au>
2025-07-14 14:05:02 +1000

    add MHD + Radiation check

--- src/QuokkaSimulation.hpp
+++ src/QuokkaSimulation.hpp
@@ -179,6 +179,8 @@ template <typename problem_t> class QuokkaSimulation : public AMRSimulation<prob
 
 	void initialize()
 	{
+		static_assert(!(Physics_Traits<problem_t>::is_mhd_enabled && Physics_Traits<problem_t>::is_radiation_enabled),
+			      "MHD + Radiation is not supported yet.");
 #if (AMREX_SPACEDIM != 3)
 		static_assert(!(Physics_Traits<problem_t>::is_mhd_enabled), "MHD is only supported in 3D.");
 #endif // (AMREX_SPACEDIM != 3)
