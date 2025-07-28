9054a001fd72bd4f3e3200439681febf91edf107
ChongChong He <chongchong.he@anu.edu.au>
2025-07-29 00:06:02 +1000

    make a copy of streaming test to DustDyn

--- src/problems/DustDyn/test_dust_dyn.cpp
+++ src/problems/DustDyn/test_dust_dyn.cpp
@@ -1,3 +1,8 @@
+//==============================================================================
+// TwoMomentRad - a radiation transport library for patch-based AMR codes
+// Copyright 2020 Benjamin Wibking.
+// Released under the MIT license. See LICENSE file included in the GitHub repo.
+//==============================================================================
 /// \file test_radiation_streaming.cpp
 /// \brief Defines a test problem for radiation in the free-streaming regime.
 ///
