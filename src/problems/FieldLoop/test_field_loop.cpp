d3af44d0f0517eae6217f29b1570978e5c36fa58
Ben Wibking <ben@wibking.com>
2025-07-31 08:58:48 -0400

    Update src/problems/FieldLoop/test_field_loop.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/problems/FieldLoop/test_field_loop.cpp
+++ src/problems/FieldLoop/test_field_loop.cpp
@@ -158,7 +158,7 @@ template <> void QuokkaSimulation<FieldLoop>::ComputeDerivedVar(int lev, std::st
 
 		amrex::ParallelFor(mf, [=] AMREX_GPU_DEVICE(int bx, int i, int j, int k) noexcept {
 			// Compute divergence using finite differences
-			amrex::Real divB = (Bx_arr[bx](i + 1, j, k, Physics_Indices<FieldLoop>::mhdFirstIndex) -
+			amrex::Real const divB = (Bx_arr[bx](i + 1, j, k, Physics_Indices<FieldLoop>::mhdFirstIndex) -
 					    Bx_arr[bx](i, j, k, Physics_Indices<FieldLoop>::mhdFirstIndex)) /
 					       dx[0] +
 					   (By_arr[bx](i, j + 1, k, Physics_Indices<FieldLoop>::mhdFirstIndex) -
