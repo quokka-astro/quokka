29ecb355cf2160e394733d7be0852a223d5f1471
Ben Wibking <ben@wibking.com>
2025-08-14 15:40:09 -0400

    add syncs around emf_reg_->Reflux() for debugging

--- src/simulation.hpp
+++ src/simulation.hpp
@@ -1633,8 +1633,10 @@ template <typename problem_t> void AMRSimulation<problem_t>::timeStepWithSubcycl
 
 			// update magnetic field based on coarse-fine EMF mismatch
 			if constexpr (Physics_Traits<problem_t>::is_mhd_enabled) {
+				amrex::Gpu::streamSynchronizeAll();
 				// NOLINTNEXTLINE(readability-container-data-pointer)
 				emf_reg_[lev + 1]->Reflux({AMREX_D_DECL(&state_new_fc_[lev][0], &state_new_fc_[lev][1], &state_new_fc_[lev][2])});
+				amrex::Gpu::streamSynchronizeAll();
 			}
 		}
 		AverageDownTo(lev); // average lev+1 down to lev
