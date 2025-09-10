de73aec7a5f638d9892bc19207a1c2fcb807fe2c
ChongChong He <chongchong.he@anu.edu.au>
2025-09-10 18:27:25 +1000

    update shocktube

--- src/problems/HydroShocktube/test_hydro_shocktube.cpp
+++ src/problems/HydroShocktube/test_hydro_shocktube.cpp
@@ -17,6 +17,7 @@
 #include <fstream>
 #include <string>
 #include <unordered_map>
+#include "util/BC.hpp"
 
 #include "AMReX_BC_TYPES.H"
 
@@ -355,17 +356,7 @@ auto problem_main() -> int
 	const double max_time = 0.4;
 	const int max_timesteps = 8000;
 
-	// Problem initialization
-	const int ncomp_cc = Physics_Indices<ShocktubeProblem>::nvarTotal_cc;
-	amrex::Vector<amrex::BCRec> BCs_cc(ncomp_cc);
-	for (int n = 0; n < ncomp_cc; ++n) {
-		BCs_cc[0].setLo(0, amrex::BCType::ext_dir); // Dirichlet
-		BCs_cc[0].setHi(0, amrex::BCType::ext_dir);
-		for (int i = 1; i < AMREX_SPACEDIM; ++i) {
-			BCs_cc[n].setLo(i, amrex::BCType::int_dir); // periodic
-			BCs_cc[n].setHi(i, amrex::BCType::int_dir);
-		}
-	}
+	auto BCs_cc = quokka::BC<ShocktubeProblem>(amrex::BCType::ext_dir, amrex::BCType::int_dir, amrex::BCType::int_dir);
 
 	QuokkaSimulation<ShocktubeProblem> sim(BCs_cc);
 
