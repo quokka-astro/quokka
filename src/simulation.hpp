8e46f4acec06cacc2ab5db8066c3f43425826b22
ChongChong He <chongchong.he@anu.edu.au>
2025-09-12 16:58:12 +1000

    set 1 0 0 in BinaryOrbitAMR.in

--- src/simulation.hpp
+++ src/simulation.hpp
@@ -1394,11 +1394,6 @@ template <typename problem_t> void AMRSimulation<problem_t>::calculateGpotAllLev
 			}
 		}
 
-		// Assert valid periodic dimension combinations
-		AMREX_ALWAYS_ASSERT_WITH_MESSAGE(
-		    num_periodic_dims == 0 || num_periodic_dims == 2 || num_periodic_dims == 3,
-		    fmt::format("Invalid periodic boundary configuration. Only 0, 2, or 3 periodic dimensions are allowed, got {}", num_periodic_dims));
-
 		if (verbose) {
 			amrex::Print() << bc_description << "\n";
 		}
