7d16fc67fd3f15c59e8e5472fe6a96011f3750be
ChongChong He <chongchong.he@anu.edu.au>
2025-09-14 11:07:18 +1000

    comment

--- src/simulation.hpp
+++ src/simulation.hpp
@@ -1479,8 +1479,8 @@ template <typename problem_t> void AMRSimulation<problem_t>::calculateGpotAllLev
 
 		// check for NaN
 		for (int lev = 0; lev <= finest_level; ++lev) {
-			AMREX_ALWAYS_ASSERT_WITH_MESSAGE(!phi[lev].contains_nan(), fmt::format("NaN detected in phi at level {} after Poisson solve",
-											       lev)); // NOTE: this fails when multiple levels are fully refined
+ 			// NOTE: this fails when multiple levels are fully refined when open boundary condition is used.
+			AMREX_ALWAYS_ASSERT_WITH_MESSAGE(!phi[lev].contains_nan(), fmt::format("NaN detected in phi at level {} after Poisson solve", lev));
 		}
 	}
 #endif
