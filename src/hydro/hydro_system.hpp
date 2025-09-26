fab8caa4412183f37d237bc5f6dceb05dc045356
ChongChong He <chongchonghe99@gmail.com>
2025-09-27 00:15:44 +1000

    fix error in debug.yml (#1338)
    ### Description
    
    @BenWibking This was a mistake. I don't know when was that mistake made.
    The cmake-amr64-debug.yml workflow is not actually building in Release
    mode. It seems like for a long time no debug tests have been running on
    github. After debug is actually turned on, the debug tests failed. I had
    to make some change about face-centered variables, and now all 1D tests
    in debug mode passed.
    
    ### Related issues
    None.
    
    ### Checklist
    _Before this pull request can be reviewed, all of these tasks should be
    completed. Denote completed tasks with an `x` inside the square brackets
    `[ ]` in the Markdown source below:_
    - [ ] I have added a description (see above).
    - [ ] I have added a link to any related issues (if applicable; see
    above).
    - [ ] I have read the [Contributing
    Guide](https://github.com/quokka-astro/quokka/blob/development/CONTRIBUTING.md).
    - [ ] I have added tests for any new physics that this PR adds to the
    code.
    - [ ] *(For quokka-astro org members)* I have manually triggered the GPU
    tests with the magic comment `/azp run`.

--- src/hydro/hydro_system.hpp
+++ src/hydro/hydro_system.hpp
@@ -163,14 +163,23 @@ void HydroSystem<problem_t>::ConservedToPrimitive(amrex::MultiFab const &cons_cc
 	amrex::ParallelFor(cons_cc_mf, ng, [=] AMREX_GPU_DEVICE(int bx, int i, int j, int k) {
 		// First-capture the magnetic field arrays by accessing them early
 		// This forces NVCC to capture them before the constexpr if block
-		[[maybe_unused]] auto fc_x0_ref = cons_fc_x0[bx];
+		std::remove_cv_t<std::remove_reference_t<decltype(cons_fc_x0[bx])>> fc_x0_ref{};
+		if (Physics_Traits<problem_t>::is_mhd_enabled) {
+			// If not wrapped in this if clause, this will fail in Debug mode due to nan values
+			fc_x0_ref = cons_fc_x0[bx];
+		}
 #if AMREX_SPACEDIM >= 2
-		[[maybe_unused]] auto fc_x1_ref = cons_fc_x1[bx];
+		std::remove_cv_t<std::remove_reference_t<decltype(cons_fc_x1[bx])>> fc_x1_ref{};
+		if (Physics_Traits<problem_t>::is_mhd_enabled) {
+			fc_x1_ref = cons_fc_x1[bx];
+		}
 #endif
 #if AMREX_SPACEDIM == 3
-		[[maybe_unused]] auto fc_x2_ref = cons_fc_x2[bx];
+		std::remove_cv_t<std::remove_reference_t<decltype(cons_fc_x2[bx])>> fc_x2_ref{};
+		if (Physics_Traits<problem_t>::is_mhd_enabled) {
+			fc_x2_ref = cons_fc_x2[bx];
+		}
 #endif
-
 		const amrex::Real rho = cons_cc[bx](i, j, k, density_index);
 		const amrex::Real px = cons_cc[bx](i, j, k, x1Momentum_index);
 		const amrex::Real py = cons_cc[bx](i, j, k, x2Momentum_index);
@@ -988,8 +997,14 @@ void HydroSystem<problem_t>::ComputeFluxes(amrex::MultiFab &x1Flux_mf, amrex::Mu
 	amrex::IntVect ng{AMREX_D_DECL(nghost_vel, nghost_vel, nghost_vel)};
 	amrex::ParallelFor(x1Flux_mf, ng, [=] AMREX_GPU_DEVICE(int bx, int i_in, int j_in, int k_in) {
 		// first capture
-		[[maybe_unused]] const auto x1ConsVar_fc_ref = x1ConsVar_fc_in[bx];
-		[[maybe_unused]] const auto x1FSpds_ref = x1FSpds_in[bx];
+		[[maybe_unused]] std::remove_cv_t<std::remove_reference_t<decltype(x1ConsVar_fc_in[bx])>> x1ConsVar_fc_ref{};
+		if (Physics_Traits<problem_t>::is_mhd_enabled) {
+			x1ConsVar_fc_ref = x1ConsVar_fc_in[bx];
+		}
+		[[maybe_unused]] std::remove_cv_t<std::remove_reference_t<decltype(x1FSpds_in[bx])>> x1FSpds_ref{};
+		if (Physics_Traits<problem_t>::is_mhd_enabled) {
+			x1FSpds_ref = x1FSpds_in[bx];
+		}
 
 		quokka::Array4View<const amrex::Real, DIR> x1LeftState(x1LeftState_in[bx]);
 		quokka::Array4View<const amrex::Real, DIR> x1RightState(x1RightState_in[bx]);
