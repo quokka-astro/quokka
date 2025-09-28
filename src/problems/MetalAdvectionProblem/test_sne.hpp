928ada18f6ffde9bf882bea1b0ef3352796ccd09
Aditi Vijayan <aditi.vijayan@anu.edu.au>
2025-09-29 08:03:30 +1000

    add metal adv problem to the repo

---
+++ src/problems/MetalAdvectionProblem/test_sne.hpp
@@ -0,0 +1,24 @@
+#ifndef TEST_SNE_ // NOLINT
+#define TEST_SNE_
+//==============================================================================
+// TwoMomentRad - a radiation transport library for patch-based AMR codes
+// Copyright 2020 Benjamin Wibking.
+// Released under the MIT license. See LICENSE file included in the GitHub repo.
+//==============================================================================
+/// \file test_hydro3d_blast.hpp
+/// \brief Defines a test problem for a 3D explosion.
+///
+
+// external headers
+#include "fundamental_constants.H"
+#include <fmt/format.h>
+
+constexpr double yr_to_s = 3.154e7;
+constexpr double Myr = 1.e6 * yr_to_s;
+constexpr double pc = C::parsec;
+constexpr double kpc = 1.e3 * pc;
+constexpr double kmps = 1.e5;
+
+// internal headers
+
+#endif // TEST_SNE_
