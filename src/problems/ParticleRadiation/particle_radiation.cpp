c5d5cbe26dc154bc6a96ca77c69a97e514d2ef26
ChongChong He <chongchong.he@anu.edu.au>
2025-10-12 01:18:09 +1100

    enable PE and increase dust_thresh to 1e-4

--- src/problems/ParticleRadiation/particle_radiation.cpp
+++ src/problems/ParticleRadiation/particle_radiation.cpp
@@ -62,8 +62,8 @@ template <> struct Physics_Traits<ParticleRadiationProblem> {
 
 template <> struct ISM_Traits<ParticleRadiationProblem> {
 	static constexpr bool enable_dust_gas_thermal_coupling_model = true;
-	static constexpr double gas_dust_coupling_threshold = 1.0e-6;
-	static constexpr bool enable_photoelectric_heating = false;
+	static constexpr double gas_dust_coupling_threshold = 1.0e-4;
+	static constexpr bool enable_photoelectric_heating = true;
 };
 
 template <> struct RadSystem_Traits<ParticleRadiationProblem> {
@@ -78,6 +78,20 @@ template <> struct RadSystem_Traits<ParticleRadiationProblem> {
 	};
 	static constexpr OpacityModel opacity_model = OpacityModel::piecewise_constant_opacity;
 };
+
+template <>
+AMREX_GPU_HOST_DEVICE auto RadSystem<ParticleRadiationProblem>::DefinePhotoelectricHeatingE1Derivative(amrex::Real const /*temperature*/,
+											     amrex::Real const num_density) -> amrex::Real
+{
+	// Values in cgs units from Bate & Keto (2015), Eq. 26.
+	const double epsilon = 0.05; // default efficiency factor for cold molecular clouds
+	const double ref_J_ISR = 5.29e-14; // reference value for the ISR in erg cm^3
+	const double coeff = 1.33e-24;
+	return coeff * epsilon * num_density / ref_J_ISR; // s^-1
+
+	// constant rate for testing
+	// return PE_rate;
+}
 
 template <>
 AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE auto
