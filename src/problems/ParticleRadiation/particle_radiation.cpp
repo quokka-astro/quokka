ecf3a515bda52d02457da14c5ee74044a37b9d32
ChongChong He <chongchong.he@anu.edu.au>
2025-10-12 11:12:55 +1100

    restore

--- src/problems/ParticleRadiation/particle_radiation.cpp
+++ src/problems/ParticleRadiation/particle_radiation.cpp
@@ -10,7 +10,7 @@
 #include "fundamental_constants.H"
 #include "hydro/hydro_system.hpp"
 #include "particles/particle_update.hpp"
-#include "radiation/radiation_dust_system.hpp"
+#include "radiation/radiation_system.hpp"
 #include "util/BC.hpp"
 #include "util/DataTable.hpp"
 
@@ -19,17 +19,14 @@ struct ParticleRadiationProblem {
 
 constexpr double mu = 1.0 * C::m_p;
 constexpr double gamma_ = 5. / 3.;
-constexpr double rho0 = 1.0 * C::m_p; // g cm^-3
-constexpr double T0 = 1.0e3;		 // K
+constexpr double rho0 = 1.0e-8 * C::m_p; // g cm^-3
+constexpr double T0 = 10.0;		 // K
 constexpr double CV = 1. / (gamma_ - 1.) / mu * C::k_B;
-// constexpr double initial_Erad = 1.0e-30 * CV * rho0 * T0;
-// constexpr double dt_ = 0.1 * quokka::seconds_per_year;
+constexpr double initial_Erad = 1.0e-30 * CV * rho0 * T0;
+constexpr double dt_ = 0.1 * quokka::seconds_per_year;
 // constexpr double chat_over_c = 1.0e-5;
-constexpr double chat_over_c = 2000.0 * 1e5 / C::c_light; // 2000 km/s
-// constexpr double formation_time = 1.5 * dt_;
-constexpr Real arad = C::a_rad;
-constexpr Real TCMB = 2.7;		 // K, CMB temperature
-constexpr Real floor_Erad = 1e-40 * arad * TCMB * TCMB * TCMB * TCMB;
+constexpr double chat_over_c = 1.0;
+constexpr double formation_time = 1.5 * dt_;
 
 template <> struct SimulationData<ParticleRadiationProblem> {
 	std::string particles_filename = "../inputs/TestParticlesNoRad.txt";
@@ -38,6 +35,11 @@ template <> struct SimulationData<ParticleRadiationProblem> {
 template <> struct quokka::EOS_Traits<ParticleRadiationProblem> {
 	static constexpr double gamma = gamma_;
 	static constexpr double mean_molecular_weight = mu;
+};
+
+// Test enum to demonstrate type checking of particle_switch
+enum class TestEnum : unsigned int {
+	MISTAKE = 0b00000100U,
 };
 
 template <> struct Particle_Traits<ParticleRadiationProblem> {
@@ -50,62 +52,36 @@ template <> struct HydroSystem_Traits<ParticleRadiationProblem> {
 };
 
 template <> struct Physics_Traits<ParticleRadiationProblem> {
-	static constexpr bool is_self_gravity_enabled = true;
+	static constexpr bool is_self_gravity_enabled = false;
 	static constexpr bool is_hydro_enabled = true;
 	static constexpr bool is_radiation_enabled = true;
 	static constexpr bool is_mhd_enabled = false;
 	static constexpr int numMassScalars = 0;		     // number of mass scalars
 	static constexpr int numPassiveScalars = numMassScalars + 0; // number of passive scalars
-	static constexpr int nGroups = 4;			     // number of radiation groups
+	static constexpr int nGroups = 2;			     // number of radiation groups
 	static constexpr UnitSystem unit_system = UnitSystem::CGS;
-};
-
-template <> struct ISM_Traits<ParticleRadiationProblem> {
-	static constexpr bool enable_dust_gas_thermal_coupling_model = true;
-	static constexpr double gas_dust_coupling_threshold = 1.0e-4;
-	static constexpr bool enable_photoelectric_heating = true;
 };
 
 template <> struct RadSystem_Traits<ParticleRadiationProblem> {
 	static constexpr double c_hat_over_c = chat_over_c;
-	static constexpr double Erad_floor = floor_Erad;
+	static constexpr double Erad_floor = initial_Erad;
 	static constexpr int beta_order = 0;
 	static constexpr double energy_unit = C::ev2erg; // set boundary unit to eV
 	// Define radiation group boundaries for 2-group radiation
 	// Group 0: 1 eV to 100 eV, Group 1: 100 eV to 10000 eV
-	static constexpr amrex::GpuArray<double, Physics_Traits<ParticleRadiationProblem>::nGroups + 1> radBoundaries{
-		1.e-04, 1.00778140e-01, 1.00778140e+00, 5.53817071e+00, 1.e+2
-	};
+	static constexpr amrex::GpuArray<double, Physics_Traits<ParticleRadiationProblem>::nGroups + 1> radBoundaries{1.0, 100.0, 10000.0};
 	static constexpr OpacityModel opacity_model = OpacityModel::piecewise_constant_opacity;
 };
-
-template <>
-AMREX_GPU_HOST_DEVICE auto RadSystem<ParticleRadiationProblem>::DefinePhotoelectricHeatingE1Derivative(amrex::Real const /*temperature*/,
-											     amrex::Real const num_density) -> amrex::Real
-{
-	// Values in cgs units from Bate & Keto (2015), Eq. 26.
-	const double epsilon = 0.05; // default efficiency factor for cold molecular clouds
-	const double ref_J_ISR = 5.29e-14; // reference value for the ISR in erg cm^3
-	const double coeff = 1.33e-24;
-	return coeff * epsilon * num_density / ref_J_ISR; // s^-1
-
-	// constant rate for testing
-	// return PE_rate;
-}
 
 template <>
 AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE auto
 RadSystem<ParticleRadiationProblem>::DefineOpacityExponentsAndLowerValues(amrex::GpuArray<double, nGroups_ + 1> /*rad_boundaries*/, const double /*rho*/,
 									  const double /*Tgas*/) -> amrex::GpuArray<amrex::GpuArray<double, nGroups_ + 1>, 2>
 {
-	constexpr double gas_to_dust_ratio = 1.0e-3;
 	amrex::GpuArray<amrex::GpuArray<double, nGroups_ + 1>, 2> exponents_and_values{};
 	for (int i = 0; i < nGroups_ + 1; ++i) {
-		exponents_and_values[0][i] = 0.0; // power-law slopes
-	}
-	const amrex::GpuArray<double, nGroups_ + 1> dust_opacity{6e2, 1e3, 2e4, 1e5, 2e5}; // dust opacity, cm2/g. last element not used
-	for (int i = 0; i < nGroups_ + 1; ++i) {
-		exponents_and_values[1][i] = dust_opacity[i] * gas_to_dust_ratio;
+		exponents_and_values[0][i] = 0.0;     // exponent (0 = constant opacity)
+		exponents_and_values[1][i] = 1.0e-20; // opacity value (0 = optically thin)
 	}
 	return exponents_and_values;
 }
@@ -114,7 +90,7 @@ template <> void QuokkaSimulation<ParticleRadiationProblem>::createInitialStocha
 {
 	// Read particles from ASCII file. Note that this only read real components and not integer components, therefore we need to use
 	// InitSetPhyParticles to set the integer components
-	const int nreal_extra = 6 + Physics_Traits<ParticleRadiationProblem>::nGroups; // mass vx vy vz birth_time death_time lum
+	const int nreal_extra = 7; // mass vx vy vz birth_time death_time lum
 	StochasticStellarPopParticles->SetVerbose(1);
 	StochasticStellarPopParticles->InitFromAsciiFile(userData_.particles_filename, nreal_extra, nullptr);
 
@@ -135,12 +111,7 @@ template <> void QuokkaSimulation<ParticleRadiationProblem>::createInitialStocha
 			// Launch GPU kernel to set integer components
 			amrex::ParallelFor(np, [=] AMREX_GPU_DEVICE(int i) {
 				auto &p = pdata[i]; // NOLINT
-				const double death_time = p.rdata(quokka::StochasticStellarPopParticleDeathTimeIdx);
-				if (death_time <= 0.0) {
-					p.idata(0) = static_cast<int>(quokka::StellarEvolutionStage::SNRemnant);
-				} else {
-					p.idata(0) = static_cast<int>(quokka::StellarEvolutionStage::SNProgenitor);
-				}
+				p.idata(0) = static_cast<int>(quokka::StellarEvolutionStage::SNProgenitor);
 			});
 		}
 	}
@@ -154,16 +125,15 @@ template <> void QuokkaSimulation<ParticleRadiationProblem>::setInitialCondition
 	const amrex::Box &indexRange = grid_elem.indexRange_;
 	const amrex::Array4<double> &state_cc = grid_elem.array_;
 
+	const auto Erad0 = initial_Erad;
+
 	amrex::ParallelFor(indexRange, [=] AMREX_GPU_DEVICE(int i, int j, int k) {
 		const double rho = rho0;
 		const double rho_e = CV * T0 * rho;
 
-		// compute energy fractions
-		const auto Erad_g = RadSystem<ParticleRadiationProblem>::ComputeThermalRadiationMultiGroup(TCMB, RadSystem<ParticleRadiationProblem>::radBoundaries_);
-
 		// Set radiation variables
 		for (int g = 0; g < Physics_Traits<ParticleRadiationProblem>::nGroups; ++g) {
-			state_cc(i, j, k, RadSystem<ParticleRadiationProblem>::radEnergy_index + Physics_NumVars::numRadVarsPerGroup * g) = Erad_g[g];
+			state_cc(i, j, k, RadSystem<ParticleRadiationProblem>::radEnergy_index + Physics_NumVars::numRadVarsPerGroup * g) = Erad0;
 			state_cc(i, j, k, RadSystem<ParticleRadiationProblem>::x1RadFlux_index + Physics_NumVars::numRadVarsPerGroup * g) = 0;
 			state_cc(i, j, k, RadSystem<ParticleRadiationProblem>::x2RadFlux_index + Physics_NumVars::numRadVarsPerGroup * g) = 0;
 			state_cc(i, j, k, RadSystem<ParticleRadiationProblem>::x3RadFlux_index + Physics_NumVars::numRadVarsPerGroup * g) = 0;
@@ -179,10 +149,12 @@ template <> void QuokkaSimulation<ParticleRadiationProblem>::setInitialCondition
 
 auto problem_main() -> int
 {
-	auto BCs_cc = quokka::BC<ParticleRadiationProblem>(quokka::BCType::int_dir); // periodic
+	auto BCs_cc = quokka::BC<ParticleRadiationProblem>(quokka::BCType::reflecting);
 
 	// Problem initialization
 	QuokkaSimulation<ParticleRadiationProblem> sim(BCs_cc);
+	sim.initDt_ = dt_;
+	sim.maxDt_ = dt_;
 
 	// Read parameters from input file
 	const amrex::ParmParse pp("problem");
@@ -239,8 +211,48 @@ auto problem_main() -> int
 		const double change_of_total_energy = total_energy - total_energy_init;
 		amrex::Print() << "Change of total energy: " << change_of_total_energy << "\n";
 
-		const double lum_mean = change_of_total_energy / sim.tNew_[0]; // mean luminosity, erg/s
-		amrex::Print() << "Mean luminosity: " << lum_mean << " erg/s\n";
+		// Expected answer from table interpolation.
+		// Radiation is deposited into cells after the first step.
+		// The table gives luminosity values based on (mass, age) interpolation.
+		// For this test with the current table, the expected luminosity per particle is determined by table interpolation.
+		// The emission per particle from step 0 is 0.0;
+		// The emission per particle from step 1 is 2.5e20 * dt_.
+		// The emission per particle from step 2 is (2 * 2.5e20) * dt_.
+		const int n_stars = 4;
+		double L_star = NAN;
+		double change_of_total_energy_expected = NAN;
+		AMREX_ALWAYS_ASSERT_WITH_MESSAGE(sim.maxTimesteps_ == 3, "This test requires max_timesteps = 3");
+		if (rad_table_output_spacing == quokka::SpacingType::log) {
+			L_star = 3e40;
+			change_of_total_energy_expected = (1.0 + 2.0) * L_star * dt_ * n_stars;
+		} else if (rad_table_output_spacing == quokka::SpacingType::fast_log) {
+			// The stellar age won't fall exactly onto the fastlog-sampled grids, so in order to test perfect accuracy, we have to set luminosity to
+			// constant over time
+			change_of_total_energy_expected = (1.0e+41 + 1.0e+41) * dt_ * n_stars;
+		} else {
+			L_star = 2.5e40;
+			change_of_total_energy_expected = (1.0 + 2.0) * L_star * dt_ * n_stars;
+		}
+		const double change_of_total_energy_expected_group2 = change_of_total_energy_expected * 10.0;
+		const double change_all_groups = change_of_total_energy_expected + change_of_total_energy_expected_group2;
+		amrex::Print() << "Current time: " << sim.tNew_[0] << "\n";
+		amrex::Print() << "Expected change of total energy: " << change_all_groups << "\n";
+
+		const double error_rel_to_tot = std::abs(change_of_total_energy - change_all_groups) / total_energy;
+		const double error_rel_to_rad = std::abs(change_of_total_energy - change_all_groups) / change_all_groups;
+		amrex::Print() << "Relative error to total energy: " << error_rel_to_tot << "\n";
+		amrex::Print() << "Relative error to radiation energy: " << error_rel_to_rad << "\n";
+
+		// On CPUs, the error is 1e-15, close to machine accuracy. One GPUs, the error, caused by std::log or std::pow, is slight higher at 1e-14.
+		const double tolerance = rad_table_output_spacing == quokka::SpacingType::fast_log ? 1.0e-11 : 1e-13; // Tolerance relative to total energy
+		if (!(error_rel_to_tot < tolerance) || !(error_rel_to_rad < tolerance)) {
+			status = 1;
+			amrex::Print() << "Test failed: change of total energy mismatch.\n";
+		}
+
+		if (status == 0) {
+			amrex::Print() << "Test passed: change of total energy within tolerance.\n";
+		}
 	}
 
 	return status;
