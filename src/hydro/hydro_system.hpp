506dfbeab19a0da778302007ea98ba8287514056
mxchen39 <3050760682@qq.com>
2025-10-19 22:32:26 +0800

    Enforce dust density floor

--- src/hydro/hydro_system.hpp
+++ src/hydro/hydro_system.hpp
@@ -897,6 +897,16 @@ template <typename problem_t> void HydroSystem<problem_t>::EnforceLimits(amrex::
 			}
 		}
 
+		// Enforce dust density floor
+		if constexpr (Physics_Traits<problem_t>::is_dust_enabled) {
+			for (int g = 0; g < Physics_Traits<problem_t>::nDustGroups; ++g) {
+				amrex::Real dust_rho = state[bx](i, j, k, dustDensity_index + g * numDustVars_);
+				if (dust_rho < densityFloor) {
+					state[bx](i, j, k, dustDensity_index + g * numDustVars_) = densityFloor;
+				}
+			}
+		}
+
 		// Enforce temperature floor
 		if ((rho_new > std::numeric_limits<amrex::Real>::min()) && !HydroSystem<problem_t>::is_eos_isothermal()) {
 			amrex::Real const vx1 = state[bx](i, j, k, x1Momentum_index) / rho_new;
