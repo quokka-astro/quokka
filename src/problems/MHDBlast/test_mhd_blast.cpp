79171cd8a1a60175d2ac3b6c3d3c4877da0f92f8
lizmcole <lizmcole@users.noreply.github.com>
2025-11-03 17:22:07 +1100

    Update src/problems/MHDBlast/test_mhd_blast.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/problems/MHDBlast/test_mhd_blast.cpp
+++ src/problems/MHDBlast/test_mhd_blast.cpp
@@ -104,7 +104,7 @@ template <> void QuokkaSimulation<MHDBlast>::refineGrid(int lev, amrex::TagBoxAr
 		const amrex::Box &box = mfi.validbox();
 		const auto state = state_new_cc_[lev].const_array(mfi);
 		const auto tag = tags.array(mfi);
-		std::array<amrex::Array4<const amrex::Real>, AMREX_SPACEDIM> cons_fc{
+		std::array<amrex::Array4<const amrex::Real>, AMREX_SPACEDIM> const cons_fc{
 		    AMREX_D_DECL(state_fc[0].const_array(mfi), state_fc[1].const_array(mfi), state_fc[2].const_array(mfi))};
 
 		amrex::ParallelFor(box, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
