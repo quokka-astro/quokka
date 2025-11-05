4394f640e2badb7f42a92d2f96e5dceece065b34
ChongChong He <chongchong.he@anu.edu.au>
2025-11-05 20:02:59 +1100

    rename all problems

---
+++ src/problems/GravRadParticle3D/testGravRadParticle3D.cpp
@@ -0,0 +1,303 @@
+/// ile testGravRadParticle3D.cpp
+/// \brief Defines a 3D test problem for radiating particles with gravity.
+///
+
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <fmt/format.h>
+
+#include "AMReX.H"
+#include "AMReX_BCRec.H"
+#include "AMReX_BC_TYPES.H"
+#include "AMReX_Box.H"
+#include "AMReX_Vector.H"
+#include "QuokkaSimulation.hpp"
+#include "hydro/EOS.hpp"
+#include "particles/PhysicsParticles.hpp"
+#include "radiation/radiation_system.hpp"
+#include "util/BC.hpp"
+
+struct ParticleProblem {
+};
+
+constexpr int nGroups_ = 1;
+
+constexpr double erad_floor = 1.0e-15;
+constexpr double initial_Erad = erad_floor;
+constexpr double initial_Egas = 1.0e-5;
+constexpr double c = 100.0;	   // speed of light
+constexpr double chat = 2.0;	   // reduced speed of light
+constexpr double kappa0 = 1.0e-20; // opacity
+constexpr double rho0 = 1.0e-8;
+constexpr double m_H = C::m_p + C::m_e;
+
+const double lum1 = 1.0;
+
+template <> struct quokka::EOS_Traits<ParticleProblem> {
+	static constexpr double mean_molecular_weight = 1.0;
+	static constexpr double gamma = 5. / 3.;
+};
+
+template <> struct Particle_Traits<ParticleProblem> {
+	static constexpr ParticleSwitch particle_switch = ParticleSwitch::CIC | ParticleSwitch::Rad | ParticleSwitch::CICRad;
+};
+
+template <> struct Physics_Traits<ParticleProblem> {
+	// cell-centred
+	static constexpr bool is_hydro_enabled = false;
+	static constexpr int numMassScalars = 0;		     // number of mass scalars
+	static constexpr int numPassiveScalars = numMassScalars + 0; // number of passive scalars
+	static constexpr bool is_radiation_enabled = true;
+	static constexpr bool is_self_gravity_enabled = true;
+	// face-centred
+	static constexpr bool is_mhd_enabled = false;
+	static constexpr int nGroups = nGroups_; // number of radiation groups
+	static constexpr UnitSystem unit_system = UnitSystem::CONSTANTS;
+	static constexpr double boltzmann_constant = 1.0;
+	static constexpr double gravitational_constant = 1.0;
+	static constexpr double c_light = c;
+	static constexpr double radiation_constant = 1.0;
+};
+
+template <> struct RadSystem_Traits<ParticleProblem> {
+	static constexpr double c_hat_over_c = chat / c;
+	static constexpr double Erad_floor = erad_floor;
+	static constexpr int beta_order = 0;
+};
+
+template <> void QuokkaSimulation<ParticleProblem>::createInitialCICRadParticles()
+{
+	// read particles from ASCII file
+	const int nreal_extra = 6 + nGroups_; // mass vx vy vz birth_time death_time lum1
+	CICRadParticles->SetVerbose(1);
+	CICRadParticles->InitFromAsciiFile("../inputs/GravRadParticles3D.txt", nreal_extra, nullptr);
+}
+
+template <> void QuokkaSimulation<ParticleProblem>::createInitialCICParticles()
+{
+	// read particles from ASCII file - same as CICRadParticles but only mass and velocity components
+	const int nreal_extra = 4; // mass vx vy vz
+	CICParticles->SetVerbose(1);
+	CICParticles->InitFromAsciiFile("../inputs/GravRadParticles3D_cic_only.txt", nreal_extra, nullptr);
+}
+
+template <> void QuokkaSimulation<ParticleProblem>::createInitialRadParticles()
+{
+	// read particles from ASCII file - same as CICRadParticles but only birth_time, death_time, and luminosity
+	const int nreal_extra = 2 + nGroups_; // birth_time death_time lum1
+	RadParticles->SetVerbose(1);
+	RadParticles->InitFromAsciiFile("../inputs/GravRadParticles3D_rad_only.txt", nreal_extra, nullptr);
+}
+
+template <> AMREX_GPU_HOST_DEVICE auto RadSystem<ParticleProblem>::ComputePlanckOpacity(const double /*rho*/, const double /*Tgas*/) -> amrex::Real
+{
+	return kappa0;
+}
+
+template <> AMREX_GPU_HOST_DEVICE auto RadSystem<ParticleProblem>::ComputeFluxMeanOpacity(const double /*rho*/, const double /*Tgas*/) -> amrex::Real
+{
+	return kappa0;
+}
+
+template <> void QuokkaSimulation<ParticleProblem>::setInitialConditionsOnGrid(quokka::grid const &grid_elem)
+{
+	const amrex::Box &indexRange = grid_elem.indexRange_;
+	const amrex::Array4<double> &state_cc = grid_elem.array_;
+
+	const auto Erad0 = initial_Erad;
+	const auto Egas0 = initial_Egas;
+
+	// loop over the grid and set the initial condition
+	amrex::ParallelFor(indexRange, [=] AMREX_GPU_DEVICE(int i, int j, int k) {
+		state_cc(i, j, k, RadSystem<ParticleProblem>::radEnergy_index) = Erad0;
+		state_cc(i, j, k, RadSystem<ParticleProblem>::x1RadFlux_index) = 0;
+		state_cc(i, j, k, RadSystem<ParticleProblem>::x2RadFlux_index) = 0;
+		state_cc(i, j, k, RadSystem<ParticleProblem>::x3RadFlux_index) = 0;
+		state_cc(i, j, k, RadSystem<ParticleProblem>::gasEnergy_index) = Egas0;
+		state_cc(i, j, k, RadSystem<ParticleProblem>::gasDensity_index) = rho0;
+		state_cc(i, j, k, RadSystem<ParticleProblem>::gasInternalEnergy_index) = Egas0;
+		state_cc(i, j, k, RadSystem<ParticleProblem>::x1GasMomentum_index) = 0.;
+		state_cc(i, j, k, RadSystem<ParticleProblem>::x2GasMomentum_index) = 0.;
+		state_cc(i, j, k, RadSystem<ParticleProblem>::x3GasMomentum_index) = 0.;
+	});
+}
+
+template <>
+auto QuokkaSimulation<ParticleProblem>::ComputeProjections(const amrex::Direction dir) const -> std::unordered_map<std::string, amrex::BaseFab<amrex::Real>>
+{
+	std::unordered_map<std::string, amrex::BaseFab<amrex::Real>> proj;
+
+	Real const H_mass_fraction = 1.0;
+
+	// compute (total) density projection
+	proj["nH"] = quokka::diagnostics::ComputePlaneProjection<amrex::ReduceOpSum>(
+	    state_new_cc_, finestLevel(), geom, ref_ratio, dir, [=] AMREX_GPU_DEVICE(int i, int j, int k, amrex::Array4<const Real> const &state) noexcept {
+		    Real const rho = state(i, j, k, RadSystem<ParticleProblem>::gasDensity_index);
+		    return (H_mass_fraction * rho) / m_H;
+	    });
+
+	return proj;
+}
+
+auto problem_main() -> int
+{
+	// Problem parameters
+	// const int nx = 1000;
+	// const double Lx = 1.0;
+
+	// Boundary conditions
+	auto BCs_cc = quokka::BC<ParticleProblem>(quokka::BCType::reflecting);
+
+	// Problem initialization
+	QuokkaSimulation<ParticleProblem> sim(BCs_cc);
+
+	sim.radiationReconstructionOrder_ = 3; // PPM
+
+	// initialize
+	sim.setInitialConditions();
+
+	// evolve
+	sim.evolve();
+
+	int status = 0;
+
+	// compute total radiation energy
+	const double total_Erad_over_vol = sim.state_new_cc_[0].sum(RadSystem<ParticleProblem>::radEnergy_index);
+	const double dx = sim.Geom(0).CellSize(0);
+	const double dy = sim.Geom(0).CellSize(1);
+	const double dz = sim.Geom(0).CellSize(2);
+	const double dvol = dx * dy * dz;
+	const double total_Erad = total_Erad_over_vol * dvol;
+	const double t_sim = sim.tNew_[0];
+	const double t_alive = std::min(0.5, t_sim);		     // particles only live for 0.5 time units
+	double total_Erad_exact = 2.0 * lum1 * t_alive * (chat / c); // two particles with luminosity lum1
+	total_Erad_exact *= 2.0;				     // two particle system (Rad + CICRad)
+	const auto total_num_of_cells = sim.Geom(0).Domain().volume();
+	total_Erad_exact += static_cast<double>(total_num_of_cells) * dvol * initial_Erad;
+
+	const double rel_err = std::abs(total_Erad - total_Erad_exact) / total_Erad_exact;
+
+	// Compute exact location of the CICRad particles
+	// The particles are originally at (-0.5, 0) and (0.5, 0) and they move with
+	// velocity 1/sqrt(2) in the y/-y direction. The problem is designed such that
+	// the particles will move in a circle with radius 0.5
+	const double velocity = 0.5;
+	const double radius = 1.0;
+	const double theta = velocity * t_sim / radius;
+	const double exact_x = radius * std::cos(theta);
+	const double exact_y = radius * std::sin(theta);
+	const double exact_z = 0.0;
+
+	// Exact location of the CIC particles
+	const double exact_x_cic = 0.0;
+	const double exact_y_cic = 0.0;
+	const double exact_z_cic = 0.0;
+
+	// Exact location of the Rad particles
+	const double exact_x_rad = 0.3;
+	const double exact_y_rad = 0.0;
+	const double exact_z_rad = 0.0;
+
+	if (amrex::ParallelDescriptor::IOProcessor()) {
+		// Test CICRad particles
+		[[maybe_unused]] const auto [ids1, positions_cicrad, int1] =
+		    sim.particleRegister_.getParticleDescriptor(quokka::ParticleType::CICRad)->getParticleDataAtAllLevels();
+		double position_error_cicrad = 0.0;
+		double position_norm_cicrad = 0.0;
+
+		// Test CIC particles
+		[[maybe_unused]] const auto [ids2, positions_cic, int2] =
+		    sim.particleRegister_.getParticleDescriptor(quokka::ParticleType::CIC)->getParticleDataAtAllLevels();
+		double position_error_cic = 0.0;
+		const double position_norm_cic = 1.0; // set to 1.0 since the particles are exactly at the origin
+
+		// Test Rad particles
+		[[maybe_unused]] const auto [ids3, positions_rad, int3] =
+		    sim.particleRegister_.getParticleDescriptor(quokka::ParticleType::Rad)->getParticleDataAtAllLevels();
+		double position_error_rad = 0.0;
+		double position_norm_rad = 0.0;
+
+		// Test both particle types against exact solution
+		for (const auto &position : positions_cicrad) {
+			if (position[0] * exact_x > 0.0) {
+				position_error_cicrad += std::abs(position[0] - exact_x);
+				position_error_cicrad += std::abs(position[1] - exact_y);
+				position_error_cicrad += std::abs(position[2] - exact_z);
+			} else {
+				position_error_cicrad += std::abs(position[0] - (-exact_x));
+				position_error_cicrad += std::abs(position[1] - (-exact_y));
+				position_error_cicrad += std::abs(position[2] - (-exact_z));
+			}
+			position_norm_cicrad += std::abs(exact_x);
+			position_norm_cicrad += std::abs(exact_y);
+			position_norm_cicrad += std::abs(exact_z);
+		}
+
+		for (const auto &position : positions_cic) {
+			if (position[0] * exact_x > 0.0) {
+				position_error_cic += std::abs(position[0] - exact_x_cic);
+				position_error_cic += std::abs(position[1] - exact_y_cic);
+				position_error_cic += std::abs(position[2] - exact_z_cic);
+			} else {
+				position_error_cic += std::abs(position[0] - (-exact_x_cic));
+				position_error_cic += std::abs(position[1] - (-exact_y_cic));
+				position_error_cic += std::abs(position[2] - (-exact_z_cic));
+			}
+		}
+
+		for (const auto &position : positions_rad) {
+			if (position[0] * exact_x_rad > 0.0) {
+				position_error_rad += std::abs(position[0] - exact_x_rad);
+				position_error_rad += std::abs(position[1] - exact_y_rad);
+				position_error_rad += std::abs(position[2] - exact_z_rad);
+			} else {
+				position_error_rad += std::abs(position[0] - (-exact_x_rad));
+				position_error_rad += std::abs(position[1] - (-exact_y_rad));
+				position_error_rad += std::abs(position[2] - (-exact_z_rad));
+			}
+			position_norm_rad += std::abs(exact_x_rad);
+			position_norm_rad += std::abs(exact_y_rad);
+			position_norm_rad += std::abs(exact_z_rad);
+		}
+
+		const double rel_position_error_cicrad = position_error_cicrad / position_norm_cicrad;
+		const double rel_position_error_cic = position_error_cic / position_norm_cic;
+		const double rel_position_error_rad = position_error_rad / position_norm_rad;
+
+		const double rel_err_tol = 1.0e-7;
+		const double rel_position_error_tol = t_sim < 1.0 ? 2.0e-4 : 2.0e-3;
+		status = 1;
+		if (rel_err < rel_err_tol && rel_position_error_cicrad < rel_position_error_tol && rel_position_error_cic < rel_position_error_tol &&
+		    rel_position_error_rad < rel_position_error_tol) {
+			status = 0;
+			amrex::Print() << "Relative error within tolerance.\n";
+		}
+
+		amrex::Print() << "Exact positions of the CICRad particles should be: " << exact_x << ", " << exact_y << ", " << exact_z << "\n";
+		amrex::Print() << "Real positions are: \n";
+		for (const auto &position : positions_cicrad) {
+			amrex::Print() << position[0] << ", " << position[1] << ", " << position[2] << "\n";
+		}
+		amrex::Print() << "Exact positions of the CIC particles should be: " << exact_x_cic << ", " << exact_y_cic << ", " << exact_z_cic << "\n";
+		amrex::Print() << "Real positions are: \n";
+		for (const auto &position : positions_cic) {
+			amrex::Print() << position[0] << ", " << position[1] << ", " << position[2] << "\n";
+		}
+		amrex::Print() << "Exact positions of the Rad particles should be: " << exact_x_rad << ", " << exact_y_rad << ", " << exact_z_rad << "\n";
+		amrex::Print() << "Real positions are: \n";
+		for (const auto &position : positions_rad) {
+			amrex::Print() << position[0] << ", " << position[1] << ", " << position[2] << "\n";
+		}
+		amrex::Print() << "Relative L1 norm on radiation energy = " << rel_err << "\n";
+		amrex::Print() << "Relative L1 norm on CICRad particle positions = " << rel_position_error_cicrad << "\n";
+		amrex::Print() << "Relative L1 norm on CIC particle positions = " << rel_position_error_cic << "\n";
+		amrex::Print() << "Relative L1 norm on Rad particle positions = " << rel_position_error_rad << "\n";
+
+		// Cleanup and exit
+		amrex::Print() << "Finished."
+			       << "\n";
+	}
+
+	return status;
+}
---
+++ src/problems/ParticleRadiation/testParticleRadiation.cpp
@@ -0,0 +1,259 @@
+/// ile testParticleRadiation.cpp
+/// \brief Defines a test problem for radiation from particles.
+///
+
+#include "AMReX_ParallelDescriptor.H"
+#include "AMReX_ParmParse.H"
+#include "AMReX_Print.H"
+
+#include "QuokkaSimulation.hpp"
+#include "fundamental_constants.H"
+#include "hydro/hydro_system.hpp"
+#include "particles/particle_update.hpp"
+#include "radiation/radiation_system.hpp"
+#include "util/BC.hpp"
+#include "util/DataTable.hpp"
+
+struct ParticleRadiationProblem {
+};
+
+constexpr double mu = 1.0 * C::m_p;
+constexpr double gamma_ = 5. / 3.;
+constexpr double rho0 = 1.0e-8 * C::m_p; // g cm^-3
+constexpr double T0 = 10.0;		 // K
+constexpr double CV = 1. / (gamma_ - 1.) / mu * C::k_B;
+constexpr double initial_Erad = 1.0e-30 * CV * rho0 * T0;
+constexpr double dt_ = 0.1 * quokka::seconds_per_year;
+// constexpr double chat_over_c = 1.0e-5;
+constexpr double chat_over_c = 1.0;
+constexpr double formation_time = 1.5 * dt_;
+
+template <> struct SimulationData<ParticleRadiationProblem> {
+	std::string particles_filename = "../inputs/TestParticlesNoRad.txt";
+};
+
+template <> struct quokka::EOS_Traits<ParticleRadiationProblem> {
+	static constexpr double gamma = gamma_;
+	static constexpr double mean_molecular_weight = mu;
+};
+
+// Test enum to demonstrate type checking of particle_switch
+enum class TestEnum : unsigned int {
+	MISTAKE = 0b00000100U,
+};
+
+template <> struct Particle_Traits<ParticleRadiationProblem> {
+	// static constexpr ParticleSwitch particle_switch = ParticleSwitch::None;
+	static constexpr ParticleSwitch particle_switch = ParticleSwitch::StochasticStellarPop;
+};
+
+template <> struct HydroSystem_Traits<ParticleRadiationProblem> {
+	static constexpr bool reconstruct_eint = true; // need to reconstruct temperature
+};
+
+template <> struct Physics_Traits<ParticleRadiationProblem> {
+	static constexpr bool is_self_gravity_enabled = false;
+	static constexpr bool is_hydro_enabled = true;
+	static constexpr bool is_radiation_enabled = true;
+	static constexpr bool is_mhd_enabled = false;
+	static constexpr int numMassScalars = 0;		     // number of mass scalars
+	static constexpr int numPassiveScalars = numMassScalars + 0; // number of passive scalars
+	static constexpr int nGroups = 2;			     // number of radiation groups
+	static constexpr UnitSystem unit_system = UnitSystem::CGS;
+};
+
+template <> struct RadSystem_Traits<ParticleRadiationProblem> {
+	static constexpr double c_hat_over_c = chat_over_c;
+	static constexpr double Erad_floor = initial_Erad;
+	static constexpr int beta_order = 0;
+	static constexpr double energy_unit = C::ev2erg; // set boundary unit to eV
+	// Define radiation group boundaries for 2-group radiation
+	// Group 0: 1 eV to 100 eV, Group 1: 100 eV to 10000 eV
+	static constexpr amrex::GpuArray<double, Physics_Traits<ParticleRadiationProblem>::nGroups + 1> radBoundaries{1.0, 100.0, 10000.0};
+	static constexpr OpacityModel opacity_model = OpacityModel::piecewise_constant_opacity;
+};
+
+template <>
+AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE auto
+RadSystem<ParticleRadiationProblem>::DefineOpacityExponentsAndLowerValues(amrex::GpuArray<double, nGroups_ + 1> /*rad_boundaries*/, const double /*rho*/,
+									  const double /*Tgas*/) -> amrex::GpuArray<amrex::GpuArray<double, nGroups_ + 1>, 2>
+{
+	amrex::GpuArray<amrex::GpuArray<double, nGroups_ + 1>, 2> exponents_and_values{};
+	for (int i = 0; i < nGroups_ + 1; ++i) {
+		exponents_and_values[0][i] = 0.0;     // exponent (0 = constant opacity)
+		exponents_and_values[1][i] = 1.0e-20; // opacity value (0 = optically thin)
+	}
+	return exponents_and_values;
+}
+
+template <> void QuokkaSimulation<ParticleRadiationProblem>::createInitialStochasticStellarPopParticles()
+{
+	// Read particles from ASCII file. Note that this only read real components and not integer components, therefore we need to use
+	// InitSetPhyParticles to set the integer components
+	const int nreal_extra = 7; // mass vx vy vz birth_time death_time lum
+	StochasticStellarPopParticles->SetVerbose(1);
+	StochasticStellarPopParticles->InitFromAsciiFile(userData_.particles_filename, nreal_extra, nullptr);
+
+	// Using a for loop from lev = 0 to StochasticStellarPopParticles->maxLevel() won't work because not all levels necessarily have particles, and when
+	// some levels do not have particles, StochasticStellarPopParticles->GetParticles(lev) will result in a Segfault. Therefore, we loop over the actual
+	// particle container.
+	for (auto &kv : StochasticStellarPopParticles->GetParticles()) {
+		for (auto &ikv : kv) {
+			auto &particle_array = ikv.second.GetArrayOfStructs();
+			const int np = particle_array.numParticles();
+
+			if (np == 0) {
+				continue;
+			}
+
+			auto *pdata = particle_array().data();
+
+			// Launch GPU kernel to set integer components
+			amrex::ParallelFor(np, [=] AMREX_GPU_DEVICE(int i) {
+				auto &p = pdata[i]; // NOLINT
+				p.idata(0) = static_cast<int>(quokka::StellarEvolutionStage::SNProgenitor);
+			});
+		}
+	}
+
+	// Ensure GPU operations are complete
+	amrex::Gpu::streamSynchronize();
+}
+
+template <> void QuokkaSimulation<ParticleRadiationProblem>::setInitialConditionsOnGrid(quokka::grid const &grid_elem)
+{
+	const amrex::Box &indexRange = grid_elem.indexRange_;
+	const amrex::Array4<double> &state_cc = grid_elem.array_;
+
+	const auto Erad0 = initial_Erad;
+
+	amrex::ParallelFor(indexRange, [=] AMREX_GPU_DEVICE(int i, int j, int k) {
+		const double rho = rho0;
+		const double rho_e = CV * T0 * rho;
+
+		// Set radiation variables
+		for (int g = 0; g < Physics_Traits<ParticleRadiationProblem>::nGroups; ++g) {
+			state_cc(i, j, k, RadSystem<ParticleRadiationProblem>::radEnergy_index + Physics_NumVars::numRadVarsPerGroup * g) = Erad0;
+			state_cc(i, j, k, RadSystem<ParticleRadiationProblem>::x1RadFlux_index + Physics_NumVars::numRadVarsPerGroup * g) = 0;
+			state_cc(i, j, k, RadSystem<ParticleRadiationProblem>::x2RadFlux_index + Physics_NumVars::numRadVarsPerGroup * g) = 0;
+			state_cc(i, j, k, RadSystem<ParticleRadiationProblem>::x3RadFlux_index + Physics_NumVars::numRadVarsPerGroup * g) = 0;
+		}
+		state_cc(i, j, k, RadSystem<ParticleRadiationProblem>::gasEnergy_index) = rho_e;
+		state_cc(i, j, k, RadSystem<ParticleRadiationProblem>::gasDensity_index) = rho;
+		state_cc(i, j, k, RadSystem<ParticleRadiationProblem>::gasInternalEnergy_index) = rho_e;
+		state_cc(i, j, k, RadSystem<ParticleRadiationProblem>::x1GasMomentum_index) = 0.0;
+		state_cc(i, j, k, RadSystem<ParticleRadiationProblem>::x2GasMomentum_index) = 0.0;
+		state_cc(i, j, k, RadSystem<ParticleRadiationProblem>::x3GasMomentum_index) = 0.0;
+	});
+}
+
+auto problem_main() -> int
+{
+	auto BCs_cc = quokka::BC<ParticleRadiationProblem>(quokka::BCType::reflecting);
+
+	// Problem initialization
+	QuokkaSimulation<ParticleRadiationProblem> sim(BCs_cc);
+	sim.initDt_ = dt_;
+	sim.maxDt_ = dt_;
+
+	// Read parameters from input file
+	const amrex::ParmParse pp("problem");
+	pp.query("particles_filename", sim.userData_.particles_filename);
+
+	quokka::SpacingType rad_table_output_spacing = quokka::SpacingType::linear;
+	const amrex::ParmParse ppp("particles");
+	ppp.query("rad_table_output_spacing", rad_table_output_spacing);
+
+	// initialize (this will parse particle parameters and load luminosity table)
+	sim.setInitialConditions();
+
+	amrex::GpuArray<amrex::Real, AMREX_SPACEDIM> const &dx0 = sim.geom[0].CellSizeArray();
+	amrex::Real const vol = AMREX_D_TERM(dx0[0], *dx0[1], *dx0[2]);
+	// Total radiation energy in the field
+	amrex::Real total_Erad_init = 0.0;
+	for (int g = 0; g < Physics_Traits<ParticleRadiationProblem>::nGroups; ++g) {
+		total_Erad_init +=
+		    sim.state_new_cc_[0].sum(RadSystem<ParticleRadiationProblem>::radEnergy_index + Physics_NumVars::numRadVarsPerGroup * g) * vol;
+	}
+
+	// total gas energy
+	const amrex::Real total_gas_energy_init = sim.state_new_cc_[0].sum(RadSystem<ParticleRadiationProblem>::gasEnergy_index) * vol;
+
+	// set force finest level to true for test particles
+	// sim.particleRegister_.getParticleDescriptor(quokka::ParticleType::StochasticStellarPop)->setForceFinestLevel(true);
+
+	// evolve
+	sim.evolve();
+
+	// ----- Check Stochastic particles -----
+
+	// Total radiation energy in the field
+	amrex::Real total_Erad = 0.0;
+	for (int g = 0; g < Physics_Traits<ParticleRadiationProblem>::nGroups; ++g) {
+		total_Erad += sim.state_new_cc_[0].sum(RadSystem<ParticleRadiationProblem>::radEnergy_index + Physics_NumVars::numRadVarsPerGroup * g) * vol;
+	}
+
+	// total gas energy
+	const amrex::Real total_gas_energy = sim.state_new_cc_[0].sum(RadSystem<ParticleRadiationProblem>::gasEnergy_index) * vol;
+
+	int status = 0; // Initialize to success
+
+	if (amrex::ParallelDescriptor::IOProcessor()) {
+
+		// print total gas energy
+		amrex::Print() << "Total gas energy (initial): " << total_gas_energy_init << "\n";
+		amrex::Print() << "Total gas energy (final): " << total_gas_energy << "\n";
+		amrex::Print() << "Total radiation energy (initial): " << total_Erad_init / chat_over_c << "\n";
+		amrex::Print() << "Total radiation energy (final): " << total_Erad / chat_over_c << "\n";
+
+		const double total_energy_init = total_Erad_init / chat_over_c + total_gas_energy_init;
+		const double total_energy = total_Erad / chat_over_c + total_gas_energy;
+		const double change_of_total_energy = total_energy - total_energy_init;
+		amrex::Print() << "Change of total energy: " << change_of_total_energy << "\n";
+
+		// Expected answer from table interpolation.
+		// Radiation is deposited into cells after the first step.
+		// The table gives luminosity values based on (mass, age) interpolation.
+		// For this test with the current table, the expected luminosity per particle is determined by table interpolation.
+		// The emission per particle from step 0 is 0.0;
+		// The emission per particle from step 1 is 2.5e20 * dt_.
+		// The emission per particle from step 2 is (2 * 2.5e20) * dt_.
+		const int n_stars = 4;
+		double L_star = NAN;
+		double change_of_total_energy_expected = NAN;
+		AMREX_ALWAYS_ASSERT_WITH_MESSAGE(sim.maxTimesteps_ == 3, "This test requires max_timesteps = 3");
+		if (rad_table_output_spacing == quokka::SpacingType::log) {
+			L_star = 3e40;
+			change_of_total_energy_expected = (1.0 + 2.0) * L_star * dt_ * n_stars;
+		} else if (rad_table_output_spacing == quokka::SpacingType::fast_log) {
+			// The stellar age won't fall exactly onto the fastlog-sampled grids, so in order to test perfect accuracy, we have to set luminosity to
+			// constant over time
+			change_of_total_energy_expected = (1.0e+41 + 1.0e+41) * dt_ * n_stars;
+		} else {
+			L_star = 2.5e40;
+			change_of_total_energy_expected = (1.0 + 2.0) * L_star * dt_ * n_stars;
+		}
+		const double change_of_total_energy_expected_group2 = change_of_total_energy_expected * 10.0;
+		const double change_all_groups = change_of_total_energy_expected + change_of_total_energy_expected_group2;
+		amrex::Print() << "Current time: " << sim.tNew_[0] << "\n";
+		amrex::Print() << "Expected change of total energy: " << change_all_groups << "\n";
+
+		const double error_rel_to_tot = std::abs(change_of_total_energy - change_all_groups) / total_energy;
+		const double error_rel_to_rad = std::abs(change_of_total_energy - change_all_groups) / change_all_groups;
+		amrex::Print() << "Relative error to total energy: " << error_rel_to_tot << "\n";
+		amrex::Print() << "Relative error to radiation energy: " << error_rel_to_rad << "\n";
+
+		// On CPUs, the error is 1e-15, close to machine accuracy. One GPUs, the error, caused by std::log or std::pow, is slight higher at 1e-14.
+		const double tolerance = rad_table_output_spacing == quokka::SpacingType::fast_log ? 1.0e-11 : 1e-13; // Tolerance relative to total energy
+		if (!(error_rel_to_tot < tolerance) || !(error_rel_to_rad < tolerance)) {
+			status = 1;
+			amrex::Print() << "Test failed: change of total energy mismatch.\n";
+		}
+
+		if (status == 0) {
+			amrex::Print() << "Test passed: change of total energy within tolerance.\n";
+		}
+	}
+
+	return status;
+}
