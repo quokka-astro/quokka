a61def04349b695b66aa34135b983c720777c6cf
lizmcole <lizmcole@users.noreply.github.com>
2025-11-20 10:01:32 +1100

    Update src/problems/HydroHighMach/testHydroHighMach.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/problems/HydroHighMach/testHydroHighMach.cpp
+++ src/problems/HydroHighMach/testHydroHighMach.cpp
@@ -266,7 +266,7 @@ auto problem_main() -> int
 
 	const double error_tol = 0.26;
 	int status = 0;
-	amrex::Real error_norm = sim.computeErrorNorm();
+	amrex::Real const error_norm = sim.computeErrorNorm();
 	if (error_norm < error_tol || std::isnan(error_norm)) {
 		status = 1;
 	}
