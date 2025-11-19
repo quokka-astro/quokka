9ab744318fb346741b1aa64152bc3bd1d344c170
lizmcole <lizmcole@users.noreply.github.com>
2025-11-20 10:01:39 +1100

    Update src/problems/HydroLeblanc/testHydroLeblanc.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/problems/HydroLeblanc/testHydroLeblanc.cpp
+++ src/problems/HydroLeblanc/testHydroLeblanc.cpp
@@ -369,7 +369,7 @@ auto problem_main() -> int
 	// Compute test success condition
 	int status = 0;
 	const double error_tol = 0.002;
-	amrex::Real error_norm = sim.computeErrorNorm();
+	amrex::Real const error_norm = sim.computeErrorNorm();
 	if (error_norm < error_tol) {
 		status = 1;
 	}
