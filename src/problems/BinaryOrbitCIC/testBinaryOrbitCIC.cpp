17aaa6e4283463bf5853838074a8936dbd3ed273
ChongChong He <chongchong.he@anu.edu.au>
2025-11-23 17:09:04 +1100

    remove refineGrid

--- src/problems/BinaryOrbitCIC/testBinaryOrbitCIC.cpp
+++ src/problems/BinaryOrbitCIC/testBinaryOrbitCIC.cpp
@@ -150,15 +150,6 @@ template <> void QuokkaSimulation<BinaryOrbit>::computeAfterTimestep()
 	++cycle;
 }
 
-template <> void QuokkaSimulation<BinaryOrbit>::refineGrid(int lev, amrex::TagBoxArray &tags, amrex::Real /*time*/, int /*ngrow*/)
-{
-	for (amrex::MFIter mfi(state_new_cc_[lev]); mfi.isValid(); ++mfi) {
-		const amrex::Box &box = mfi.validbox();
-		const auto tag = tags.array(mfi);
-		amrex::ParallelFor(box, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept { tag(i, j, k) = amrex::TagBox::SET; });
-	}
-}
-
 auto problem_main() -> int
 {
 	// read in runtiem parameter geometry.is_periodic
