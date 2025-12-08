d9a76534b6dfedada45ee3a7cbdf4c29aded2cfd
lizmcole <lizmcole@users.noreply.github.com>
2025-12-08 13:41:52 +1100

    Update src/problems/AlfvenWaveLinearConvergence/testAlfvenWaveLinearConvergence.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/problems/AlfvenWaveLinearConvergence/testAlfvenWaveLinearConvergence.cpp
+++ src/problems/AlfvenWaveLinearConvergence/testAlfvenWaveLinearConvergence.cpp
@@ -343,7 +343,7 @@ void QuokkaSimulation<AlfvenWaveLinear>::computeReferenceSolution(amrex::MultiFa
 		auto const ncomp = ref.nComp();
 		const amrex::Real time = tNew_[0];
 
-		amrex::ParallelFor(indexRange, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+		amrex::ParallelFor(indexRange, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept -> void {
 			for (int n = 0; n < ncomp; ++n) {
 				stateExact(i, j, k, n) = 0.0; // fill unused quantities with zeros
 			}
