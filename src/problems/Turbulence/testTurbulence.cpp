26012ff604157ad2e8c10c2b746468e514285cf2
XRByte <sarkar.anish.1001@gmail.com>
2025-12-08 15:11:01 +0530

    Added dust parameters to turbulentbox.cpp

--- src/problems/Turbulence/testTurbulence.cpp
+++ src/problems/Turbulence/testTurbulence.cpp
@@ -17,7 +17,8 @@ template <> struct Physics_Traits<TurbulentBox> {
 	static constexpr bool is_radiation_enabled = false;
 	static constexpr bool is_mhd_enabled = false;
 	static constexpr bool is_self_gravity_enabled = false;
-
+	static constexpr bool is_dust_enabled = false;
+	static constexpr int nDustGroups = 1; // number of dust groups
 	static constexpr int numMassScalars = 0;
 	static constexpr int numPassiveScalars = numMassScalars + 1;
 	static constexpr int nGroups = 1; // number of radiation groups
