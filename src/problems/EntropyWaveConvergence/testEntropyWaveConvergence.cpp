912a6fd6dfb4be40de7a2cc66560c0ae1425cce4
lizmcole <lizmcole@users.noreply.github.com>
2025-12-17 09:51:28 +1100

    Update testEntropyWaveConvergence.cpp

--- src/problems/EntropyWaveConvergence/testEntropyWaveConvergence.cpp
+++ src/problems/EntropyWaveConvergence/testEntropyWaveConvergence.cpp
@@ -416,9 +416,9 @@ auto problem_main() -> int
 	quokka::richardson::applyQuietDefaults();
 
 	quokka::richardson::Parameters params{};
-	params.machine_precision_target = 2.0e-10; // limit based on delta_rho_magn
+	params.machine_precision_target = 2.0e-9; // limit based on delta_rho_magn
 	params.nx_initial = 16;
-	params.nx_max = 128;
+	params.nx_max = 64;
 	params.expected_rate = 2.0;
 	params.tolerance = 0.3;
 	params.test_name = "Entropy Wave";
