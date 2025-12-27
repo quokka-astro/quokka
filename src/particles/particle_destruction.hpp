91207ce5c023c2f47581d856bd814dafc2d30133
ChongChong He <chongchonghe99@gmail.com>
2025-12-27 12:33:52 +0800

    update fmt

--- src/particles/particle_destruction.hpp
+++ src/particles/particle_destruction.hpp
@@ -79,8 +79,9 @@ static void destroyParticlesImpl(ContainerType *container, int mass_idx, int lev
 
 			// Print the total number of particles destroyed at this time step
 			if (amrex::ParallelDescriptor::IOProcessor()) {
-				amrex::Print() << ">>>Particle destruction:\n\tTime: " << current_time << " - Destroyed " << global_total_destroyed
-					       << " particles (from level " << lev_min << " and above)\n\n";
+				if (particle_verbose > 0) {
+					amrex::Print() << fmt::format("[PARTICLES] Particle destruction: Time: {} - Destroyed {} particles (from level {} and above)\n", current_time, global_total_destroyed, lev_min);
+				}
 			}
 		}
 	}
