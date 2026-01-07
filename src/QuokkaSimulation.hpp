81159e288a6df73a63a32e025866c79c8b831eb8
ChongChong He <chongchonghe99@gmail.com>
2026-01-08 07:26:57 +1100

    Self-shielded Grackle table (#1527)
    ### Description
    Use this command to generate a Grackle cooling table with self-shielding
    (https://grackle.readthedocs.io/en/latest/Parameters.html#data-files),
    excluding photoelectric heating to allow time/space-variable
    photoelectric heating rate.
    
    ```
    ./resample_grackle_cooling_tables.py --shield --exclude_pe --output CloudyData_UVB=HM2012_shielded_resampled_noPE.h5
    ```
    
    More changes:
    - The `resample_grackle_cooling_tables.py` script will also make a phase
    plot of cooling length as a function of gas number density `n` and
    effective temperature `T/mu`.
    
    ### Related issues
    Are there any GitHub issues that are fixed by this pull request? Add a
    link to them here.
    
    ### Checklist
    _Before this pull request can be reviewed, all of these tasks should be
    completed. Denote completed tasks with an `x` inside the square brackets
    `[ ]` in the Markdown source below:_
    - [x] I have added a description (see above).
    - [x] I have added a link to any related issues (if applicable; see
    above).
    - [x] I have read the [Contributing
    Guide](https://github.com/quokka-astro/quokka/blob/development/CONTRIBUTING.md).
    - [x] I have added tests for any new physics that this PR adds to the
    code.
    - [x] *(For quokka-astro org members)* I have manually triggered the GPU
    tests with the magic comment `/azp run`.
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/QuokkaSimulation.hpp
+++ src/QuokkaSimulation.hpp
@@ -559,6 +559,7 @@ template <typename problem_t> void QuokkaSimulation<problem_t>::readParmParse()
 	}
 
 	// set cooling runtime parameters
+	bool cooling_table_include_pe = false;
 	{
 		amrex::ParmParse const hpp("cooling");
 		int alwaysReadTables = 0;
@@ -573,7 +574,7 @@ template <typename problem_t> void QuokkaSimulation<problem_t>::readParmParse()
 			if (coolingTableType_ == "resampled") {
 				// read resampled cooling tables
 				amrex::Print() << "Reading resampled cooling tables...\n";
-				quokka::ResampledCooling::readResampledData(coolingTableFilename_, resampledTables_);
+				cooling_table_include_pe = quokka::ResampledCooling::readResampledData(coolingTableFilename_, resampledTables_);
 			} else {
 				amrex::Abort("Invalid cooling table type! Only 'resampled' is supported.");
 			}
@@ -613,6 +614,9 @@ template <typename problem_t> void QuokkaSimulation<problem_t>::readParmParse()
 		pp.query("const_sfr_Msun_per_year_per_kpc2", const_sfr_Msun_per_year_per_kpc2_);
 		// It's allowed to turn on sfh and not turn on use_sfh_based_pe_heating, but the opposite is not allowed.
 		if (use_sfh_based_pe_heating_) {
+			AMREX_ALWAYS_ASSERT_WITH_MESSAGE(
+			    !cooling_table_include_pe,
+			    "When use_sfh_based_pe_heating is set to true, please use a Grackle cooling table that does NOT include photoelectric heating.");
 			AMREX_ALWAYS_ASSERT_WITH_MESSAGE(
 			    !sfh_to_pe_heating_table_filename_.empty(),
 			    "When use_sfh_based_pe_heating is set to true, a PE heating table must be specified via sfh_to_pe_heating_table");
