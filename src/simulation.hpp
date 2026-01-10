2f9b7224ce5be1d651a6075d6a9a4280cc658549
ChongChong He <chongchonghe99@gmail.com>
2026-01-10 17:03:58 +1100

    Add a new regression test: random SN in a box (#1547)
    ### Description
    Refactored RandomBlast Problem: The testRandomBlast.cpp problem was
    significantly refactored to utilize the new particle system for SN
    feedback, removing the previous stochastic sampling method and
    integrating particle initialization from a file.
    
    Particle Generation Script: Added a new Python script
    (create_particles.py) to generate initial particle distributions for
    various types (CIC, Sink, Rad, StochasticStellarPop) with options for
    spatial sampling (uniform, Gaussian, Plummer) and physical parameters.
    
    Dependent on #1546 : Supernova (SN) Explosion Tracking: Modified the C++
    particle physics core to accurately track and return the number of
    supernova explosions occurring within a simulation timestep, enhancing
    diagnostics and logging
    
    ### Checklist
    _Before this pull request can be reviewed, all of these tasks should be
    completed. Denote completed tasks with an `x` inside the square brackets
    `[ ]` in the Markdown source below:_
    - [x] I have added a description (see above).
    - [x] I have added a link to any related issues (if applicable; see
    above).
    - [x] I have read the [Contributing
    Guide](https://github.com/quokka-astro/quokka/blob/development/CONTRIBUTING.md).
    - [x] I have added tests for any new physics that this PR adds to the
    code.
    - [x] *(For quokka-astro org members)* I have manually triggered the GPU
    tests with the magic comment `/azp run`.
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/simulation.hpp
+++ src/simulation.hpp
@@ -212,6 +212,7 @@ template <typename problem_t> class AMRSimulation : public amrex::AmrCore
 	int sfh_interval_ = -1;		       // interval for the star formation history
 	amrex::Real sfh_time_interval_ = -1.0; // time interval for the star formation history
 	amrex::Real last_sfh_time_ = 0.0;
+	int sn_count_ = 0; // number of SN explosions in a step (used for diagnostics)
 
 	amrex::Real densityFloor_ = 0.0; // default
 	amrex::Real tempFloor_ = 0.0;	 // default
