368d57e2a59b61979a0d0eb4a7e00854953770f2
ChongChong He <chongchong.he@anu.edu.au>
2026-01-19 17:44:02 +1100

    revert sn-count

--- src/simulation.hpp
+++ src/simulation.hpp
@@ -212,8 +212,7 @@ template <typename problem_t> class AMRSimulation : public amrex::AmrCore
 	int sfh_interval_ = -1;		       // interval for the star formation history
 	amrex::Real sfh_time_interval_ = -1.0; // time interval for the star formation history
 	amrex::Real last_sfh_time_ = 0.0;
-	int sn_count_ = 0;	      // number of SN explosions in a step (used for diagnostics)
-	int sn_count_cumulative_ = 0; // cumulative number of SN explosions (used for diagnostics)
+	int sn_count_ = 0; // number of SN explosions in a step (used for diagnostics)
 
 	amrex::Real densityFloor_ = 0.0; // default
 	amrex::Real tempFloor_ = 0.0;	 // default
@@ -1926,8 +1925,6 @@ template <typename problem_t> void AMRSimulation<problem_t>::particleMeshInterac
 
 	// Deposit the SN particles into the MultiFab
 	const auto [num_sn_explosions, max_velocity] = particleRegister_.depositSN(state_new_cc_[lev], state_fc_ptr, lev, time, dt);
-	sn_count_ = num_sn_explosions;
-	sn_count_cumulative_ += num_sn_explosions;
 
 	// Print SN explosion count if verbose and non-zero
 	if (verbose && num_sn_explosions > 0) {
