06ca6b98b6907454a93fafde9602cd642e28a4d8
ChongChong He <chongchonghe99@gmail.com>
2026-01-26 12:04:22 +1100

    keep OpenBC the same as dev branch

--- src/simulation.hpp
+++ src/simulation.hpp
@@ -1923,11 +1923,9 @@ template <typename problem_t> void AMRSimulation<problem_t>::kickParticlesAllLev
 
 	// Set up boundary condition types for phi (only needed if applying Dirichlet BCs)
 	amrex::Vector<amrex::BCRec> phiBC(1);
-	if (needs_dirichlet_bc) {
-		for (int i = 0; i < AMREX_SPACEDIM; ++i) {
-			phiBC[0].setLo(i, BCs_cc_[Physics_Indices<problem_t>::hydroFirstIndex].lo(i));
-			phiBC[0].setHi(i, BCs_cc_[Physics_Indices<problem_t>::hydroFirstIndex].hi(i));
-		}
+	for (int i = 0; i < AMREX_SPACEDIM; ++i) {
+		phiBC[0].setLo(i, BCs_cc_[Physics_Indices<problem_t>::hydroFirstIndex].lo(i));
+		phiBC[0].setHi(i, BCs_cc_[Physics_Indices<problem_t>::hydroFirstIndex].hi(i));
 	}
 
 	// Compute accelerations and kick particles
@@ -1942,14 +1940,20 @@ template <typename problem_t> void AMRSimulation<problem_t>::kickParticlesAllLev
 
 		// Fill extended potential from existing phi
 		if (lev == 0) {
-			// Base level: copy valid cells and fill boundaries
+			// Base level: just copy real cells and and fill boundaries in periodic dimensions
 			amrex::MultiFab::Copy(phi_extended, phi[lev], 0, 0, 1, 0);
 			phi_extended.FillBoundary(geom[lev].periodicity());
 
-			// Apply Dirichlet BC (phi=0) at non-periodic boundaries for MLMG solver
+			// Apply physical boundary conditions to phi
 			if (needs_dirichlet_bc) {
+				// Apply Dirichlet BC (phi=0) at non-periodic boundaries for MLMG solver
 				amrex::GpuBndryFuncFab<setFunctorPhiZero> boundaryFunctor(setFunctorPhiZero{});
 				amrex::PhysBCFunct<amrex::GpuBndryFuncFab<setFunctorPhiZero>> phiBdryFunct(geom[lev], phiBC, boundaryFunctor);
+				phiBdryFunct(phi_extended, 0, 1, phi_extended.nGrowVect(), 0., 0);
+			} else {
+				// Apply physical boundary conditions to phi at open boundaries
+				amrex::GpuBndryFuncFab<setFunctorParticleAccel> boundaryFunctor(setFunctorParticleAccel{});
+				amrex::PhysBCFunct<amrex::GpuBndryFuncFab<setFunctorParticleAccel>> phiBdryFunct(geom[lev], phiBC, boundaryFunctor);
 				phiBdryFunct(phi_extended, 0, 1, phi_extended.nGrowVect(), 0., 0);
 			}
 		} else {
