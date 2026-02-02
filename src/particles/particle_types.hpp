a8e4beb27b43b40ab7907e1e7c1e108722622270
ChongChong He <chongchonghe99@gmail.com>
2026-02-03 10:53:07 +1100

    rename LowMassStar

--- src/particles/particle_types.hpp
+++ src/particles/particle_types.hpp
@@ -184,7 +184,7 @@ template <typename problem_t> using CICRadParticleIterator = amrex::ParIter<CICR
 // - SNRemnant: Supernova remnant stage
 // - LowMassComposite: composite of low-mass stars
 // - Removed: marked for removal
-enum class StellarEvolutionStage { LowMassStar, SNProgenitor, SNRemnant, LowMassComposite, Removed };
+enum class StellarEvolutionStage { HighMassNoneProgenitor, SNProgenitor, SNRemnant, LowMassComposite, Removed };
 
 //-------------------- Stellar population particles --------------------
 
