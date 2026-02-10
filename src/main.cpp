d18637a82b4c6cf63aeb175788fbb655cffad8da
ChongChong He <chongchonghe99@gmail.com>
2026-02-10 11:34:22 +1100

    add test: unused varaible

--- src/main.cpp
+++ src/main.cpp
@@ -75,6 +75,8 @@ auto main(int argc, char **argv) -> int
 
 	amrex::Real const start_time = amrex::ParallelDescriptor::second();
 
+  int unused_variable = 1;
+
 	int result = 0;
 	{ // objects must be destroyed before amrex::finalize, so enter new
 		// scope here to do that automatically
@@ -82,6 +84,8 @@ auto main(int argc, char **argv) -> int
 		result = problem_main();
 
 	} // destructors must be called before amrex::Finalize()
+
+  const int uninitliazed_varaible;
 
 	// compute elapsed time
 	amrex::Real elapsed_sec = amrex::ParallelDescriptor::second() - start_time;
