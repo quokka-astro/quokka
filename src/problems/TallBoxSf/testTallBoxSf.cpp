0cff67093dd3d472acce116f4441ac71d727bf4e
ChongChong He <chongchonghe99@gmail.com>
2026-02-20 15:02:37 +1100

    gpu issue in TallBoxSf

--- src/problems/TallBoxSf/testTallBoxSf.cpp
+++ src/problems/TallBoxSf/testTallBoxSf.cpp
@@ -234,6 +234,12 @@ template <> void QuokkaSimulation<TheProblem>::setInitialConditionsOnGrid(quokka
 	// Create GPU const tables for initial conditions if available
 	const auto &ic_table = userData_.ic_table.const_tables();
 
+	amrex::Real initial_scalar_density = 0.0;
+	if constexpr (Physics_Traits<TheProblem>::numPassiveScalars > 0) {
+		const amrex::Real cell_vol = AMREX_D_TERM(dx[0], *dx[1], *dx[2]);
+		initial_scalar_density = 1.0e-6 * quokka::scalar_yield_per_SN / cell_vol;
+	}
+
 	amrex::ParallelFor(indexRange, [=] AMREX_GPU_DEVICE(int i, int j, int k) {
 		amrex::Real const z = prob_lo[2] + ((k + static_cast<amrex::Real>(0.5)) * dx[2]);
 
@@ -271,11 +277,11 @@ template <> void QuokkaSimulation<TheProblem>::setInitialConditionsOnGrid(quokka
 		state_cc(i, j, k, HydroSystem<TheProblem>::internalEnergy_index) = P / (gamma - 1.);
 		state_cc(i, j, k, HydroSystem<TheProblem>::energy_index) = P / (gamma - 1.) + 0.5 * rho * (vx * vx + vy * vy + vz * vz);
 
+		const auto initial_scalar_density_d = initial_scalar_density;
+
 		// Initialize passive scalar field
 		if constexpr (Physics_Traits<TheProblem>::numPassiveScalars > 0) {
-			const amrex::Real cell_vol = AMREX_D_TERM(dx[0], *dx[1], *dx[2]);
-			const amrex::Real initial_scalar_density = 1.0e-6 * quokka::scalar_yield_per_SN / cell_vol;
-			state_cc(i, j, k, HydroSystem<TheProblem>::scalar0_index) = initial_scalar_density;
+			state_cc(i, j, k, HydroSystem<TheProblem>::scalar0_index) = initial_scalar_density_d;
 		}
 	});
 }
