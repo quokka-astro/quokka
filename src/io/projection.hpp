ab3158b05ec85f62576dbdf2c6d41f7fba180db9
Ben Wibking <ben@wibking.com>
2026-02-20 15:48:07 -0500

    use sparse reduction

--- src/io/projection.hpp
+++ src/io/projection.hpp
@@ -108,13 +108,13 @@ inline auto ComputePlaneProjectionFromMultiFab(const amrex::Vector<const amrex::
 		auto const &mask_arr = mask.const_arrays();
 		auto const &dx = geom[lev].CellSizeArray();
 
-		auto plane_pair = amrex::ReduceToPlaneMF2<amrex::ReduceOpSum>(static_cast<int>(dir), geom[lev].Domain(), *mfs[lev],
-									      [=] AMREX_GPU_DEVICE(int box_no, int i, int j, int k) -> amrex::Real {
-										      if (mask_arr[box_no](i, j, k) == 0) {
-											      return 0.0;
-										      }
-										      return dx[static_cast<int>(dir)] * mf_arr[box_no](i, j, k, comp);
-									      });
+		auto plane_pair = amrex::ReduceToPlaneMF2Patchy<amrex::ReduceOpSum>(
+		    static_cast<int>(dir), geom[lev].Domain(), *mfs[lev], [=] AMREX_GPU_DEVICE(int box_no, int i, int j, int k) -> amrex::Real {
+			    if (mask_arr[box_no](i, j, k) == 0) {
+				    return 0.0;
+			    }
+			    return dx[static_cast<int>(dir)] * mf_arr[box_no](i, j, k, comp);
+		    });
 		auto &plane_global = plane_pair.second;
 
 		projections[lev].ParallelCopy(plane_global, 0, 0, 1, 0, 0);
