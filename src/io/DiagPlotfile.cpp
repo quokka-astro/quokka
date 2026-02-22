f02b8ddedc268132b37ecb51d0137715db79690f
ChongChong He <chongchonghe99@gmail.com>
2026-02-23 02:12:02 +1100

    merge in select-variables-in-plt

--- src/io/DiagPlotfile.cpp
+++ src/io/DiagPlotfile.cpp
@@ -41,6 +41,38 @@ void DiagPlotfile::init(const std::string &a_prefix, std::string_view a_diagName
 	// Read number of output files (optional)
 	pp.query("nfiles", m_nfiles);
 
+	// Read field names to include (optional, empty means all)
+	int const nVarNames = pp.countval("field_names");
+	if (nVarNames > 0) {
+		m_varNames.resize(nVarNames);
+		for (int n = 0; n < nVarNames; ++n) {
+			pp.get("field_names", m_varNames[n], n);
+		}
+		amrex::Print() << "DiagPlotfile: Selecting fields: ";
+		for (const auto &v : m_varNames) {
+			amrex::Print() << v << " ";
+		}
+		amrex::Print() << "\n";
+	} else {
+		amrex::Print() << "DiagPlotfile: Including all fields\n";
+	}
+
+	// Read fc directions to include (optional, empty means all), e.g. "x y z"
+	int const nFcDirs = pp.countval("fc_dirs");
+	if (nFcDirs > 0) {
+		m_fcDirs.resize(nFcDirs);
+		for (int n = 0; n < nFcDirs; ++n) {
+			pp.get("fc_dirs", m_fcDirs[n], n);
+		}
+		amrex::Print() << "DiagPlotfile: Selecting fc directions: ";
+		for (const auto &d : m_fcDirs) {
+			amrex::Print() << d << " ";
+		}
+		amrex::Print() << "\n";
+	} else {
+		amrex::Print() << "DiagPlotfile: Including all fc directions\n";
+	}
+
 	amrex::Print() << "DiagPlotfile initialized: file=" << m_diagfile << ", interval=" << m_interval << "\n";
 }
 
