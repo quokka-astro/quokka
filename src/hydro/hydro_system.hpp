e70f913f5011b83b666a00faac766c05cd3b7d37
mxchen39 <3050760682@qq.com>
2026-03-03 17:11:08 +0800

    try to fix a bug again

--- src/hydro/hydro_system.hpp
+++ src/hydro/hydro_system.hpp
@@ -1070,14 +1070,12 @@ void HydroSystem<problem_t>::EnforceLimits(amrex::Real const densityFloor, amrex
 		}
 
 		// Enforce dust density floor
+		amrex::Real const dust_floor = dustDensityFloor;
 		if constexpr (Physics_Traits<problem_t>::is_dust_enabled) {
 			for (int g = 0; g < Physics_Traits<problem_t>::nDustGroups; ++g) {
-				amrex::Real const dust_floor = dustDensityFloor;
-				for (int g = 0; g < Physics_Traits<problem_t>::nDustGroups; ++g) {
-					amrex::Real dust_rho = state[bx](i, j, k, dustDensity_index + g * numDustVars_);
-					if (dust_rho < dust_floor) {
-						state[bx](i, j, k, dustDensity_index + g * numDustVars_) = dust_floor;
-					}
+				amrex::Real dust_rho = state[bx](i, j, k, dustDensity_index + g * numDustVars_);
+				if (dust_rho < dust_floor) {
+					state[bx](i, j, k, dustDensity_index + g * numDustVars_) = dust_floor;
 				}
 			}
 		}
