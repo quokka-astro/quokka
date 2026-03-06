d5102a645ca862f32f925fa0922cae4626ef555d
neco kriel <necokriel@gmail.com>
2026-03-06 18:59:53 +1100

    update: better var names and param checks

--- src/problems/SmallScaleDynamo/testSmallScaleDynamo.cpp
+++ src/problems/SmallScaleDynamo/testSmallScaleDynamo.cpp
@@ -66,22 +66,16 @@ template <> void QuokkaSimulation<SmallScaleDynamo>::setInitialConditionsOnGrid(
 	});
 }
 
-// ABC seed field via discrete curl of the vector potential A = A0*[sin(k0*z)+cos(k0*y), sin(k0*x)+cos(k0*z), sin(k0*y)+cos(k0*x)].
-// B = curl(A) is computed via finite differences at face positions, ensuring div(B) = 0 to machine precision.
-// k0 = 2*pi*n/L where n = problem.seed_b_wavenumber (default 2).
-// B0 = sqrt(seed_b_fraction) * target_vdisp, where seed_b_fraction = E_mag/E_kin (default 0, i.e. no seed field).
+// seed field parameters; defaults match input file, overwritten by problem_main() before use
+int seed_b_wavenumber = 2;
+double seed_b_fraction = 0.0;
+double target_vdisp = 0.0;
+
+// we initialise the magnetic field using an ABC (Arnold-Beltrami-Childress) seed field:
+// a force-free, maximally helical field defined as the (discrete) curl of a sinusoidal vector potential.
+// the general ABC field has three independent amplitudes (per each spetial axis); here we choose them to be equal.
 template <> void QuokkaSimulation<SmallScaleDynamo>::setInitialConditionsOnGridFaceVars(quokka::grid const &grid_elem)
 {
-	amrex::ParmParse const pp("problem");
-	int seed_b_wavenumber = 2;
-	double seed_b_fraction = 0.0;
-	pp.query("seed_b_wavenumber", seed_b_wavenumber);
-	pp.query("seed_b_fraction", seed_b_fraction);
-
-	double target_vdisp = 0.0;
-	amrex::ParmParse const pp_turb("turbulence");
-	pp_turb.query("target_vdisp", target_vdisp);
-
 	const amrex::Array4<double> &state_fc = grid_elem.array_;
 	const amrex::Box &indexRange = grid_elem.indexRange_;
 	const quokka::direction dir = grid_elem.dir_;
@@ -89,50 +83,49 @@ template <> void QuokkaSimulation<SmallScaleDynamo>::setInitialConditionsOnGridF
 	const amrex::GpuArray<amrex::Real, AMREX_SPACEDIM> prob_lo = grid_elem.prob_lo_;
 	const amrex::GpuArray<amrex::Real, AMREX_SPACEDIM> prob_hi = grid_elem.prob_hi_;
 
-	const double Lx = prob_hi[0] - prob_lo[0];
-	const double k0 = 2.0 * M_PI * seed_b_wavenumber / Lx;
-	const double B0 = std::sqrt(seed_b_fraction) * target_vdisp;
-	const double A0 = B0 / k0;
+	const double box_length = prob_hi[0] - prob_lo[0];
+	const double seed_wavenumber = 2.0 * M_PI * seed_b_wavenumber / box_length;
+	const double seed_vecpot_amplitude = std::sqrt(seed_b_fraction) * target_vdisp / seed_wavenumber;
 
 	constexpr int mhd_index = Physics_Indices<SmallScaleDynamo>::mhdFirstIndex;
 
 	amrex::ParallelFor(indexRange, [=] AMREX_GPU_DEVICE(int i, int j, int k) {
 		if (dir == quokka::direction::x) {
 			// x-face: B_x = dA_z/dy - dA_y/dz
-			const double x_f = prob_lo[0] + i * dx[0];
+			const double x_face = prob_lo[0] + i * dx[0];
 			const double y_hi = prob_lo[1] + (j + 1) * dx[1];
 			const double y_lo = prob_lo[1] + j * dx[1];
 			const double z_hi = prob_lo[2] + (k + 1) * dx[2];
 			const double z_lo = prob_lo[2] + k * dx[2];
-			const double Az_yhi = A0 * (std::sin(k0 * y_hi) + std::cos(k0 * x_f));
-			const double Az_ylo = A0 * (std::sin(k0 * y_lo) + std::cos(k0 * x_f));
-			const double Ay_zhi = A0 * (std::sin(k0 * x_f) + std::cos(k0 * z_hi));
-			const double Ay_zlo = A0 * (std::sin(k0 * x_f) + std::cos(k0 * z_lo));
-			state_fc(i, j, k, mhd_index) = (Az_yhi - Az_ylo) / dx[1] - (Ay_zhi - Ay_zlo) / dx[2];
+			const double vecpot_z_at_y_hi = seed_vecpot_amplitude * (std::sin(seed_wavenumber * y_hi) + std::cos(seed_wavenumber * x_face));
+			const double vecpot_z_at_y_lo = seed_vecpot_amplitude * (std::sin(seed_wavenumber * y_lo) + std::cos(seed_wavenumber * x_face));
+			const double vecpot_y_at_z_hi = seed_vecpot_amplitude * (std::sin(seed_wavenumber * x_face) + std::cos(seed_wavenumber * z_hi));
+			const double vecpot_y_at_z_lo = seed_vecpot_amplitude * (std::sin(seed_wavenumber * x_face) + std::cos(seed_wavenumber * z_lo));
+			state_fc(i, j, k, mhd_index) = (vecpot_z_at_y_hi - vecpot_z_at_y_lo) / dx[1] - (vecpot_y_at_z_hi - vecpot_y_at_z_lo) / dx[2];
 		} else if (dir == quokka::direction::y) {
 			// y-face: B_y = dA_x/dz - dA_z/dx
-			const double y_f = prob_lo[1] + j * dx[1];
+			const double y_face = prob_lo[1] + j * dx[1];
 			const double z_hi = prob_lo[2] + (k + 1) * dx[2];
 			const double z_lo = prob_lo[2] + k * dx[2];
 			const double x_hi = prob_lo[0] + (i + 1) * dx[0];
 			const double x_lo = prob_lo[0] + i * dx[0];
-			const double Ax_zhi = A0 * (std::sin(k0 * z_hi) + std::cos(k0 * y_f));
-			const double Ax_zlo = A0 * (std::sin(k0 * z_lo) + std::cos(k0 * y_f));
-			const double Az_xhi = A0 * (std::sin(k0 * y_f) + std::cos(k0 * x_hi));
-			const double Az_xlo = A0 * (std::sin(k0 * y_f) + std::cos(k0 * x_lo));
-			state_fc(i, j, k, mhd_index) = (Ax_zhi - Ax_zlo) / dx[2] - (Az_xhi - Az_xlo) / dx[0];
+			const double vecpot_x_at_z_hi = seed_vecpot_amplitude * (std::sin(seed_wavenumber * z_hi) + std::cos(seed_wavenumber * y_face));
+			const double vecpot_x_at_z_lo = seed_vecpot_amplitude * (std::sin(seed_wavenumber * z_lo) + std::cos(seed_wavenumber * y_face));
+			const double vecpot_z_at_x_hi = seed_vecpot_amplitude * (std::sin(seed_wavenumber * y_face) + std::cos(seed_wavenumber * x_hi));
+			const double vecpot_z_at_x_lo = seed_vecpot_amplitude * (std::sin(seed_wavenumber * y_face) + std::cos(seed_wavenumber * x_lo));
+			state_fc(i, j, k, mhd_index) = (vecpot_x_at_z_hi - vecpot_x_at_z_lo) / dx[2] - (vecpot_z_at_x_hi - vecpot_z_at_x_lo) / dx[0];
 		} else if (dir == quokka::direction::z) {
 			// z-face: B_z = dA_y/dx - dA_x/dy
-			const double z_f = prob_lo[2] + k * dx[2];
+			const double z_face = prob_lo[2] + k * dx[2];
 			const double x_hi = prob_lo[0] + (i + 1) * dx[0];
 			const double x_lo = prob_lo[0] + i * dx[0];
 			const double y_hi = prob_lo[1] + (j + 1) * dx[1];
 			const double y_lo = prob_lo[1] + j * dx[1];
-			const double Ay_xhi = A0 * (std::sin(k0 * x_hi) + std::cos(k0 * z_f));
-			const double Ay_xlo = A0 * (std::sin(k0 * x_lo) + std::cos(k0 * z_f));
-			const double Ax_yhi = A0 * (std::sin(k0 * z_f) + std::cos(k0 * y_hi));
-			const double Ax_ylo = A0 * (std::sin(k0 * z_f) + std::cos(k0 * y_lo));
-			state_fc(i, j, k, mhd_index) = (Ay_xhi - Ay_xlo) / dx[0] - (Ax_yhi - Ax_ylo) / dx[1];
+			const double vecpot_y_at_x_hi = seed_vecpot_amplitude * (std::sin(seed_wavenumber * x_hi) + std::cos(seed_wavenumber * z_face));
+			const double vecpot_y_at_x_lo = seed_vecpot_amplitude * (std::sin(seed_wavenumber * x_lo) + std::cos(seed_wavenumber * z_face));
+			const double vecpot_x_at_y_hi = seed_vecpot_amplitude * (std::sin(seed_wavenumber * z_face) + std::cos(seed_wavenumber * y_hi));
+			const double vecpot_x_at_y_lo = seed_vecpot_amplitude * (std::sin(seed_wavenumber * z_face) + std::cos(seed_wavenumber * y_lo));
+			state_fc(i, j, k, mhd_index) = (vecpot_y_at_x_hi - vecpot_y_at_x_lo) / dx[0] - (vecpot_x_at_y_hi - vecpot_x_at_y_lo) / dx[1];
 		}
 	});
 }
@@ -140,17 +133,19 @@ template <> void QuokkaSimulation<SmallScaleDynamo>::setInitialConditionsOnGridF
 auto problem_main() -> int
 {
 	amrex::ParmParse const pp("problem");
-	int seed_b_wavenumber = 2;
 	pp.query("seed_b_wavenumber", seed_b_wavenumber);
+	pp.query("seed_b_fraction", seed_b_fraction);
 	AMREX_ALWAYS_ASSERT_WITH_MESSAGE(seed_b_wavenumber > 0, "problem.seed_b_wavenumber must be a positive integer");
 
-	int ampl_auto_adjust = 0;
 	amrex::ParmParse const pp_turb("turbulence");
+	pp_turb.query("target_vdisp", target_vdisp);
+
+	int ampl_auto_adjust = 0;
 	pp_turb.query("ampl_auto_adjust", ampl_auto_adjust);
 	if (ampl_auto_adjust == 1) {
 		amrex::Print() << "WARNING: turbulence.ampl_auto_adjust = 1 is set. The driving amplitude will vary over time, "
 			          "which makes it harder to interpret the dynamo growth rate. Consider setting ampl_auto_adjust = 0 "
-			          "and tuning ampl_factor manually to reach the desired velocity dispersion.\n";
+			          "and tuning ampl_factor manually to achieve the desired velocity dispersion.\n";
 	}
 
 	auto BCs_cc = quokka::BC<SmallScaleDynamo>(quokka::BCType::int_dir);
