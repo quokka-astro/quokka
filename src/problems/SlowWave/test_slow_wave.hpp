db675ce7102238d1dcb235ca206dfddaf984f841
Neco Kriel <necokriel@gmail.com>
2026-05-25 13:50:12 +0200

    add(SlowWave): fixed-resolution slow magnetosonic wave test problem.

---
+++ src/problems/SlowWave/test_slow_wave.hpp
@@ -0,0 +1,15 @@
+#ifndef TEST_SLOW_WAVE_HPP_ // NOLINT
+#define TEST_SLOW_WAVE_HPP_
+//==============================================================================
+// Copyright 2026 Neco Kriel.
+// Released under the MIT license. See LICENSE file included in the GitHub repo.
+//==============================================================================
+/// \file test_slow_wave.hpp
+/// \brief Fixed-resolution slow magnetosonic wave test problem.
+///
+
+#include <format>
+
+#include "hydro/mhd_system.hpp"
+
+#endif // TEST_SLOW_WAVE_HPP_
