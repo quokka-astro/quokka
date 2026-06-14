68edf55a636c56ddaddbbb43ff8a0df5d8d8e218
Neco Kriel <necokriel@gmail.com>
2026-06-14 16:03:20 +0200

    fix(applyResistiveCorrection): move constexpr-if out of gpu lambda to fix cuda extended lambda restriction.

--- src/hydro/mhd_system.hpp
+++ src/hydro/mhd_system.hpp
@@ -110,6 +110,12 @@ template <typename problem_t> class MHDSystem : public HyperbolicSystem<problem_
 									    std::array<int, 3> const &delta_w0, std::array<int, 3> const &delta_w1,
 									    amrex::Real dx_w0, amrex::Real dx_w1, amrex::Real resistivity) -> amrex::Real;
 
+	AMREX_GPU_DEVICE AMREX_FORCE_INLINE static void applyResistiveCorrection(amrex::Array4<amrex::Real> const &E2_ave, int i, int j, int k,
+										 amrex::Array4<const amrex::Real> const &B_w0,
+										 amrex::Array4<const amrex::Real> const &B_w1,
+										 std::array<int, 3> const &delta_w0, std::array<int, 3> const &delta_w1,
+										 amrex::Real dx_w0, amrex::Real dx_w1, amrex::Real resistivity);
+
 	static void ReconstructTo(FluxDir dir, arrayconst_t &cState, array_t &lState, array_t &rState, const amrex::Box &box_iValid, int reconstructionOrder,
 				  SlopeLimiter plmLimiter);
 
@@ -807,12 +813,7 @@ void MHDSystem<problem_t>::EMFAverage_LondrilloDelZanna2004(amrex::Array4<amrex:
 		const double term2 = ((a1_m * a1_p) / (a1_m + a1_p)) * (B0_p_ - B0_m_) + ((a0_m * a0_p) / (a0_m + a0_p)) * (B1_m_ - B1_p_);
 
 		E2_ave(i, j, k) = (numerator / denominator) + term2;
-		if constexpr (Physics_Traits<problem_t>::resistivity_model == ResistivityModel::constant) {
-			E2_ave(i, j, k) -= computeResistiveEMF(B_w0, B_w1, i, j, k, delta_w0, delta_w1, dx_w0, dx_w1, resistivity);
-		} else if constexpr (Physics_Traits<problem_t>::resistivity_model == ResistivityModel::problem_defined) {
-			const amrex::Real eta = computeResistivity<problem_t>(i, j, k, B_w0, B_w1, dx_w0, dx_w1);
-			E2_ave(i, j, k) -= computeResistiveEMF(B_w0, B_w1, i, j, k, delta_w0, delta_w1, dx_w0, dx_w1, eta);
-		}
+		MHDSystem<problem_t>::applyResistiveCorrection(E2_ave, i, j, k, B_w0, B_w1, delta_w0, delta_w1, dx_w0, dx_w1, resistivity);
 	});
 }
 
@@ -917,12 +918,7 @@ void MHDSystem<problem_t>::EMFAverage_Balsara2025(amrex::Array4<amrex::Real> E2_
 			E2_ave(i, j, k) = E2_dstar;
 		}
 
-		if constexpr (Physics_Traits<problem_t>::resistivity_model == ResistivityModel::constant) {
-			E2_ave(i, j, k) -= computeResistiveEMF(B_w0, B_w1, i, j, k, delta_w0, delta_w1, dx_w0, dx_w1, resistivity);
-		} else if constexpr (Physics_Traits<problem_t>::resistivity_model == ResistivityModel::problem_defined) {
-			const amrex::Real eta = computeResistivity<problem_t>(i, j, k, B_w0, B_w1, dx_w0, dx_w1);
-			E2_ave(i, j, k) -= computeResistiveEMF(B_w0, B_w1, i, j, k, delta_w0, delta_w1, dx_w0, dx_w1, eta);
-		}
+		MHDSystem<problem_t>::applyResistiveCorrection(E2_ave, i, j, k, B_w0, B_w1, delta_w0, delta_w1, dx_w0, dx_w1, resistivity);
 	});
 }
 
@@ -1064,6 +1060,21 @@ AMREX_GPU_DEVICE AMREX_FORCE_INLINE auto MHDSystem<problem_t>::computeResistiveE
 	const amrex::Real J_edge = (B_w1(i, j, k) - B_w1(i - delta_w0[0], j - delta_w0[1], k - delta_w0[2])) / dx_w0 -
 				   (B_w0(i, j, k) - B_w0(i - delta_w1[0], j - delta_w1[1], k - delta_w1[2])) / dx_w1;
 	return resistivity * J_edge;
+}
+
+template <typename problem_t>
+AMREX_GPU_DEVICE AMREX_FORCE_INLINE void MHDSystem<problem_t>::applyResistiveCorrection(amrex::Array4<amrex::Real> const &E2_ave, int i, int j, int k,
+											 amrex::Array4<const amrex::Real> const &B_w0,
+											 amrex::Array4<const amrex::Real> const &B_w1,
+											 std::array<int, 3> const &delta_w0, std::array<int, 3> const &delta_w1,
+											 amrex::Real dx_w0, amrex::Real dx_w1, amrex::Real resistivity)
+{
+	if constexpr (Physics_Traits<problem_t>::resistivity_model == ResistivityModel::constant) {
+		E2_ave(i, j, k) -= computeResistiveEMF(B_w0, B_w1, i, j, k, delta_w0, delta_w1, dx_w0, dx_w1, resistivity);
+	} else if constexpr (Physics_Traits<problem_t>::resistivity_model == ResistivityModel::problem_defined) {
+		const amrex::Real eta = computeResistivity<problem_t>(i, j, k, B_w0, B_w1, dx_w0, dx_w1);
+		E2_ave(i, j, k) -= computeResistiveEMF(B_w0, B_w1, i, j, k, delta_w0, delta_w1, dx_w0, dx_w1, eta);
+	}
 }
 
 #endif // HYDRO_SYSTEM_HPP_
