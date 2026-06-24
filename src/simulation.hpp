ea9117fc360560ceef73947295ed85c82203c658
Neco Kriel <necokriel@gmail.com>
2026-06-24 19:23:06 +0200

    Add startup git hash logging: print version and dependency hashes to stdout at simulation start.

--- src/simulation.hpp
+++ src/simulation.hpp
@@ -808,6 +808,15 @@ template <typename problem_t> void AMRSimulation<problem_t>::initialize()
 	simulationMetadata_["git_hash_quokka"] = getGitHashForQuokka();
 	simulationMetadata_["git_hash_amrex"] = getGitHashForAmrex();
 
+	// print version and git hashes to stdout
+	amrex::Print() << std::format("Quokka version {} (git: {})\n", QUOKKA_VERSION, getGitHashForQuokka());
+	amrex::Print() << std::format("AMReX git: {}\n", getGitHashForAmrex());
+	amrex::Print() << std::format("Microphysics git: {}\n", MICROPHYSICS_GIT_HASH);
+#if AMREX_SPACEDIM == 3
+	amrex::Print() << std::format("AMReX-Hydro git: {}\n", AMREX_HYDRO_GIT_HASH);
+#endif
+	amrex::Print() << std::format("turbulence git: {}\n", TURBULENCE_GIT_HASH);
+
 	// add units and physics-specific metadata
 	if constexpr (Physics_Traits<problem_t>::is_hydro_enabled || Physics_Traits<problem_t>::is_radiation_enabled) {
 		initializeSimulationMetadata();
