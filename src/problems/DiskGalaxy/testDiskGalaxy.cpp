040c9e5749889c409711acfbc8d9dfa55382ae81
ChongChong He <chongchonghe99@gmail.com>
2026-06-29 14:34:50 +1000

    Squash merge chong/use-devicemem-datatable into chong/cooling/use-DataTable2

--- src/problems/DiskGalaxy/testDiskGalaxy.cpp
+++ src/problems/DiskGalaxy/testDiskGalaxy.cpp
@@ -103,7 +103,7 @@ template <> void QuokkaSimulation<DiskGalaxy>::preCalculateInitialConditions()
 	pp.query("vcirc_file", filename);
 
 	auto halo_table = quokka::DataTable<1, 4, quokka::OutOfBounds::clamp>::CSVReader(filename, quokka::SpacingType::linear);
-	auto const halo_table_const = halo_table.const_tables();
+	auto const halo_table_const = halo_table.const_tables_host();
 	AMREX_ALWAYS_ASSERT_WITH_MESSAGE(halo_table_const.sizes[0] > 0, "disk_galaxy.vcirc_file contained no numeric rows.");
 	AMREX_ALWAYS_ASSERT_WITH_MESSAGE(halo_table_const.spacing_types[0] == quokka::SpacingType::linear,
 					 "disk_galaxy.vcirc_file must use linear spacing for the radius coordinate.");
