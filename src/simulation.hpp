cc2a51986b2553df357b4ac0557cc19830534c6e
lizmcole <lizmcole@users.noreply.github.com>
2026-07-06 11:21:44 +1000

    Rename statistics_prefix to statistics_file

--- src/simulation.hpp
+++ src/simulation.hpp
@@ -975,7 +975,7 @@ template <typename problem_t> void AMRSimulation<problem_t>::readParameters()
 	pp.query("checkpoint_prefix", chk_file);
 
 	// Default statistics prefix
-	pp.query("statistics_prefix", stats_file);
+	pp.query("statistics_file", stats_file);
 
 	// Default do_reflux = 1
 	pp.query("do_reflux", do_reflux);
