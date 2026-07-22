802e89eef9132bec0051e205d12d979624b78dbb
Aditi Vijayan <aditi.vijayan@anu.edu.au>
2026-07-22 11:16:01 +1000

    comment out point wise error writing

--- src/QuokkaSimulation.hpp
+++ src/QuokkaSimulation.hpp
@@ -291,6 +291,10 @@ template <typename problem_t> class QuokkaSimulation : public AMRSimulation<prob
 				    "Physical resistivity requires use_dual_energy = 0: Ohmic heating is not yet added to the auxiliary energy equation.");
 			}
 		}
+	if (enableElectronConduction_) {
+				AMREX_ALWAYS_ASSERT_WITH_MESSAGE(do_subcycle == 0,
+								 "AMR subcycling is not supported with conduction. Set do_subcycle = 0.");
+			}
 	}
 
 	[[nodiscard]] AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE auto densityFloor(amrex::Real x, amrex::Real y, amrex::Real z,
@@ -1330,7 +1334,7 @@ template <typename problem_t> auto QuokkaSimulation<problem_t>::computeComponent
 	amrex::MultiFab residual(state_ref_level0.boxArray(), state_ref_level0.DistributionMap(), ncomp, 0);
 	amrex::MultiFab::Copy(residual, state_ref_level0, 0, 0, ncomp, 0);
 	amrex::MultiFab::Saxpy(residual, -1., state_new_cc_[0], 0, 0, ncomp, 0);
-	amrex::WriteSingleLevelPlotfile("point_wise_error", residual, componentNames_cc_, geom[0], /*time=*/0.0, /*level_step=*/0);
+	// amrex::WriteSingleLevelPlotfile("point_wise_error", residual, componentNames_cc_, geom[0], /*time=*/0.0, /*level_step=*/0);
 
 	const auto n_cells = static_cast<amrex::Real>(residual.boxArray().numPts());
 
