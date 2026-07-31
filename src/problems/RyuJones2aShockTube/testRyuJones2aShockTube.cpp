d2b7be15d52176d759a42d613e7123fd101e1635
Neco Kriel <necokriel@gmail.com>
2026-07-31 18:02:31 +0200

    fix-content(testRyuJones2aShockTube.cpp): use std::numbers::inv_sqrtpi for Bx/Bz.

--- src/problems/RyuJones2aShockTube/testRyuJones2aShockTube.cpp
+++ src/problems/RyuJones2aShockTube/testRyuJones2aShockTube.cpp
@@ -15,6 +15,7 @@
 #include <cmath>
 #include <format>
 #include <fstream>
+#include <numbers>
 #include <string>
 #include <unordered_map>
 
@@ -54,10 +55,10 @@ constexpr amrex::Real vx_R = 0.0;
 constexpr amrex::Real vy_R = 0.0;
 constexpr amrex::Real vz_R = 0.0;
 
-constexpr amrex::Real Bx = 0.5641895835477562;	 // 2 / sqrt(4 pi); constant
-constexpr amrex::Real By_L = 1.0155412503859613; // 3.6 / sqrt(4 pi)
-constexpr amrex::Real By_R = 1.1283791670955125; // 4 / sqrt(4 pi)
-constexpr amrex::Real Bz = 0.5641895835477562;	 // 2 / sqrt(4 pi); constant
+constexpr amrex::Real Bx = std::numbers::inv_sqrtpi; // 2 / sqrt(4 pi); constant
+constexpr amrex::Real By_L = 1.0155412503859613;      // 3.6 / sqrt(4 pi)
+constexpr amrex::Real By_R = 1.1283791670955125;      // 4 / sqrt(4 pi)
+constexpr amrex::Real Bz = std::numbers::inv_sqrtpi;  // 2 / sqrt(4 pi); constant
 
 template <> void QuokkaSimulation<RyuJones2aShockTubeProblem>::setInitialConditionsOnGrid(quokka::grid const &grid_elem)
 {
