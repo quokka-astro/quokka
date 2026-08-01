3b79bf7b0a4ba2008e458bfc4e58ff3433015f4b
Neco Kriel <necokriel@gmail.com>
2026-08-02 00:34:52 +0200

    add-content(HydroBlast3D): opt into constant viscosity model for the bulk-viscosity Sedov smoke test.

--- src/problems/HydroBlast3D/testHydroBlast3D.cpp
+++ src/problems/HydroBlast3D/testHydroBlast3D.cpp
@@ -40,6 +40,7 @@ template <> struct HydroSystem_Traits<SedovProblem> {
 template <> struct Physics_Traits<SedovProblem> : DefaultPhysicsTraits {
 	// cell-centred
 	static constexpr bool is_hydro_enabled = true;
+	static constexpr ViscosityModel viscosity_model = ViscosityModel::constant; // shear/bulk default to 0; no-op unless set
 };
 
 // declare global variables
