8ce1ba5a8da103b6eb8152fac2cdbd303a3e95cf
James471 <James.Watt@anu.edu.au>
2026-08-11 13:23:04 +1000

    Move network info into cmake for handwritten network

--- src/networks/NetworkRegistry.cmake
+++ src/networks/NetworkRegistry.cmake
@@ -1,10 +1,31 @@
 # Quokka-side microphysics network registry.
 
+# ';' is written as '|' for AION_CONSTEXPR/ZION_CONSTEXPR because
+# cmake_parse_arguments() splits single-value fields on ';'
+# setup_target_for_microphysics_compilation() substitutes '|' back to ';'
+set(_pi_hand_written_species_args
+  NSPEC 3
+  SPECIES_ENUM "H = 0, Hp, e"
+  SPEC_NAMES "\"H\", \"Hp\", \"e\""
+  SHORT_SPEC_NAMES "\"H\", \"H+\", \"e-\""
+  AION "1.0, 1.0, 5.4858e-4"
+  AION_INV "1.0, 1.0, 1822.89"
+  ZION "1.0, 1.0, 1.0"
+  # constexpr switch bodies
+  AION_CONSTEXPR "case H:  a = 1.0| break| case Hp: a = 1.0| break| case e:  a = 1.0| break|"
+  ZION_CONSTEXPR "case H:  z = 1.0| break| case Hp: z = 1.0| break| case e:  z = 1.0| break|"
+  NUM_CHEM_BANDS 1
+  CHEM_BANDS "13.6, 62.1" # eV
+  POWER_LAW_INDEX 0 # jaff network.radiation.power_law_index
+)
+
 foreach(_pi_name photoionization DType_JAFF)
-  # photoionization itself is hand-written; the other two are jaff-generated.
   set(_pi_is_jaff FALSE)
+  set(_pi_species_args "")
   if (_pi_name STREQUAL "DType_JAFF")
     set(_pi_is_jaff TRUE)
+  else()
+    set(_pi_species_args ${_pi_hand_written_species_args})
   endif()
 
   register_microphysics_network(${_pi_name}
@@ -23,5 +44,6 @@ foreach(_pi_name photoionization DType_JAFF)
     USES_INTEGRATOR_DIRS TRUE
     NETWORK_INCLUDE_DIRS "${CMAKE_SOURCE_DIR}/src/networks/${_pi_name}"
                           "${CMAKE_SOURCE_DIR}/extern/Microphysics/networks"
+    ${_pi_species_args}
   )
 endforeach()
