8ed68edac6b6ab3894bd7bb60377a29c27ddf3d1
Aditi Vijayan <aditi.vijayan@anu.edu.au>
2026-08-14 10:25:19 +1000

    remove debug statements and add recal_flux to rk2 flux

--- src/QuokkaSimulation.hpp
+++ src/QuokkaSimulation.hpp
@@ -2644,9 +2644,6 @@ auto QuokkaSimulation<problem_t>::advanceHydroAtLevel(amrex::MultiFab &state_old
 		amrex::MultiFab::Saxpy( flux_rk2[idim], 1.0, (*recal_fluxes)[idim], HydroSystem<problem_t>::internalEnergy_index, HydroSystem<problem_t>::internalEnergy_index, 1, 0 );
     }}
 		incrementFluxRegisters(fr_as_crse, fr_as_fine, flux_rk2, lev, dt_lev);
-		// if (enableElectronConduction_ == 1) {
-			// incrementFluxRegisters(fr_as_crse, fr_as_fine, recal_fluxes.value(), lev, dt_lev);
-		// }
 		if constexpr (Physics_Traits<problem_t>::is_mhd_enabled) {
 			// E = -v x B, our emf is v x B, so we need to pass -dt
 			incrementEMFRegisters(emf_as_crse, emf_as_fine, ec_emf_components_rk_ave, lev, -1.0 * dt_lev);
