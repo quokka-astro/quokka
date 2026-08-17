456a038720c9d2a11e39fff591939da0d405ba27
Chong-Chong He <chongchonghe99@gmail.com>
2026-08-17 14:43:52 +0000

    Disable temperature floor in HydroQuirk idealized test

--- src/problems/HydroQuirk/testHydroQuirk.cpp
+++ src/problems/HydroQuirk/testHydroQuirk.cpp
@@ -230,6 +230,8 @@ auto problem_main() -> int
 {
 	// Problem initialization
 	QuokkaSimulation<QuirkProblem> sim;
+	// idealized test in non-physical CGS units: disable the default 5 K temperature floor
+	sim.tempFloor_ = 0.0;
 
 	sim.reconstructionOrder_ = 2; // PLM
 	sim.stopTime_ = 0.4;
