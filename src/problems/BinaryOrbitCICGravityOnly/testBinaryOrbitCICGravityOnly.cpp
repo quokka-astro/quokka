21dafa1c35cb4eb23ab0bc283f73f0511ac414e2
ChongChong He <chongchonghe99@gmail.com>
2026-08-27 13:08:18 +1000

    cleanup

--- src/problems/BinaryOrbitCICGravityOnly/testBinaryOrbitCICGravityOnly.cpp
+++ src/problems/BinaryOrbitCICGravityOnly/testBinaryOrbitCICGravityOnly.cpp
@@ -1,8 +1,3 @@
-//==============================================================================
-// TwoMomentRad - a radiation transport library for patch-based AMR codes
-// Copyright 2020 Benjamin Wibking.
-// Released under the MIT license. See LICENSE file included in the GitHub repo.
-//==============================================================================
 /// \file testBinaryOrbitCICGravityOnly.cpp
 /// \brief Defines a test problem for a binary orbit with only self-gravity enabled.
 ///
