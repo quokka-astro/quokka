e4972a3a5d39c09144dae80ed892d3c7268860ae
Aditi Vijayan <aditi.vijayan@anu.edu.au>
2026-09-03 09:43:26 +1000

    address codex comment about identical eint between timestep and flux calculation and get mscalars on faces

--- src/conduction/ElectronConduction.hpp
+++ src/conduction/ElectronConduction.hpp
@@ -101,22 +101,11 @@ template <typename problem_t> class ElectronConduction
 		amrex::IntVect ng = amrex::IntVect(AMREX_D_DECL(state.nGrow(), state.nGrow(), state.nGrow()));
 
 		amrex::ParallelFor(state, ng, [=] AMREX_GPU_DEVICE(int bx, int i, int j, int k) noexcept {
-			std::array<amrex::Array4<const amrex::Real>, AMREX_SPACEDIM> local_state_fc{};
-			if (Physics_Traits<problem_t>::is_mhd_enabled) {
-				local_state_fc[0] = state_fc_x0[bx];
-#if AMREX_SPACEDIM >= 2
-				local_state_fc[1] = state_fc_x1[bx];
-#endif
-#if AMREX_SPACEDIM == 3
-				local_state_fc[2] = state_fc_x2[bx];
-#endif
-			}
-
 			auto const &cons = state_x0[bx];
 			const amrex::Real rho = cons(i, j, k, HydroSystem<problem_t>::density_index);
-			const amrex::Real Eint = HydroSystem<problem_t>::ComputeInternalEnergy(cons, i, j, k, &local_state_fc);
+			const amrex::Real Eint = cons(i, j, k, HydroSystem<problem_t>::internalEnergy_index);
 			// Temperature always from EOS
-			quokka::optional<amrex::GpuArray<amrex::Real, nmscalars_>> massScalars = {};
+			quokka::optional<amrex::GpuArray<amrex::Real, nmscalars_>> massScalars = RadSystem<problem_t>::ComputeMassScalars(cons, i, j, k);
 			const amrex::Real Tgas = ::quokka::EOS<problem_t>::ComputeTgasFromEint(rho, Eint, massScalars);
 
 			primVar_arr[bx](i, j, k, 0) = rho;
@@ -139,11 +128,17 @@ template <typename problem_t> class ElectronConduction
 			     , ReconstructPrimVar<FluxDir::X2>(primVar, leftState[1], rightState[1], ng_reconstruct, params);
 			     , ReconstructPrimVar<FluxDir::X3>(primVar, leftState[2], rightState[2], ng_reconstruct, params);)
 
-		auto const evaluateFace = [=] AMREX_GPU_DEVICE(amrex::Real rho_L, amrex::Real T_L, amrex::Real rho_R, amrex::Real T_R, amrex::Real &kappa_face,
+		auto const evaluateFace = [=] AMREX_GPU_DEVICE(amrex::Real rho_L, amrex::Real T_L, amrex::Real rho_R, amrex::Real T_R,
+							       amrex::GpuArray<amrex::Real, nmscalars_> const &massScalars_L,
+							       amrex::GpuArray<amrex::Real, nmscalars_> const &massScalars_R, amrex::Real &kappa_face,
 							       amrex::Real &qsat_face) noexcept {
 			const amrex::Real rho_face = 0.5 * (rho_L + rho_R);
 			const amrex::Real T_face = amrex::max(0.5 * (T_L + T_R), t_min);
-			quokka::optional<amrex::GpuArray<amrex::Real, nmscalars_>> massScalars = {};
+			amrex::GpuArray<amrex::Real, nmscalars_> massArray_face{};
+			for (int n = 0; n < nmscalars_; ++n) {
+				massArray_face[n] = 0.5 * (massScalars_L[n] + massScalars_R[n]);
+			}
+			quokka::optional<amrex::GpuArray<amrex::Real, nmscalars_>> massScalars = massArray_face;
 			const amrex::Real Eint_face = ::quokka::EOS<problem_t>::ComputeEintFromTgas(rho_face, T_face, massScalars);
 			const amrex::Real Pgas_face = ::quokka::EOS<problem_t>::ComputePressure(rho_face, Eint_face, massScalars);
 			const amrex::Real cs_face = ::quokka::EOS<problem_t>::ComputeSoundSpeed(rho_face, Pgas_face, massScalars);
@@ -160,7 +155,10 @@ template <typename problem_t> class ElectronConduction
 			const amrex::Real gradT = (temp[bx](i, j, k, 1) - temp[bx](i - 1, j, k, 1)) / dx[0];
 			amrex::Real kappa_face = 0.0;
 			amrex::Real q_sat_face = 0.0;
-			evaluateFace(left_x[bx](i, j, k, 0), left_x[bx](i, j, k, 1), right_x[bx](i, j, k, 0), right_x[bx](i, j, k, 1), kappa_face, q_sat_face);
+			auto const massScalars_L = RadSystem<problem_t>::ComputeMassScalars(state_x0[bx], i - 1, j, k);
+			auto const massScalars_R = RadSystem<problem_t>::ComputeMassScalars(state_x0[bx], i, j, k);
+			evaluateFace(left_x[bx](i, j, k, 0), left_x[bx](i, j, k, 1), right_x[bx](i, j, k, 0), right_x[bx](i, j, k, 1), massScalars_L,
+				     massScalars_R, kappa_face, q_sat_face);
 			const amrex::Real q_classical = -kappa_face * gradT;
 			const amrex::Real limiter = 1.0 + std::abs(q_classical) / amrex::max(q_sat_face, small);
 			flux_x[bx](i, j, k) = q_classical / limiter;
@@ -174,7 +172,10 @@ template <typename problem_t> class ElectronConduction
 			const amrex::Real gradT = (temp[bx](i, j, k, 1) - temp[bx](i, j - 1, k, 1)) / dx[1];
 			amrex::Real kappa_face = 0.0;
 			amrex::Real q_sat_face = 0.0;
-			evaluateFace(left_y[bx](i, j, k, 0), left_y[bx](i, j, k, 1), right_y[bx](i, j, k, 0), right_y[bx](i, j, k, 1), kappa_face, q_sat_face);
+			auto const massScalars_L = RadSystem<problem_t>::ComputeMassScalars(state_x0[bx], i, j - 1, k);
+			auto const massScalars_R = RadSystem<problem_t>::ComputeMassScalars(state_x0[bx], i, j, k);
+			evaluateFace(left_y[bx](i, j, k, 0), left_y[bx](i, j, k, 1), right_y[bx](i, j, k, 0), right_y[bx](i, j, k, 1), massScalars_L,
+				     massScalars_R, kappa_face, q_sat_face);
 			const amrex::Real q_classical = -kappa_face * gradT;
 			const amrex::Real limiter = 1.0 + std::abs(q_classical) / amrex::max(q_sat_face, small);
 			flux_y[bx](i, j, k) = q_classical / limiter;
@@ -189,7 +190,10 @@ template <typename problem_t> class ElectronConduction
 			const amrex::Real gradT = (temp[bx](i, j, k, 1) - temp[bx](i, j, k - 1, 1)) / dx[2];
 			amrex::Real kappa_face = 0.0;
 			amrex::Real q_sat_face = 0.0;
-			evaluateFace(left_z[bx](i, j, k, 0), left_z[bx](i, j, k, 1), right_z[bx](i, j, k, 0), right_z[bx](i, j, k, 1), kappa_face, q_sat_face);
+			auto const massScalars_L = RadSystem<problem_t>::ComputeMassScalars(state_x0[bx], i, j, k - 1);
+			auto const massScalars_R = RadSystem<problem_t>::ComputeMassScalars(state_x0[bx], i, j, k);
+			evaluateFace(left_z[bx](i, j, k, 0), left_z[bx](i, j, k, 1), right_z[bx](i, j, k, 0), right_z[bx](i, j, k, 1), massScalars_L,
+				     massScalars_R, kappa_face, q_sat_face);
 			const amrex::Real q_classical = -kappa_face * gradT;
 			const amrex::Real limiter = 1.0 + std::abs(q_classical) / amrex::max(q_sat_face, small);
 			flux_z[bx](i, j, k) = q_classical / limiter;
@@ -223,7 +227,7 @@ template <typename problem_t> class ElectronConduction
 			const amrex::Real pz = state_out[bx](i, j, k, HydroSystem<problem_t>::x3Momentum_index);
 
 			const amrex::Real Ekin = 0.5 * (px * px + py * py + pz * pz) / rho;
-			const amrex::Real Eint_old = HydroSystem<problem_t>::ComputeInternalEnergy(state_out[bx], i, j, k, &local_state_fc);
+			const amrex::Real Eint_old = state_out[bx](i, j, k, HydroSystem<problem_t>::internalEnergy_index);
 			const amrex::Real Emag = HydroSystem<problem_t>::ComputeMagneticEnergy(i, j, k, &local_state_fc);
 			amrex::Real div_flux = (flux_x_const[bx](i + 1, j, k) - flux_x_const[bx](i, j, k)) / dx[0];
 #if AMREX_SPACEDIM >= 2
