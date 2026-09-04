8b0c7a503b6ff1d625b714908e14463d5b711827
Michele Fulghieri <m.fulghieri@campus.unimib.it>
2026-09-04 11:01:10 +0200

    Test for CIC-only timestep setup: BinaryCICTimestep.cpp

---
+++ src/problems/BinaryCICTimestep/testBinaryCICTimestep.cpp
@@ -0,0 +1,60 @@
+//==============================================================================
+// TwoMomentRad - a radiation transport library for patch-based AMR codes
+// Copyright 2020 Benjamin Wibking.
+// Released under the MIT license. See LICENSE file included in the GitHub repo.
+//==============================================================================
+/// \file testBinaryOrbitCIC.cpp
+/// \brief Defines a test problem for a binary orbit with particle-only
+///        to test the particle-only timestepping (no HD or radiation active).
+///
+
+
+#include "QuokkaSimulation.hpp"
+#include "particles/particle_types.hpp"
+
+
+#include <AMReX_Math.H>
+#include <AMReX_ParticleMesh.H>
+#include <AMReX_ParallelDescriptor.H>
+#include <AMReX_Print.H>
+
+#include <iomanip>
+
+struct BinaryCICTimestep {
+    static constexpr int nvarTotal_cc = 1;
+};
+
+template <> struct Particle_Traits<BinaryCICTimestep>{  
+    static constexpr ParticleSwitch particle_switch = ParticleSwitch::CIC;
+};
+
+template <> struct Physics_Traits<BinaryCICTimestep> {
+    static constexpr bool is_hydro_enabled        = false;
+	static constexpr bool is_self_gravity_enabled = true;
+	static constexpr bool is_radiation_enabled    = false;
+	static constexpr bool is_mhd_enabled          = false;
+    static constexpr int nGroups                  = 0;
+	static constexpr int numMassScalars           = 0;
+	static constexpr int numPassiveScalars        = 0;
+	static constexpr bool is_dust_enabled         = false;
+	static constexpr UnitSystem unit_system = UnitSystem::CGS;
+};
+
+
+template <> void QuokkaSimulation<BinaryCICTimestep>::createInitialCICParticles()
+{
+
+}
+
+
+auto problem_main() -> int {
+	int status = 0;
+	QuokkaSimulation<BinaryCICTimestep> sim;
+	sim.readParameters();
+
+	sim.setInitialConditions();
+	sim.evolve();
+
+	return status;
+}
+
