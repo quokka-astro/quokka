b19a8a6acae11c86fa18986e478d3608f42f7710
Michele Fulghieri <m.fulghieri@campus.unimib.it>
2026-09-04 17:28:54 +0200

    Test BinaryCICTimestep.cpp implmentation, waiting for making consistent particle-only guards in member functions in the code

--- src/problems/BinaryCICTimestep/testBinaryCICTimestep.cpp
+++ src/problems/BinaryCICTimestep/testBinaryCICTimestep.cpp
@@ -8,10 +8,8 @@
 ///        to test the particle-only timestepping (no HD or radiation active).
 ///
 
-
 #include "QuokkaSimulation.hpp"
 #include "particles/particle_types.hpp"
-
 
 #include <AMReX_Math.H>
 #include <AMReX_ParticleMesh.H>
@@ -21,37 +19,37 @@
 #include <iomanip>
 
 struct BinaryCICTimestep {
-    static constexpr int nvarTotal_cc = 1;
 };
 
-template <> struct Particle_Traits<BinaryCICTimestep>{  
+template <> struct Particle_Traits<BinaryCICTimestep> : DefaultParticleTraits {  
     static constexpr ParticleSwitch particle_switch = ParticleSwitch::CIC;
 };
 
-template <> struct Physics_Traits<BinaryCICTimestep> {
+template <> struct Physics_Traits<BinaryCICTimestep> :DefaultPhysicsTraits {
     static constexpr bool is_hydro_enabled        = false;
-	static constexpr bool is_self_gravity_enabled = true;
 	static constexpr bool is_radiation_enabled    = false;
-	static constexpr bool is_mhd_enabled          = false;
+	static constexpr bool is_self_gravity_enabled = true;  
     static constexpr int nGroups                  = 0;
 	static constexpr int numMassScalars           = 0;
 	static constexpr int numPassiveScalars        = 0;
-	static constexpr bool is_dust_enabled         = false;
 	static constexpr UnitSystem unit_system = UnitSystem::CGS;
 };
 
 
 template <> void QuokkaSimulation<BinaryCICTimestep>::createInitialCICParticles()
 {
-
+	// read particles from ASCII file
+	const int nreal_extra = 4;
+	CICParticles->SetVerbose(1);
+	CICParticles->InitFromAsciiFile("../inputs/BinaryCICTimestep_particles.txt", nreal_extra, nullptr);
 }
 
 
 auto problem_main() -> int {
 	int status = 0;
 	QuokkaSimulation<BinaryCICTimestep> sim;
+	sim.initialize();
 	sim.readParameters();
-
 	sim.setInitialConditions();
 	sim.evolve();
 
