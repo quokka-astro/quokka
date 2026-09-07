34ded1631a1f4cbf847e0733a8513500f8f3fb15
Aditi Vijayan <aditi.vijayan@anu.edu.au>
2026-09-08 09:23:48 +1000

    handle non positive densities explicitly

--- src/hydro/hydro_system.hpp
+++ src/hydro/hydro_system.hpp
@@ -1062,7 +1062,7 @@ void HydroSystem<problem_t>::EnforceLimits(amrex::Real const densityFloor, amrex
 
 				if (nscalars_ > 0) {
 					for (int n = 0; n < nscalars_; ++n) {
-						if (rho == 0.0) {
+						if (rho <= 0.0) {
 							state[bx](i, j, k, scalar0_index + n) = 0.0;
 						} else {
 							state[bx](i, j, k, scalar0_index + n) *= rho_new / rho;
