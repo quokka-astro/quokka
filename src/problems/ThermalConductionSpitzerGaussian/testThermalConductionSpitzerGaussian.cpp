7175f3ef6da99d0e3e6effca044bf37adf1f79b4
Aditi Vijayan <aditi.vijayan@anu.edu.au>
2026-09-15 09:53:08 +1000

    fix crash

--- src/problems/ThermalConductionSpitzerGaussian/testThermalConductionSpitzerGaussian.cpp
+++ src/problems/ThermalConductionSpitzerGaussian/testThermalConductionSpitzerGaussian.cpp
@@ -220,9 +220,9 @@ void QuokkaSimulation<ThermalConductionSpitzerGaussianProblem>::ComputeDerivedVa
 			auto const &output = mf.array(iter);
 			auto const &state = state_cc.const_array(iter);
 			std::array<amrex::Array4<const amrex::Real>, AMREX_SPACEDIM> cons_fc{};
-			if (Physics_Traits<problem_t>::is_mhd_enabled) {
-				cons_fc = {AMREX_D_DECL(state_fc[0].const_array(iter), state_fc[1].const_array(iter), state_fc[2].const_array(iter))};
-			}
+        if (Physics_Traits<ThermalConductionSpitzerGaussianProblem>::is_mhd_enabled) {
+            cons_fc = {AMREX_D_DECL(state_fc[0].const_array(iter), state_fc[1].const_array(iter), state_fc[2].const_array(iter))};
+        }
 			amrex::ParallelFor(indexRange, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
 				Real const rho = state(i, j, k, HydroSystem<ThermalConductionSpitzerGaussianProblem>::density_index);
 				Real const Eint = HydroSystem<ThermalConductionSpitzerGaussianProblem>::ComputeInternalEnergy(state, i, j, k, &cons_fc);
