7732143115823ea8f1e6134fa4d359580c0220e2
James471 <James.Watt@anu.edu.au>
2026-09-22 02:22:56 +1000

    Add GetThermalBandQuanta

--- src/radiation/radiation_system.hpp
+++ src/radiation/radiation_system.hpp
@@ -365,6 +365,8 @@ template <typename problem_t> class RadSystem : public HyperbolicSystem<problem_
 
 	// static functions
 
+	AMREX_GPU_HOST_DEVICE static auto GetThermalBandQuanta(int group_index) -> amrex::Real;
+
 #ifdef PHOTOCHEMISTRY
 	AMREX_GPU_HOST_DEVICE static auto GetChemBandQuanta(int group_index) -> amrex::Real;
 #endif
@@ -848,12 +850,15 @@ void RadSystem<problem_t>::ConservedToPrimitive(amrex::Array4<const amrex::Real>
 	});
 }
 
+template <typename problem_t> AMREX_GPU_HOST_DEVICE auto RadSystem<problem_t>::GetThermalBandQuanta(int const group_index) -> amrex::Real
+{
+	AMREX_ASSERT(group_index >= 0 && group_index < nGroupsThermal_);
+	return 0.5 * (radBoundaries_[group_index] + radBoundaries_[group_index + 1]) * C::ev2erg;
+}
+
 #ifdef PHOTOCHEMISTRY
 template <typename problem_t> AMREX_GPU_HOST_DEVICE auto RadSystem<problem_t>::GetChemBandQuanta(int group_index) -> amrex::Real
 {
-	// ChemBands() is in eV (jaff's native unit for radiation band edges);
-	// convert to erg here rather than have every problem's CMakeLists
-	// convert to Hz by hand.
 	auto const ev_bounds = RadSystem_Traits<problem_t>::ChemBands();
 	amrex::Real const ev_low = ev_bounds[group_index];
 	amrex::Real const ev_high = ev_bounds[group_index + 1];
